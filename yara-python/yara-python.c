fef1570ec08182ef916c4a2a4e9720fc787afe55
Victor M. Alvarez <plusvic@gmail.com>
2010-09-29 13:16:39 +0000

    Fix undeclared "Py_ssize_t" compilation error with Python2.4

--- yara-python/yara-python.c
+++ yara-python/yara-python.c
@@ -21,6 +21,12 @@ GNU General Public License for more details.
 
 #include <yara.h>
 
+#if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
+typedef int Py_ssize_t;
+#define PY_SSIZE_T_MAX INT_MAX
+#define PY_SSIZE_T_MIN INT_MIN
+#endif
+
 /* Module globals */
 
 static PyObject *YaraError = NULL;
