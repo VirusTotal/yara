982a0d401e14b6465c2063554d9cfca62edb41ee
Victor M. Alvarez <plusvic@gmail.com>
2012-02-23 11:35:37 +0000

    Improve bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,21 @@
 # Quick and dirty script to reset everything
 # regarding libtool, autoconf, automake, etc.
 
-make distclean
-aclocal
-libtoolize
-autoreconf --force && cd libyara/ && autoreconf --force
+
+mkdir m4
+mkdir libyara/m4
+
+# Check if libtoolize exists, if not, try with glibtoolize (Mac OS X name it that way)
+hash libtoolize &> /dev/null
+
+if [ $? -eq 1 ]; then
+    glibtoolize
+else
+    libtoolize
+fi
+
+automake --add-missing 
+cd libyara  && aclocal && automake --add-missing && cd ..
+
+autoreconf -vif
+cd libyara && aclocal && autoreconf -vif && cd ..
--- config.h
+++ config.h
@@ -1,11 +1,45 @@
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
 /* Define to 1 if you have the `pcre' library (-lpcre). */
 #define HAVE_LIBPCRE 1
 
 /* Define to 1 if you have the `re2' library (-lre2). */
 /* #undef HAVE_LIBRE2 */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
 
 /* Name of package */
 #define PACKAGE "yara"
@@ -24,6 +58,9 @@
 
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "1.6"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
 
 /* Version number of package */
 #define VERSION "1.6"
