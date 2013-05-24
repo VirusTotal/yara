77f24996c69f0c6575014768a310d10684674c9b
Victor M. Alvarez <plusvic@gmail.com>
2013-05-24 10:29:14 +0000

    Update configure.ac

--- config.h
+++ config.h
@@ -51,19 +51,16 @@
 #define PACKAGE_NAME "yara"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "yara 1.7"
+#define PACKAGE_STRING "yara 2.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "yara"
 
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.7"
+#define PACKAGE_VERSION "2.0"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.7"
+#define VERSION "2.0"
