224aa4b8bb813acdfd3362876e030e0054aecbbb
Victor M. Alvarez <plusvic@gmail.com>
2013-11-04 12:46:26 +0000

    Fix build system

--- config.h
+++ config.h
@@ -7,6 +7,9 @@
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#define HAVE_LIBPTHREAD 1
+
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
 
