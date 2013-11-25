c6d6b2a76835a739da51b172ebe9f5b933352bf8
Victor M. Alvarez <plusvic@gmail.com>
2013-11-25 18:46:15 +0100

    Bump to version 1.7.1

--- yara-python/setup.py
+++ yara-python/setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup, Extension
-                           
+
 setup(  name = "yara-python",
-        version = "1.7",
+        version = "1.7.1",
         author = "Victor M. Alvarez",
         author_email = "vmalvarez@virustotal.com",
-        url = 'http://yara-project.googlecode.com',
+        url = 'http://plusvic.github.io/yara/',
         platforms = ['any'],
         ext_modules = [ Extension(
-                                    name='yara', 
+                                    name='yara',
                                     sources=['yara-python.c'],
                                     libraries=['yara','pcre'],
                                     include_dirs=['/usr/local/include']
                                     )])
-     
- 
-                                  
+
+
+
--- yara-python/setupwin64.py
+++ yara-python/setupwin64.py
@@ -1,15 +1,15 @@
 from distutils.core import setup, Extension
-             
+
 setup(  name = "yara-python",
-        version = "1.7",
+        version = "1.7.1",
         author = "Victor M. Alvarez",
         author_email = "vmalvarez@virustotal.com",
         ext_modules = [ Extension(
-                                    name='yara', 
+                                    name='yara',
                                     sources=['yara-python.c'],
                                     include_dirs=['../windows/include', '../libyara'],
                                     extra_objects=['../windows/yara/x64/Release/libyara64.lib','../windows/lib/pcre64.lib']
                                     )])
-     
- 
-                                  
+
+
+
