VirusTotal/yara

commit d5358cd39fb29f94cc7e205a7e97497c460502f9

Avoid possible stack overflow when compiled with assertions disabled

libyara/rules.c
@@ -265,6 +265,10 @@ int _yr_scan_fast_hex_re_exec(
                  *(ip + 12) == *next_input))
             {
               assert(sp < MAX_FAST_HEX_RE_STACK);
+
+              if (sp < MAX_FAST_HEX_RE_STACK)
+                return -2;
+
               code_stack[sp] = ip + 11;
               input_stack[sp] = next_input;
               matches_stack[sp] = matches + i;
@@ -669,8 +673,7 @@ int _yr_scan_verify_re_match(
         NULL);
   }
 
-  if (STRING_IS_WIDE(ac_match->string) &&
-      forward_matches < 0)
+  if (STRING_IS_WIDE(ac_match->string) && forward_matches == -1)
   {
     flags |= RE_FLAGS_WIDE;
     forward_matches = exec(
@@ -682,7 +685,10 @@ int _yr_scan_verify_re_match(
         NULL);
   }
 
-  if (forward_matches < 0)
+  if (forward_matches == -2)
+    return ERROR_INTERNAL_FATAL_ERROR;
+
+  if (forward_matches == -1)
     return ERROR_SUCCESS;
 
   if (forward_matches == 0 && ac_match->backward_code == NULL)

commit bc075630a00ea070b496d1c3fede00cfb822bc98

Fix bug introduced in d5358cd39fb29f94cc7e205a7e97497c460502f9

libyara/rules.c
@@ -266,7 +266,7 @@ int _yr_scan_fast_hex_re_exec(
             {
               assert(sp < MAX_FAST_HEX_RE_STACK);
 
-              if (sp < MAX_FAST_HEX_RE_STACK)
+              if (sp >= MAX_FAST_HEX_RE_STACK)
                 return -2;
 
               code_stack[sp] = ip + 11;