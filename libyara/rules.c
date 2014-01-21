VirusTotal/yara

commit 3a7fe213404e91022a5c678e3d47bd732d4bd61f

Fixed a mutex leak.

libyara/rules.c
@@ -1396,6 +1396,12 @@ int yr_rules_destroy(
     external++;
   }
 
+  #if WIN32
+  CloseHandle(rules->mutex);
+  #else
+  pthread_mutex_destroy(rules->mutex);
+  #endif
+
   yr_arena_destroy(rules->arena);
   yr_free(rules);
 

commit 4d4e97ceec36183286a3ed4e3cbe9cd5e9ee5e0d

Fix bug introduced in 3a7fe213404e91022a5c678e3d47bd732d4bd61f

libyara/rules.c
@@ -1399,7 +1399,7 @@ int yr_rules_destroy(
   #if WIN32
   CloseHandle(rules->mutex);
   #else
-  pthread_mutex_destroy(rules->mutex);
+  pthread_mutex_destroy(&rules->mutex);
   #endif
 
   yr_arena_destroy(rules->arena);