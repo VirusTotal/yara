044be47a78f39d2a40d31fce8543273a372cf5de
Victor M. Alvarez <plusvic@gmail.com>
2014-07-15 12:14:47 +0200

    Add demo module

---
+++ libyara/modules/demo.c
@@ -0,0 +1,45 @@
+/*
+Copyright (c) 2014. The YARA Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <yara/modules.h>
+
+#define MODULE_NAME demo
+
+begin_declarations;
+
+  string("greeting");
+
+end_declarations;
+
+
+int module_load(
+    YR_SCAN_CONTEXT* context,
+    YR_OBJECT* module,
+    void* module_data,
+    size_t module_data_size)
+{
+
+  set_string("Hello World!", module, "greeting");
+
+  return ERROR_SUCCESS;
+}
+
+
+int module_unload(
+    YR_OBJECT* module)
+{
+  return ERROR_SUCCESS;
+}
