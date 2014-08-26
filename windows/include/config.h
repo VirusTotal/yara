a6ad94b99741648070b248e5268c287d49d2463d
Victor Manuel Alvarez <vmalvarez@virustotal.com>
2014-08-26 23:08:43 +0200

    Add config.h for Windows

---
+++ windows/include/config.h
@@ -0,0 +1,18 @@
+
+/* Name of package */
+#define PACKAGE "yara"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "vmalvarez@virustotal.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "yara"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "yara 3.1"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.1"
+
+/* Version number of package */
+#define VERSION "3.1"
