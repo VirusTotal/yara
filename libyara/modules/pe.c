d3f27073d022699c974cd1dc13d5a32d3e77545f
Victor M. Alvarez <plusvic@gmail.com>
2014-10-15 12:57:33 +0200

    Add missing include

--- libyara/modules/pe.c
+++ libyara/modules/pe.c
@@ -14,6 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#define _GNU_SOURCE
+
+#include <stdio.h>
 #include <ctype.h>
 #include <openssl/md5.h>
 #include <openssl/sha.h>
