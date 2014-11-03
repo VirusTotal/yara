93cce9028a349b88a0b76ebab44af4a4cd6e687c
Victor M. Alvarez <plusvic@gmail.com>
2014-11-03 10:19:44 +0100

    Add missing define

--- libyara/exec.c
+++ libyara/exec.c
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#define _GNU_SOURCE
+
 #include <string.h>
 #include <assert.h>
 #include <time.h>
