5480ab8a193b7c68553d6d5383af99142976656b
Victor M. Alvarez <plusvic@gmail.com>
2014-11-10 15:35:59 +0100

    Add missing library to setupwinXX.py

--- yara-python/setupwin64.py
+++ yara-python/setupwin64.py
@@ -25,5 +25,5 @@ setup(name='yara-python',
         sources=['yara-python.c'],
         include_dirs=['../windows/include', '../libyara/include'],
         extra_objects=[
-          'user32.lib', '../windows/libyara/Release/libyara64.lib']
+          'user32.lib', 'gdi32.lib', '../windows/libyara/Release/libyara64.lib']
         )])
