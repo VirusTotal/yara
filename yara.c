f795cb71619278156b4a463700b94bf5b402f760
Baptiste Fontaine <b@ptistefontaine.fr>
2015-02-19 14:51:48 +0100

    Variable redefinition error fixed
    This fixes a compilation error with clang:
    
        $ make
        (…)
        libtool: compile:  clang -DHAVE_CONFIG_H -I. -I.. -O3 -Wall -Wno-deprecated-declarations -std=gnu99 -I./include -std=gnu89 -D_THREAD_SAFE -c modules/math.c  -fno-common -DPIC -o modules/.libs/math.o
        modules/math.c:53:12: error: redefinition of 'i'
          for (int i = 0; i < 256; i++)
                   ^
        modules/math.c:45:12: note: previous definition is here
          for (int i = 0; i < s->length; i++)
                   ^
        1 error generated.

--- yara.c
+++ yara.c
@@ -1014,7 +1014,9 @@ int main(
     thread_args.rules = rules;
     thread_args.start_time = start_time;
 
-    for (int i = 0; i < threads; i++)
+    int i;
+
+    for (i = 0; i < threads; i++)
     {
       if (create_thread(&thread[i], scanning_thread, (void*) &thread_args))
       {
@@ -1033,7 +1035,7 @@ int main(
     file_queue_finish();
 
     // Wait for scan threads to finish
-    for (int i = 0; i < threads; i++)
+    for (i = 0; i < threads; i++)
       thread_join(&thread[i]);
 
     file_queue_destroy();
