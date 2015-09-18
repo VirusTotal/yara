VirusTotal/yara

commit 9397c305e420b69b72962313668ad941539c7e16

Fix multiple warnings

libyara/modules.c
@@ -14,9 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
-#include <config.h>
-
 #include <yara/exec.h>
 #include <yara/modules.h>
 #include <yara/libyara.h>

libyara/arena.c
@@ -42,7 +42,7 @@ from files.
 typedef struct _ARENA_FILE_HEADER
 {
   char      magic[4];
-  uint64_t  size;
+  uint32_t  size;
   uint8_t   version;
 
 } ARENA_FILE_HEADER;
@@ -1027,11 +1027,13 @@ int yr_arena_save_stream(
     reloc = reloc->next;
   }
 
+  assert(page->size < 0x100000000);  // 4GB
+
   header.magic[0] = 'Y';
   header.magic[1] = 'A';
   header.magic[2] = 'R';
   header.magic[3] = 'A';
-  header.size = page->size;
+  header.size = (int32_t) page->size;
   header.version = ARENA_FILE_VERSION;
 
   yr_stream_write(&header, sizeof(header), 1, stream);

libyara/exec.c
@@ -731,73 +731,73 @@ int yr_execute_code(
 
       case OP_INT8:
         pop(r1);
-        r1.i = read_int8_t_little_endian(context->mem_block, r1.i);
+        r1.i = read_int8_t_little_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_INT16:
         pop(r1);
-        r1.i = read_int16_t_little_endian(context->mem_block, r1.i);
+        r1.i = read_int16_t_little_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_INT32:
         pop(r1);
-        r1.i = read_int32_t_little_endian(context->mem_block, r1.i);
+        r1.i = read_int32_t_little_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_UINT8:
         pop(r1);
-        r1.i = read_uint8_t_little_endian(context->mem_block, r1.i);
+        r1.i = read_uint8_t_little_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_UINT16:
         pop(r1);
-        r1.i = read_uint16_t_little_endian(context->mem_block, r1.i);
+        r1.i = read_uint16_t_little_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_UINT32:
         pop(r1);
-        r1.i = read_uint32_t_little_endian(context->mem_block, r1.i);
+        r1.i = read_uint32_t_little_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_INT8BE:
         pop(r1);
-        r1.i = read_int8_t_big_endian(context->mem_block, r1.i);
+        r1.i = read_int8_t_big_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_INT16BE:
         pop(r1);
-        r1.i = read_int16_t_big_endian(context->mem_block, r1.i);
+        r1.i = read_int16_t_big_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_INT32BE:
         pop(r1);
-        r1.i = read_int32_t_big_endian(context->mem_block, r1.i);
+        r1.i = read_int32_t_big_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_UINT8BE:
         pop(r1);
-        r1.i = read_uint8_t_big_endian(context->mem_block, r1.i);
+        r1.i = read_uint8_t_big_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_UINT16BE:
         pop(r1);
-        r1.i = read_uint16_t_big_endian(context->mem_block, r1.i);
+        r1.i = read_uint16_t_big_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 
       case OP_UINT32BE:
         pop(r1);
-        r1.i = read_uint32_t_big_endian(context->mem_block, r1.i);
+        r1.i = read_uint32_t_big_endian(context->mem_block, (size_t) r1.i);
         push(r1);
         break;
 

libyara/filemap.c
@@ -187,9 +187,9 @@ YR_API int yr_filemap_map_fd(
     return ERROR_COULD_NOT_MAP_FILE;
 
   if (size == 0)
-    size = st.st_size - offset;
+    size = (size_t) (st.st_size - offset);
 
-  pmapped_file->size = yr_min(size, st.st_size - offset);
+  pmapped_file->size = yr_min(size, (size_t) (st.st_size - offset));
 
   if (pmapped_file->size != 0)
   {

libyara/include/yara/strutils.h
@@ -34,7 +34,7 @@ uint64_t xtoi(
     const char* hexstr);
 
 
-#if !HAVE_STRLCPY
+#if !HAVE_STRLCPY && !defined(strlcpy)
 size_t strlcpy(
     char *dst,
     const char *src,
@@ -42,7 +42,7 @@ size_t strlcpy(
 #endif
 
 
-#if !HAVE_STRLCAT
+#if !HAVE_STRLCAT && !defined(strlcat)
 size_t strlcat(
     char *dst,
     const char *src,
@@ -50,7 +50,7 @@ size_t strlcat(
 #endif
 
 
-#if !HAVE_MEMMEM
+#if !HAVE_MEMMEM && !defined(memmem)
 void* memmem(
     const void *haystack,
     size_t haystack_size,

libyara/modules/hash.c
@@ -135,8 +135,9 @@ define_function(data_md5)
     if (offset >= block->base &&
         offset < block->base + block->size)
     {
-      uint64_t data_offset = offset - block->base;
-      uint64_t data_len = yr_min(length, block->size - data_offset);
+      size_t data_offset = (size_t) (offset - block->base);
+      size_t data_len = (size_t) yr_min(
+          length, (size_t) (block->size - data_offset));
 
       offset += data_len;
       length -= data_len;
@@ -198,8 +199,9 @@ define_function(data_sha1)
     if (offset >= block->base &&
         offset < block->base + block->size)
     {
-      uint64_t data_offset = offset - block->base;
-      uint64_t data_len = yr_min(length, block->size - data_offset);
+      size_t data_offset = (size_t) (offset - block->base);
+      size_t data_len = (size_t) yr_min(
+          length, (size_t) block->size - data_offset);
 
       offset += data_len;
       length -= data_len;
@@ -261,8 +263,8 @@ define_function(data_sha256)
     if (offset >= block->base &&
         offset < block->base + block->size)
     {
-      uint64_t data_offset = offset - block->base;
-      uint64_t data_len = yr_min(length, block->size - data_offset);
+      size_t data_offset = (size_t) (offset - block->base);
+      size_t data_len = (size_t) yr_min(length, block->size - data_offset);
 
       offset += data_len;
       length -= data_len;
@@ -318,8 +320,8 @@ define_function(data_checksum32)
     if (offset >= block->base &&
         offset < block->base + block->size)
     {
-      uint64_t data_offset = offset - block->base;
-      uint64_t data_len = yr_min(length, block->size - data_offset);
+      size_t data_offset = (size_t) (offset - block->base);
+      size_t data_len = (size_t) yr_min(length, block->size - data_offset);
 
       offset += data_len;
       length -= data_len;

libyara/modules/pe.c
@@ -1020,7 +1020,7 @@ IMPORTED_DLL* pe_parse_imports(
     {
       char* dll_name = (char *) (pe->data + offset);
 
-      if (!pe_valid_dll_name(dll_name, pe->data_size - offset))
+      if (!pe_valid_dll_name(dll_name, pe->data_size - (size_t) offset))
         break;
 
       IMPORTED_FUNCTION* functions = pe_parse_import_descriptor(
@@ -1462,7 +1462,7 @@ define_function(exports)
 
     char* name = (char*)(pe->data + offset);
 
-    if (strncmp(name, function_name, pe->data_size - offset) == 0)
+    if (strncmp(name, function_name, pe->data_size - (size_t) offset) == 0)
       return_integer(1);
   }
 

libyara/object.c
@@ -42,7 +42,7 @@ int yr_object_create(
     YR_OBJECT** object)
 {
   YR_OBJECT* obj;
-  size_t object_size;
+  size_t object_size = 0;
 
   switch (type)
   {

commit 14a465c102438d87c512514b89d9d4a716a89a0e

Fix hash module.
This fixes a regression introduced in 9397c30, which causes the hash
module to not be found by yara. For example, after 9397c30 was commited
this happens after building yara:

wxs@psh yara % nm ./yara | grep string_md5
wxs@psh yara %

The "string_md5" symbol is one that is from hash.c. Looking at the build
log hash.c is compiled and the "string_md5" symbol is in
libyara/.libs/libyara.a and libyara/.libs/libyara.dylib after
compilation. But when yara is linked, the entire hash module is left
out.

After a bisection 9397c30 was found to be the culprit and the only
answer was this particular line being removed.

libyara/modules.c
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include <config.h>
+
 #include <yara/exec.h>
 #include <yara/modules.h>
 #include <yara/libyara.h>