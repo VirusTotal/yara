6db06a4b9aab7b79e40299a2784cb101b40783c6
Coleman Kane <ckane@colemankane.org>
2015-10-09 19:50:14 -0400

    Create new option -k for stack size
    Sets the stack size for running the yara matching engine. Larger stack
    size = more memory footprint, but also larger rule size support.

--- yara.c
+++ yara.c
@@ -158,6 +158,9 @@ args_option_t options[] =
   OPT_INTEGER('a', "timeout", &timeout,
       "abort scanning after the given number of SECONDS", "SECONDS"),
 
+  OPT_INTEGER('k', "stack-size", &timeout,
+      "Set stack size to allocate in exec() to BYTES", "BYTES"),
+
   OPT_BOOLEAN('r', "recursive", &recursive_search,
       "recursively search directories"),
 
