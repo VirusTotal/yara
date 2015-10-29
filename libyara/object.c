2aa5a51b8189b81d0c7904f99629287d60f995ba
Mario Suvajac <msuvajac@gmail.com>
2015-10-29 08:20:07 +0100

    Enable usage of yr_object_print_data() from C++ code.

--- libyara/object.c
+++ libyara/object.c
@@ -1070,7 +1070,7 @@ YR_OBJECT* yr_object_get_root(
   return o;
 }
 
-void yr_object_print_data(
+YR_API void yr_object_print_data(
     YR_OBJECT* object,
     int indent,
     int print_identifier)
