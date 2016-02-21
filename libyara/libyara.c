dcf82d735e48ae2676729df4fcf52a72188fff06
Kyle Reed <kallanreed@outlook.com>
2016-02-20 16:42:07 -0800

    Fixing whitespace from merged PR
    Signed-off-by: Kyle Reed <kallanreed@outlook.com>

--- libyara/libyara.c
+++ libyara/libyara.c
@@ -54,28 +54,28 @@ typedef HANDLE pthread_mutex_t;
 
 unsigned long pthread_self()
 {
-	return GetCurrentThreadId();
+    return GetCurrentThreadId();
 }
 
 int pthread_mutex_init(pthread_mutex_t *mutex, void* attr)
 {
-	*mutex = CreateSemaphore(NULL, 1, 1, NULL);
-	return *mutex == NULL;
+    *mutex = CreateSemaphore(NULL, 1, 1, NULL);
+    return *mutex == NULL;
 }
 
 int pthread_mutex_destroy(pthread_mutex_t *mutex)
 {
-	return CloseHandle(mutex) == TRUE ? 0 : -1;
+    return CloseHandle(mutex) == TRUE ? 0 : -1;
 }
 
 int pthread_mutex_lock(pthread_mutex_t mutex)
 {
-	return WaitForSingleObject(mutex, INFINITE) == WAIT_OBJECT_0 ? 0 : -1;
+    return WaitForSingleObject(mutex, INFINITE) == WAIT_OBJECT_0 ? 0 : -1;
 }
 
 int pthread_mutex_unlock(pthread_mutex_t mutex)
 {
-	return ReleaseSemaphore(mutex, 1, NULL) == TRUE ? 0 : -1;
+    return ReleaseSemaphore(mutex, 1, NULL) == TRUE ? 0 : -1;
 }
 #endif
 #endif
