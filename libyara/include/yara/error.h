009a5b68ed4901df8cfeba4b1b2986f1cae3df00
Kyle Reed <kallanreed@outlook.com>
2016-02-27 19:59:22 -0800

    Update error number
    Signed-off-by: Kyle Reed <kallanreed@outlook.com>

--- libyara/include/yara/error.h
+++ libyara/include/yara/error.h
@@ -72,6 +72,7 @@ limitations under the License.
 #define ERROR_DIVISION_BY_ZERO                  44
 #define ERROR_REGULAR_EXPRESSION_TOO_LARGE      45
 #define ERROR_TOO_MANY_RE_FIBERS                46
+#define ERROR_COULD_NOT_READ_PROCESS_MEMORY     47
 
 
 #define FAIL_ON_ERROR(x) { \
