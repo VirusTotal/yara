VirusTotal/yara

commit 0db16d3639140c0b6a7a6d0de06e5e2622e90c04

Implement Aho-Corasick automaton with interleaved arrays.
This improves both the performance and the memory footprint.

libyara/include/yara/ahocorasick.h
@@ -22,29 +22,50 @@ limitations under the License.
 #include <yara/types.h>
 
 
-int yr_ac_create_automaton(
-    YR_ARENA* arena,
+#define YR_AC_ROOT_STATE                0
+#define YR_AC_NEXT_STATE(t)             (t >> 32)
+#define YR_AC_INVALID_TRANSITION(t, c)  (((t) & 0xFFFF) != c)
+
+#define YR_AC_MAKE_TRANSITION(state, code, flags) \
+  ((uint64_t)((((uint64_t) state) << 32) | ((flags) << 16) | code))
+
+#define YR_AC_USED_FLAG    0x1
+
+#define YR_AC_USED_TRANSITION_SLOT(x)   ((x) & (YR_AC_USED_FLAG << 16))
+#define YR_AC_UNUSED_TRANSITION_SLOT(x) (!YR_AC_USED_TRANSITION_SLOT(x))
+
+
+typedef struct ac_tables
+{
+  YR_AC_TRANSITION* transitions;
+  YR_AC_MATCH** matches;
+
+} YR_AC_TABLES;
+
+
+int yr_ac_automaton_create(
     YR_AC_AUTOMATON** automaton);
 
 
+int yr_ac_automaton_destroy(
+    YR_AC_AUTOMATON* automaton);
+
+
 int yr_ac_add_string(
-    YR_ARENA* arena,
     YR_AC_AUTOMATON* automaton,
     YR_STRING* string,
-    YR_ATOM_LIST_ITEM* atom);
+    YR_ATOM_LIST_ITEM* atom,
+    YR_ARENA* matches_arena);
 
 
-YR_AC_STATE* yr_ac_next_state(
-    YR_AC_STATE* state,
-    uint8_t input);
-
-
-int yr_ac_create_failure_links(
+int yr_ac_compile(
+    YR_AC_AUTOMATON* automaton,
     YR_ARENA* arena,
-    YR_AC_AUTOMATON* automaton);
+    YR_AC_TABLES* tables);
 
 
 void yr_ac_print_automaton(
     YR_AC_AUTOMATON* automaton);
+
 
 #endif

libyara/include/yara/types.h
@@ -37,66 +37,9 @@ typedef int32_t tidx_mask_t;
 #define DECLARE_REFERENCE(type, name) \
     union { type name; int64_t name##_; } YR_ALIGN(8)
 
-#pragma pack(push)
-#pragma pack(8)
 
 
 #define NAMESPACE_TFLAGS_UNSATISFIED_GLOBAL      0x01
-
-
-typedef struct _YR_NAMESPACE
-{
-  int32_t t_flags[MAX_THREADS];     // Thread-specific flags
-  DECLARE_REFERENCE(char*, name);
-
-} YR_NAMESPACE;
-
-
-#define META_TYPE_NULL      0
-#define META_TYPE_INTEGER   1
-#define META_TYPE_STRING    2
-#define META_TYPE_BOOLEAN   3
-
-#define META_IS_NULL(x) \
-    ((x) != NULL ? (x)->type == META_TYPE_NULL : TRUE)
-
-
-typedef struct _YR_META
-{
-  int32_t type;
-  YR_ALIGN(8) int64_t integer;
-
-  DECLARE_REFERENCE(const char*, identifier);
-  DECLARE_REFERENCE(char*, string);
-
-} YR_META;
-
-
-typedef struct _YR_MATCH
-{
-  int64_t base;
-  int64_t offset;
-  int32_t length;
-
-  union {
-    uint8_t* data;           // Confirmed matches use "data",
-    int32_t chain_length;    // unconfirmed ones use "chain_length"
-  } YR_ALIGN(8);
-
-  YR_ALIGN(8) struct _YR_MATCH* prev;
-  YR_ALIGN(8) struct _YR_MATCH* next;
-
-} YR_MATCH;
-
-
-typedef struct _YR_MATCHES
-{
-  int32_t count;
-
-  DECLARE_REFERENCE(YR_MATCH*, head);
-  DECLARE_REFERENCE(YR_MATCH*, tail);
-
-} YR_MATCHES;
 
 
 #define STRING_GFLAGS_REFERENCED        0x01
@@ -116,7 +59,6 @@ typedef struct _YR_MATCHES
 #define STRING_GFLAGS_CHAIN_TAIL        0x4000
 #define STRING_GFLAGS_FIXED_OFFSET      0x8000
 #define STRING_GFLAGS_GREEDY_REGEXP     0x10000
-
 
 #define STRING_IS_HEX(x) \
     (((x)->g_flags) & STRING_GFLAGS_HEXADECIMAL)
@@ -176,6 +118,97 @@ typedef struct _YR_MATCHES
     ((x)->matches[yr_get_tidx()])
 
 
+#define RULE_TFLAGS_MATCH                0x01
+
+#define RULE_GFLAGS_PRIVATE              0x01
+#define RULE_GFLAGS_GLOBAL               0x02
+#define RULE_GFLAGS_REQUIRE_EXECUTABLE   0x04
+#define RULE_GFLAGS_REQUIRE_FILE         0x08
+#define RULE_GFLAGS_NULL                 0x1000
+
+#define RULE_IS_PRIVATE(x) \
+    (((x)->g_flags) & RULE_GFLAGS_PRIVATE)
+
+#define RULE_IS_GLOBAL(x) \
+    (((x)->g_flags) & RULE_GFLAGS_GLOBAL)
+
+#define RULE_IS_NULL(x) \
+    (((x)->g_flags) & RULE_GFLAGS_NULL)
+
+#define RULE_MATCHES(x) \
+    ((x)->t_flags[yr_get_tidx()] & RULE_TFLAGS_MATCH)
+
+
+#define META_TYPE_NULL      0
+#define META_TYPE_INTEGER   1
+#define META_TYPE_STRING    2
+#define META_TYPE_BOOLEAN   3
+
+#define META_IS_NULL(x) \
+    ((x) != NULL ? (x)->type == META_TYPE_NULL : TRUE)
+
+
+#define EXTERNAL_VARIABLE_TYPE_NULL           0
+#define EXTERNAL_VARIABLE_TYPE_FLOAT          1
+#define EXTERNAL_VARIABLE_TYPE_INTEGER        2
+#define EXTERNAL_VARIABLE_TYPE_BOOLEAN        3
+#define EXTERNAL_VARIABLE_TYPE_STRING         4
+#define EXTERNAL_VARIABLE_TYPE_MALLOC_STRING  5
+
+#define EXTERNAL_VARIABLE_IS_NULL(x) \
+    ((x) != NULL ? (x)->type == EXTERNAL_VARIABLE_TYPE_NULL : TRUE)
+
+
+#pragma pack(push)
+#pragma pack(8)
+
+
+typedef struct _YR_NAMESPACE
+{
+  int32_t t_flags[MAX_THREADS];     // Thread-specific flags
+  DECLARE_REFERENCE(char*, name);
+
+} YR_NAMESPACE;
+
+
+typedef struct _YR_META
+{
+  int32_t type;
+  YR_ALIGN(8) int64_t integer;
+
+  DECLARE_REFERENCE(const char*, identifier);
+  DECLARE_REFERENCE(char*, string);
+
+} YR_META;
+
+
+typedef struct _YR_MATCH
+{
+  int64_t base;
+  int64_t offset;
+  int32_t length;
+
+  union {
+    uint8_t* data;           // Confirmed matches use "data",
+    int32_t chain_length;    // unconfirmed ones use "chain_length"
+  } YR_ALIGN(8);
+
+  YR_ALIGN(8) struct _YR_MATCH* prev;
+  YR_ALIGN(8) struct _YR_MATCH* next;
+
+} YR_MATCH;
+
+
+typedef struct _YR_MATCHES
+{
+  int32_t count;
+
+  DECLARE_REFERENCE(YR_MATCH*, head);
+  DECLARE_REFERENCE(YR_MATCH*, tail);
+
+} YR_MATCHES;
+
+
 typedef struct _YR_STRING
 {
   int32_t g_flags;
@@ -200,27 +233,6 @@ typedef struct _YR_STRING
 } YR_STRING;
 
 
-#define RULE_TFLAGS_MATCH                0x01
-
-#define RULE_GFLAGS_PRIVATE              0x01
-#define RULE_GFLAGS_GLOBAL               0x02
-#define RULE_GFLAGS_REQUIRE_EXECUTABLE   0x04
-#define RULE_GFLAGS_REQUIRE_FILE         0x08
-#define RULE_GFLAGS_NULL                 0x1000
-
-#define RULE_IS_PRIVATE(x) \
-    (((x)->g_flags) & RULE_GFLAGS_PRIVATE)
-
-#define RULE_IS_GLOBAL(x) \
-    (((x)->g_flags) & RULE_GFLAGS_GLOBAL)
-
-#define RULE_IS_NULL(x) \
-    (((x)->g_flags) & RULE_GFLAGS_NULL)
-
-#define RULE_MATCHES(x) \
-    ((x)->t_flags[yr_get_tidx()] & RULE_TFLAGS_MATCH)
-
-
 typedef struct _YR_RULE
 {
   int32_t g_flags;               // Global flags
@@ -237,18 +249,6 @@ typedef struct _YR_RULE
   #endif
 
 } YR_RULE;
-
-
-#define EXTERNAL_VARIABLE_TYPE_NULL           0
-#define EXTERNAL_VARIABLE_TYPE_FLOAT          1
-#define EXTERNAL_VARIABLE_TYPE_INTEGER        2
-#define EXTERNAL_VARIABLE_TYPE_BOOLEAN        3
-#define EXTERNAL_VARIABLE_TYPE_STRING         4
-#define EXTERNAL_VARIABLE_TYPE_MALLOC_STRING  5
-
-
-#define EXTERNAL_VARIABLE_IS_NULL(x) \
-    ((x) != NULL ? (x)->type == EXTERNAL_VARIABLE_TYPE_NULL : TRUE)
 
 
 typedef struct _YR_EXTERNAL_VARIABLE
@@ -278,53 +278,9 @@ typedef struct _YR_AC_MATCH
 } YR_AC_MATCH;
 
 
-typedef struct _YR_AC_STATE
-{
-  int8_t depth;
-
-  DECLARE_REFERENCE(struct _YR_AC_STATE*, failure);
-  DECLARE_REFERENCE(YR_AC_MATCH*, matches);
-
-} YR_AC_STATE;
-
-
-typedef struct _YR_AC_STATE_TRANSITION
-{
-  uint8_t input;
-
-  DECLARE_REFERENCE(YR_AC_STATE*, state);
-  DECLARE_REFERENCE(struct _YR_AC_STATE_TRANSITION*, next);
-
-} YR_AC_STATE_TRANSITION;
-
-
-typedef struct _YR_AC_TABLE_BASED_STATE
-{
-  int8_t depth;
-
-  DECLARE_REFERENCE(YR_AC_STATE*, failure);
-  DECLARE_REFERENCE(YR_AC_MATCH*, matches);
-  DECLARE_REFERENCE(YR_AC_STATE*, state) transitions[256];
-
-} YR_AC_TABLE_BASED_STATE;
-
-
-typedef struct _YR_AC_LIST_BASED_STATE
-{
-  int8_t depth;
-
-  DECLARE_REFERENCE(YR_AC_STATE*, failure);
-  DECLARE_REFERENCE(YR_AC_MATCH*, matches);
-  DECLARE_REFERENCE(YR_AC_STATE_TRANSITION*, transitions);
-
-} YR_AC_LIST_BASED_STATE;
-
-
-typedef struct _YR_AC_AUTOMATON
-{
-  DECLARE_REFERENCE(YR_AC_STATE*, root);
-
-} YR_AC_AUTOMATON;
+typedef uint64_t            YR_AC_TRANSITION;
+typedef YR_AC_TRANSITION*   YR_AC_TRANSITION_TABLE;
+typedef YR_AC_MATCH**       YR_AC_MATCH_TABLE;
 
 
 typedef struct _YARA_RULES_FILE_HEADER
@@ -334,13 +290,52 @@ typedef struct _YARA_RULES_FILE_HEADER
   DECLARE_REFERENCE(YR_RULE*, rules_list_head);
   DECLARE_REFERENCE(YR_EXTERNAL_VARIABLE*, externals_list_head);
   DECLARE_REFERENCE(uint8_t*, code_start);
-  DECLARE_REFERENCE(YR_AC_AUTOMATON*, automaton);
+  DECLARE_REFERENCE(YR_AC_MATCH_TABLE, match_table);
+  DECLARE_REFERENCE(YR_AC_TRANSITION_TABLE, transition_table);
 
 } YARA_RULES_FILE_HEADER;
 
-
-
 #pragma pack(pop)
+
+
+//
+// Structs defined below are never stored in the compiled rules file
+//
+
+
+struct _YR_AC_STATE;
+
+
+typedef struct _YR_AC_STATE
+{
+  uint8_t depth;
+  uint8_t input;
+
+  uint32_t t_table_slot;
+
+  struct _YR_AC_STATE* failure;
+  struct _YR_AC_STATE* first_child;
+  struct _YR_AC_STATE* siblings;
+
+  YR_AC_MATCH* matches;
+
+} YR_AC_STATE;
+
+
+typedef struct _YR_AC_AUTOMATON
+{
+  // Both m_table and t_table have the same number of elements, which is
+  // stored in tables_size.
+
+  uint32_t tables_size;
+  uint32_t t_table_unused_candidate;
+
+  YR_AC_TRANSITION_TABLE t_table;
+  YR_AC_MATCH_TABLE m_table;
+
+  YR_AC_STATE* root;
+
+} YR_AC_AUTOMATON;
 
 
 typedef struct _YR_RULES {
@@ -352,7 +347,8 @@ typedef struct _YR_RULES {
   YR_ARENA* arena;
   YR_RULE* rules_list_head;
   YR_EXTERNAL_VARIABLE* externals_list_head;
-  YR_AC_AUTOMATON* automaton;
+  YR_AC_TRANSITION_TABLE transition_table;
+  YR_AC_MATCH_TABLE match_table;
 
 } YR_RULES;
 

libyara/rules.c
@@ -220,72 +220,88 @@ int _yr_rules_scan_mem_block(
     int timeout,
     time_t start_time)
 {
-  YR_AC_MATCH* ac_match;
-  YR_AC_STATE* next_state;
-  YR_AC_STATE* current_state = rules->automaton->root;
+  YR_AC_TRANSITION_TABLE transition_table = rules->transition_table;
+  YR_AC_MATCH_TABLE match_table = rules->match_table;
+
+  YR_AC_MATCH* match;
+  YR_AC_TRANSITION transition;
 
   size_t i = 0;
+  uint32_t state = YR_AC_ROOT_STATE;
+  uint16_t index;
 
   while (i < block->size)
   {
-    ac_match = current_state->matches;
+    match = match_table[state];
 
-    while (ac_match != NULL)
+    while (match != NULL)
     {
-      if (ac_match->backtrack <= i)
+      if (timeout > 0 && i % 4096 == 0)
+      {
+        if (difftime(time(NULL), start_time) > timeout)
+          return ERROR_SCAN_TIMEOUT;
+      }
+
+      if (match->backtrack <= i)
       {
         FAIL_ON_ERROR(yr_scan_verify_match(
             context,
-            ac_match,
+            match,
             block->data,
             block->size,
             block->base,
-            i - ac_match->backtrack));
+            i - match->backtrack));
       }
 
-      ac_match = ac_match->next;
+      match = match->next;
     }
 
-    next_state = yr_ac_next_state(current_state, block->data[i]);
+    index = block->data[i++] + 1;
+    transition = transition_table[state + index];
 
-    while (next_state == NULL && current_state->depth > 0)
+    while (YR_AC_INVALID_TRANSITION(transition, index))
     {
-      current_state = current_state->failure;
-      next_state = yr_ac_next_state(current_state, block->data[i]);
+      if (state != YR_AC_ROOT_STATE)
+      {
+        state = transition_table[state] >> 32;
+        transition = transition_table[state + index];
+      }
+      else
+      {
+        transition = 0;
+        break;
+      }
     }
 
-    if (next_state != NULL)
-      current_state = next_state;
+    state = transition >> 32;
 
-    i++;
-
-    if (timeout > 0 && i % 256 == 0)
-    {
-      if (difftime(time(NULL), start_time) > timeout)
-        return ERROR_SCAN_TIMEOUT;
-    }
   }
 
-  ac_match = current_state->matches;
 
-  while (ac_match != NULL)
+  match = match_table[state];
+
+  while (match != NULL)
   {
-    if (ac_match->backtrack <= block->size)
+    if (match->backtrack <= i)
     {
       FAIL_ON_ERROR(yr_scan_verify_match(
           context,
-          ac_match,
+          match,
           block->data,
           block->size,
           block->base,
-          block->size - ac_match->backtrack));
+          i - match->backtrack));
     }
 
-    ac_match = ac_match->next;
+    match = match->next;
   }
+
+
 
   return ERROR_SUCCESS;
 }
+
+
 
 
 YR_API int yr_rules_scan_mem_blocks(
@@ -631,10 +647,11 @@ YR_API int yr_rules_load_stream(
   header = (YARA_RULES_FILE_HEADER*)
       yr_arena_base_address(new_rules->arena);
 
-  new_rules->automaton = header->automaton;
   new_rules->code_start = header->code_start;
   new_rules->externals_list_head = header->externals_list_head;
   new_rules->rules_list_head = header->rules_list_head;
+  new_rules->match_table = header->match_table;
+  new_rules->transition_table = header->transition_table;
   new_rules->tidx_mask = 0;
 
   FAIL_ON_ERROR(yr_mutex_create(&new_rules->mutex));

libyara/arena.c
@@ -760,7 +760,9 @@ int yr_arena_write_string(
 // yr_arena_append
 //
 // Appends source_arena to target_arena. This operation destroys source_arena,
-// after returning any pointer to source_arena is no longer valid.
+// after returning any pointer to source_arena is no longer valid. The data
+// from source_arena is guaranteed to be aligned to a 16 bytes boundary when
+// written to the source_arena
 //
 // Args:
 //    YR_ARENA* target_arena    - Pointer to target the arena.
@@ -774,6 +776,20 @@ int yr_arena_append(
     YR_ARENA* target_arena,
     YR_ARENA* source_arena)
 {
+  uint8_t padding_data[15];
+  size_t padding_size = 16 - target_arena->current_page->used % 16;
+
+  if (padding_size < 16)
+  {
+    memset(&padding_data, 0xCC, padding_size);
+
+    FAIL_ON_ERROR(yr_arena_write_data(
+        target_arena,
+        padding_data,
+        padding_size,
+        NULL));
+  }
+
   target_arena->current_page->next = source_arena->page_list_head;
   source_arena->page_list_head->prev = target_arena->current_page;
   target_arena->current_page = source_arena->current_page;

libyara/atoms.c
@@ -1107,6 +1107,22 @@ int yr_atoms_extract_from_re(
     *atoms = _yr_atoms_list_concat(*atoms, case_insentive_atoms);
   }
 
+  // No atoms has been extracted, let's add a zero-length atom.
+
+  if (*atoms == NULL)
+  {
+    *atoms = (YR_ATOM_LIST_ITEM*) yr_malloc(sizeof(YR_ATOM_LIST_ITEM));
+
+    if (*atoms == NULL)
+      return ERROR_INSUFICIENT_MEMORY;
+
+    (*atoms)->atom_length = 0;
+    (*atoms)->backtrack = 0;
+    (*atoms)->forward_code = re->root_node->forward_code;
+    (*atoms)->backward_code = NULL;
+    (*atoms)->next = NULL;
+  }
+
   return ERROR_SUCCESS;
 }
 

libyara/compiler.c
@@ -80,9 +80,6 @@ YR_API int yr_compiler_create(
     result = yr_arena_create(65536, 0, &new_compiler->re_code_arena);
 
   if (result == ERROR_SUCCESS)
-    result = yr_arena_create(65536, 0, &new_compiler->automaton_arena);
-
-  if (result == ERROR_SUCCESS)
     result = yr_arena_create(65536, 0, &new_compiler->externals_arena);
 
   if (result == ERROR_SUCCESS)
@@ -92,9 +89,13 @@ YR_API int yr_compiler_create(
     result = yr_arena_create(65536, 0, &new_compiler->metas_arena);
 
   if (result == ERROR_SUCCESS)
-    result = yr_ac_create_automaton(
-        new_compiler->automaton_arena,
-        &new_compiler->automaton);
+    result = yr_arena_create(65536, 0, &new_compiler->automaton_arena);
+
+  if (result == ERROR_SUCCESS)
+    result = yr_arena_create(65536, 0, &new_compiler->matches_arena);
+
+  if (result == ERROR_SUCCESS)
+    result = yr_ac_automaton_create(&new_compiler->automaton);
 
   if (result == ERROR_SUCCESS)
   {
@@ -121,10 +122,13 @@ YR_API void yr_compiler_destroy(
   yr_arena_destroy(compiler->strings_arena);
   yr_arena_destroy(compiler->code_arena);
   yr_arena_destroy(compiler->re_code_arena);
-  yr_arena_destroy(compiler->automaton_arena);
   yr_arena_destroy(compiler->externals_arena);
   yr_arena_destroy(compiler->namespaces_arena);
   yr_arena_destroy(compiler->metas_arena);
+  yr_arena_destroy(compiler->automaton_arena);
+  yr_arena_destroy(compiler->matches_arena);
+
+    yr_ac_automaton_destroy(compiler->automaton);
 
   yr_hash_table_destroy(
       compiler->rules_table,
@@ -374,6 +378,7 @@ YR_API int yr_compiler_add_string(
   }
 }
 
+
 int _yr_compiler_compile_rules(
   YR_COMPILER* compiler)
 {
@@ -412,10 +417,13 @@ int _yr_compiler_compile_rules(
       sizeof(YR_EXTERNAL_VARIABLE),
       NULL);
 
-  // Create Aho-Corasick automaton's failure links.
-  result = yr_ac_create_failure_links(
+  YR_AC_TABLES tables;
+
+  // Write Aho-Corasick automaton to arena.
+  result = yr_ac_compile(
+      compiler->automaton,
       compiler->automaton_arena,
-      compiler->automaton);
+      &tables);
 
   if (result == ERROR_SUCCESS)
     result = yr_arena_create(1024, 0, &arena);
@@ -428,7 +436,8 @@ int _yr_compiler_compile_rules(
         offsetof(YARA_RULES_FILE_HEADER, rules_list_head),
         offsetof(YARA_RULES_FILE_HEADER, externals_list_head),
         offsetof(YARA_RULES_FILE_HEADER, code_start),
-        offsetof(YARA_RULES_FILE_HEADER, automaton),
+        offsetof(YARA_RULES_FILE_HEADER, match_table),
+        offsetof(YARA_RULES_FILE_HEADER, transition_table),
         EOL);
 
   if (result == ERROR_SUCCESS)
@@ -442,18 +451,12 @@ int _yr_compiler_compile_rules(
     rules_file_header->code_start = (uint8_t*) yr_arena_base_address(
         compiler->code_arena);
 
-    rules_file_header->automaton = (YR_AC_AUTOMATON*) yr_arena_base_address(
-        compiler->automaton_arena);
+    rules_file_header->match_table = tables.matches;
+    rules_file_header->transition_table = tables.transitions;
   }
 
   if (result == ERROR_SUCCESS)
-    result = yr_arena_append(
-        arena,
-        compiler->automaton_arena);
-
-  if (result == ERROR_SUCCESS)
   {
-    compiler->automaton_arena = NULL;
     result = yr_arena_append(
         arena,
         compiler->code_arena);
@@ -518,6 +521,22 @@ int _yr_compiler_compile_rules(
   if (result == ERROR_SUCCESS)
   {
     compiler->sz_arena = NULL;
+    result = yr_arena_append(
+        arena,
+        compiler->automaton_arena);
+  }
+
+  if (result == ERROR_SUCCESS)
+  {
+    compiler->automaton_arena = NULL;
+    result = yr_arena_append(
+        arena,
+        compiler->matches_arena);
+  }
+
+  if (result == ERROR_SUCCESS)
+  {
+    compiler->matches_arena = NULL;
     compiler->compiled_rules_arena = arena;
     result = yr_arena_coalesce(arena);
   }
@@ -552,7 +571,8 @@ YR_API int yr_compiler_get_rules(
 
   yara_rules->externals_list_head = rules_file_header->externals_list_head;
   yara_rules->rules_list_head = rules_file_header->rules_list_head;
-  yara_rules->automaton = rules_file_header->automaton;
+  yara_rules->match_table = rules_file_header->match_table;
+  yara_rules->transition_table = rules_file_header->transition_table;
   yara_rules->code_start = rules_file_header->code_start;
   yara_rules->tidx_mask = 0;
 

libyara/exception.h
@@ -91,6 +91,7 @@ typedef struct sigaction sa;
     act.sa_flags = 0; /* SA_ONSTACK? */                         \
     sigemptyset(&oldmask);                                      \
     sigemptyset(&act.sa_mask);                                  \
+    sigemptyset(&oldmask);                                      \
     pthread_sigmask(SIG_SETMASK, &act.sa_mask, &oldmask);       \
     sigaction(SIGBUS, &act, &oldact);                           \
     int tidx = yr_get_tidx();                                   \

commit 0bc31fb1f5d97bda3e7a58cca3c62fa5c32d8c1d

Fix issues in 32-bits mode introduced in 0db16d3639140c0b6a7a6d0de06e5e2622e90c04

libyara/ahocorasick.c
@@ -458,12 +458,15 @@ int _yr_ac_find_suitable_transition_table_slot(
 
     if (automaton->tables_size - i < 257)
     {
-      size_t t_bytes_size = automaton->tables_size * sizeof(YR_AC_TRANSITION);
-      size_t m_bytes_size = automaton->tables_size * sizeof(YR_AC_MATCH*);
+      size_t t_bytes_size = automaton->tables_size *
+          sizeof(YR_AC_TRANSITION);
+
+      size_t m_bytes_size = automaton->tables_size * 
+          sizeof(YR_AC_MATCH_TABLE_ENTRY);
 
       automaton->t_table = (YR_AC_TRANSITION_TABLE) yr_realloc(
           automaton->t_table, t_bytes_size * 2);
-      
+
 	  automaton->m_table = (YR_AC_MATCH_TABLE) yr_realloc(
           automaton->m_table, m_bytes_size * 2);
 
@@ -582,7 +585,7 @@ int _yr_ac_build_transition_table(
       automaton->tables_size * sizeof(YR_AC_TRANSITION));
 
   automaton->m_table = (YR_AC_MATCH_TABLE) yr_malloc(
-      automaton->tables_size * sizeof(YR_AC_MATCH*));
+      automaton->tables_size * sizeof(YR_AC_MATCH_TABLE_ENTRY));
 
   if (automaton->t_table == NULL || automaton->t_table == NULL)
   {
@@ -596,10 +599,10 @@ int _yr_ac_build_transition_table(
       automaton->tables_size * sizeof(YR_AC_TRANSITION));
 
   memset(automaton->m_table, 0,
-      automaton->tables_size * sizeof(YR_AC_MATCH*));
+      automaton->tables_size * sizeof(YR_AC_MATCH_TABLE_ENTRY));
 
   automaton->t_table[0] = YR_AC_MAKE_TRANSITION(0, 0, YR_AC_USED_FLAG);
-  automaton->m_table[0] = root_state->matches;
+  automaton->m_table[0].match = root_state->matches;
 
   // Index 0 is for root node. Unused indexes start at 1.
   automaton->t_table_unused_candidate = 1;
@@ -632,7 +635,7 @@ int _yr_ac_build_transition_table(
     automaton->t_table[slot] = YR_AC_MAKE_TRANSITION(
         state->failure->t_table_slot, 0, YR_AC_USED_FLAG);
 
-    automaton->m_table[slot] = state->matches;
+    automaton->m_table[slot].match = state->matches;
 
     // Push childrens of current_state
 
@@ -909,13 +912,13 @@ int yr_ac_compile(
   FAIL_ON_ERROR(yr_arena_write_data(
       arena,
       automaton->m_table,
-      sizeof(YR_AC_MATCH*),
+      sizeof(YR_AC_MATCH_TABLE_ENTRY),
       (void**) &tables->matches));
 
   FAIL_ON_ERROR(yr_arena_make_relocatable(
       arena,
       tables->matches,
-      0,
+      offsetof(YR_AC_MATCH_TABLE_ENTRY, match),
       EOL));
 
   for (i = 1; i < automaton->tables_size; i++)
@@ -925,13 +928,13 @@ int yr_ac_compile(
     FAIL_ON_ERROR(yr_arena_write_data(
         arena,
         automaton->m_table + i,
-        sizeof(YR_AC_MATCH*),
+        sizeof(YR_AC_MATCH_TABLE_ENTRY),
         (void**) &ptr));
 
     FAIL_ON_ERROR(yr_arena_make_relocatable(
         arena,
         ptr,
-        0,
+        offsetof(YR_AC_MATCH_TABLE_ENTRY, match),
         EOL));
   }
 

libyara/include/yara/ahocorasick.h
@@ -35,10 +35,10 @@ limitations under the License.
 #define YR_AC_UNUSED_TRANSITION_SLOT(x) (!YR_AC_USED_TRANSITION_SLOT(x))
 
 
-typedef struct ac_tables
+typedef struct _YR_AC_TABLES
 {
   YR_AC_TRANSITION* transitions;
-  YR_AC_MATCH** matches;
+  YR_AC_MATCH_TABLE_ENTRY* matches;
 
 } YR_AC_TABLES;
 

libyara/include/yara/types.h
@@ -278,9 +278,16 @@ typedef struct _YR_AC_MATCH
 } YR_AC_MATCH;
 
 
-typedef uint64_t            YR_AC_TRANSITION;
-typedef YR_AC_TRANSITION*   YR_AC_TRANSITION_TABLE;
-typedef YR_AC_MATCH**       YR_AC_MATCH_TABLE;
+typedef struct _YR_AC_MATCH_TABLE_ENTRY
+{
+  DECLARE_REFERENCE(YR_AC_MATCH*, match);
+
+} YR_AC_MATCH_TABLE_ENTRY;
+
+
+typedef uint64_t                  YR_AC_TRANSITION;
+typedef YR_AC_TRANSITION*         YR_AC_TRANSITION_TABLE;
+typedef YR_AC_MATCH_TABLE_ENTRY*  YR_AC_MATCH_TABLE;
 
 
 typedef struct _YARA_RULES_FILE_HEADER

libyara/rules.c
@@ -232,7 +232,7 @@ int _yr_rules_scan_mem_block(
 
   while (i < block->size)
   {
-    match = match_table[state];
+    match = match_table[state].match;
 
     while (match != NULL)
     {
@@ -278,7 +278,7 @@ int _yr_rules_scan_mem_block(
   }
 
 
-  match = match_table[state];
+  match = match_table[state].match;
 
   while (match != NULL)
   {