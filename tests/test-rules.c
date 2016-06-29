22ce5e09dd49ccfb175524e8c6066984d1ecfb63
plusvic <plusvic@gmail.com>
2016-06-29 18:47:07 +0200

    Implement caching in "hash" module

--- tests/test-rules.c
+++ tests/test-rules.c
@@ -1264,6 +1264,56 @@ static void test_modules()
 }
 
 
+static void test_hash_module()
+{
+  uint8_t blob[] = {0x61, 0x62, 0x63, 0x64, 0x65};
+
+  assert_true_rule_blob(
+      "import \"hash\" \
+       rule test { \
+        condition: \
+          hash.md5(0, filesize) == \
+            \"ab56b4d92b40713acc5af89985d4b786\" \
+            and \
+          hash.md5(1, filesize) == \
+            \"e02cfbe5502b64aa5ae9f2d0d69eaa8d\" \
+            and \
+          hash.sha1(0, filesize) == \
+            \"03de6c570bfe24bfc328ccd7ca46b76eadaf4334\" \
+            and \
+          hash.sha1(1, filesize) == \
+            \"a302d65ae4d9e768a1538d53605f203fd8e2d6e2\" \
+            and \
+          hash.sha256(0, filesize) == \
+            \"36bbe50ed96841d10443bcb670d6554f0a34b761be67ec9c4a8ad2c0c44ca42c\" \
+            and \
+          hash.sha256(1, filesize) == \
+            \"aaaaf2863e043b9df604158ad5c16ff1adaf3fd7e9fcea5dcb322b6762b3b59a\" \
+      }",
+      blob);
+
+  // Test hash caching mechanism
+
+  assert_true_rule_blob(
+      "import \"hash\" \
+       rule test { \
+        condition: \
+          hash.md5(0, filesize) == \
+            \"ab56b4d92b40713acc5af89985d4b786\" \
+            and \
+          hash.md5(1, filesize) == \
+            \"e02cfbe5502b64aa5ae9f2d0d69eaa8d\" \
+            and \
+          hash.md5(0, filesize) == \
+            \"ab56b4d92b40713acc5af89985d4b786\" \
+            and \
+          hash.md5(1, filesize) == \
+            \"e02cfbe5502b64aa5ae9f2d0d69eaa8d\" \
+      }",
+      blob);
+}
+
+
 void test_integer_functions()
 {
   assert_true_rule(
@@ -1322,6 +1372,7 @@ int main(int argc, char** argv)
   // test_compare();
   test_comments();
   test_modules();
+  test_hash_module();
   test_integer_functions();
   // test_string_io();
   test_entrypoint();
