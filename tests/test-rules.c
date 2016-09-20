4705c87f5e9d48d6c12381e8ec8f56bbb9b7594d
Hilko Bengen <hillu@users.noreply.github.com>
2016-09-20 18:02:27 +0200

    Make yr_lex_parse_rules_fd compatible with Win32 (#520)
    Close: #519

--- tests/test-rules.c
+++ tests/test-rules.c
@@ -31,7 +31,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "blob.h"
 #include "util.h"
 
+#if defined(_WIN32) || defined(__CYGWIN__)
+#include <fileapi.h>
+#else
 #include <unistd.h>
+#endif
 #include <fcntl.h>
 
 static void test_boolean_operators()
@@ -1327,11 +1331,21 @@ void test_file_descriptor()
   YR_COMPILER* compiler = NULL;
   YR_RULES* rules = NULL;
   
+#if defined(_WIN32) || defined(__CYGWIN__)
+  HANDLE fd = CreateFile("tests/data/true.yar", GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+  if (fd == INVALID_HANDLE_VALUE)
+  {
+    fputs("CreateFile failed", stderr);
+    exit(1);
+  }
+#else
   int fd = open("tests/data/true.yar", O_RDONLY);
-  if (fd < 0) {
+  if (fd < 0)
+  {
     perror("open");
     exit(EXIT_FAILURE);
   }
+#endif
   if (yr_compiler_create(&compiler) != ERROR_SUCCESS)
   {
     perror("yr_compiler_create");
@@ -1343,7 +1357,11 @@ void test_file_descriptor()
     exit(EXIT_FAILURE);
   }
   
+#if defined(_WIN32) || defined(__CYGWIN__)
+  CloseHandle(fd);
+#else
   close(fd);
+#endif
 
   if (yr_compiler_get_rules(compiler, &rules) != ERROR_SUCCESS) {
     perror("yr_compiler_add_fd");
