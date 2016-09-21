94249fc2e2436e66c680446df4bac021929f3a45
plusvic <plusvic@gmail.com>
2016-09-21 10:04:54 +0200

    Other minor fixes in pe_utils.c

--- libyara/modules/pe_utils.c
+++ libyara/modules/pe_utils.c
@@ -1,9 +1,48 @@
+/*
+Copyright (c) 2014-2015. The YARA Authors. All Rights Reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdio.h>
+
+#if defined(WIN32)
 #include <string.h>
+#define strncasecmp _strnicmp
+#define timegm _mkgmtime
+#else
 #include <strings.h>
+#endif
+
+#include <string.h>
 
 #include <yara/utils.h>
 #include <yara/strutils.h>
 #include <yara/mem.h>
+#include <yara/integers.h>
 #include <yara/pe_utils.h>
 #include <yara/pe.h>
 
@@ -89,12 +128,6 @@ PIMAGE_DATA_DIRECTORY pe_get_directory_entry(
 
   return result;
 }
-
-
-#define OptionalHeader(pe,field)                \
-  (IS_64BITS_PE(pe) ?                           \
-   pe->header64->OptionalHeader.field :         \
-   pe->header->OptionalHeader.field)
 
 
 int64_t pe_rva_to_offset(
@@ -188,17 +221,6 @@ int64_t pe_rva_to_offset(
   return result;
 }
 
-
-#include <stdio.h>
-
-#include <yara/mem.h>
-#include <yara/integers.h>
-
-#if defined(WIN32)
-#include <string.h>
-#define strncasecmp _strnicmp
-#define timegm _mkgmtime
-#endif
 
 #if !HAVE_TIMEGM && !defined(WIN32)
 
