e43ca52f434ef7698aee99dcf8ab7a6756c3214b
Hilko Bengen <hillu@users.noreply.github.com>
2016-09-23 17:31:05 +0200

    pe: Add checksum field, function to calculate the checksum (#528)
    * Add pe.checksum field
    
    * Add pe.calculate_checksum function
    
    * Add documentation for pe.checksum, pe.calculate_checksum
    
    * Add tests for pe.checksum, pe.calculate_checksum

--- tests/test-pe.c
+++ tests/test-pe.c
@@ -14,6 +14,15 @@ int main(int argc, char** argv)
   assert_false_rule_file("import \"pe\" rule test { condition: pe.imports(\"KERNEL32.dll\", \"DeleteCriticalSection\") }",
       "tests/data/tiny-idata-5200");
 
+  assert_true_rule_file("import \"pe\" rule test { condition: pe.checksum == 0xA8DC }",
+      "tests/data/tiny");
+
+  assert_true_rule_file("import \"pe\" rule test { condition: pe.checksum == pe.calculate_checksum() }",
+      "tests/data/tiny");
+
+  assert_false_rule_file("import \"pe\" rule test { condition: pe.checksum == pe.calculate_checksum() }",
+      "tests/data/tiny-idata-51ff");
+
   yr_finalize();
   return 0;
 }
