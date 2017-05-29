4f77003e92519ad951209194cc9afb054660d45d
Victor M. Alvarez <plusvic@gmail.com>
2017-05-29 16:56:39 +0200

    Increase RE_MAX_AST_LEVELS from 1000 to 2000.

--- libyara/include/yara/limits.h
+++ libyara/include/yara/limits.h
@@ -83,6 +83,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define RE_MAX_FIBERS                   1024
 
 // Maximum number of levels in regexp's AST
-#define RE_MAX_AST_LEVELS               1000
+#define RE_MAX_AST_LEVELS               2000
 
 #endif
