06a508f6158f847a97f432c0e3dfcd31556b59ff
Antoine <ant1@users.noreply.github.com>
2017-06-07 11:48:29 +0000

    Fix build with LibreSSL

--- libyara/modules/pe.c
+++ libyara/modules/pe.c
@@ -41,7 +41,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <openssl/bio.h>
 #include <openssl/pkcs7.h>
 #include <openssl/x509.h>
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 #define X509_get_signature_nid(o) OBJ_obj2nid((o)->sig_alg->algorithm)
 #endif
 #endif
