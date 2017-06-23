2517aed3f33d201b55ab75d2fb0623891121d9df
Hilko Bengen <bengen@hilluzination.de>
2017-06-23 14:38:25 +0200

    Implement octal number literals

--- tests/test-rules.c
+++ tests/test-rules.c
@@ -192,6 +192,15 @@ static void test_arithmetic_operators()
   assert_true_rule(
       "rule test { condition: -0x01 == -1}", NULL);
 
+  assert_true_rule(
+      "rule test { condition: 0o10 == 8 }", NULL);
+
+  assert_true_rule(
+      "rule test { condition: 0o100 == 64 }", NULL);
+
+  assert_true_rule(
+      "rule test { condition: 0o755 == 493 }", NULL);
+
 }
 
 
