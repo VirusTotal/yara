38afd82de875b4709f7667ce35e5ba735d80b3d3
Rastislav Masaryk <rmasaryk@eset.sk>
2017-07-14 14:20:53 +0200

    json supports nested objects

--- libyara/modules/json.c
+++ libyara/modules/json.c
@@ -7,16 +7,15 @@
 
 define_function(has_key) {
   json_t* json = (json_t*)module()->data;
-  if (json == NULL) {
-  return_integer(UNDEFINED);
-  }
+  if (json == NULL)
+    return_integer(UNDEFINED);
 
   char* key = string_argument(1);
   json_t* json_value = json_object_get(json, key);
 
-  if (json_value == NULL) {
+  if (json_value == NULL)
     return_integer(0);
-  }
+  
   return_integer(1);
 }
 
@@ -37,6 +36,40 @@ int module_finalize(YR_MODULE* module) {
   return ERROR_SUCCESS;
 }
 
+void dump_json(json_t* json, YR_OBJECT* module_object, const char* root) {
+	const char* key;
+	json_t* value;
+
+	json_object_foreach(json, key, value) {
+		char* json_val = json_dumps(value, JSON_ENCODE_ANY);
+		char* to_free = json_val;
+		
+		char* new_root;
+		if (root) {
+			new_root = (char*)malloc(strlen(root) + strlen(key) + 2);
+			sprintf(new_root, "%s/%s", root, key);
+		}
+		else {
+			new_root = (char*)key;
+		}
+
+		// remove leading and trailing " from string
+		int len = strlen(json_val);
+		if ((len > 1) && (json_val[0] == '"') && (json_val[len - 1] == '"')) {
+			json_val[len - 1] = '\0';
+			json_val += 1;
+		}
+
+		set_string(json_val, module_object, "keys[%s]", new_root);
+		free(to_free);
+		if (root != NULL)
+			free(new_root);
+
+		if (json_is_object(value))
+			dump_json(value, module_object, new_root);
+	}
+}
+
 
 int module_load(YR_SCAN_CONTEXT* context, YR_OBJECT* module_object, void* module_data, size_t module_data_size) {
   YR_MEMORY_BLOCK* block = first_memory_block(context);
@@ -46,34 +79,16 @@ int module_load(YR_SCAN_CONTEXT* context, YR_OBJECT* module_object, void* module
   json_t* json = json_loads((const char*) block_data, 0, &json_error);
   module_object->data = json;
 
-  if (json) {
-  const char* key;
-  json_t* value;
-  
-  json_object_foreach(json, key, value) {
-    char* json_val = json_dumps(value, JSON_ENCODE_ANY);
-    char* to_free = json_val;
-
-    // remove leading and trailing " from string
-    int len = strlen(json_val);
-    if ((len > 1) && (json_val[0] == '"') && (json_val[len - 1] == '"')) {
-    json_val[len - 1] = '\0';
-    json_val += 1;
-    }
-
-    set_string(json_val, module_object, "keys[%s]", key);
-    free(to_free);
-  }
-  }
+  if (json)
+	  dump_json(json, module_object, NULL);
 
   return ERROR_SUCCESS;
 }
 
 
 int module_unload(YR_OBJECT* module_object) {
-  if (module_object->data) {
-  json_decref((json_t*)module_object->data);
-  }
+  if (module_object->data)
+    json_decref((json_t*)module_object->data);
   
   return ERROR_SUCCESS;
 }
