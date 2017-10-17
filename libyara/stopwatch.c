a82c0a458ccdcbde1f286acbb241834f8b90c559
Victor M. Alvarez <vmalvarez@virustotal.com>
2017-10-17 15:02:27 +0200

    Replace tabs with spaces.

--- libyara/stopwatch.c
+++ libyara/stopwatch.c
@@ -32,14 +32,14 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <yara/stopwatch.h>
 
 
-#define	timespecsub(tsp, usp, vsp)					\
-do {								\
-  (vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;		\
-  (vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;	\
-  if ((vsp)->tv_nsec < 0) {				\
-    (vsp)->tv_sec--;				\
-    (vsp)->tv_nsec += 1000000000L;			\
-  }							\
+#define timespecsub(tsp, usp, vsp)                      \
+do {                                                    \
+  (vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;        \
+  (vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;     \
+  if ((vsp)->tv_nsec < 0) {                             \
+    (vsp)->tv_sec--;                                    \
+    (vsp)->tv_nsec += 1000000000L;                      \
+  }                                                     \
 } while (0)
 
 
