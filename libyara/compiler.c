VirusTotal/yara

commit c0fa0bf760fe2fe82a7216daed34feb2c4641307

Edhoedt callback on include (#749)
* Full structure implemented, callback available. TODO: actually use the callback's result to import rules files.

* Importing files via external callback fully implemented and working

* Removing useless comments

* Adding documentation

* Updating doc for include callbacks

* Fixing compiler_set_include_callback's signature

* Fixing issues according to @plusvic 's advice
 * Limited lines to 80 chars
 * include callback is now using its own user_data and does not conflict with other callbacks
 * Copying include callback result to the stack before pushing it into the flex scanner
 * yara's memory management functions are now exposed through the API
 * Making the lexer reponsible for freeing the memory allocated by the include callback
 * Updating docs accordingly

* * fixing memory management issues in callbacks
* adding a callback to allow external code to free the memory used to return the include callback's result
* API: un-exposing yara's memory management functions
* making include callbacks the only way ton include files
* adding a default include callback to open files from the disk (mimicking original behavior)
* updating docs accordingly

* fixing missing pointer cast

* re-generating lexer.c

* removing imports ununsed since commit 92877db48e408126ce0631b85c03d47899ea5623

* fixing default file read callback

* Fixing line numbers in errors

* Fix some issues related to includes callback.

* Remove allow_includes field from YR_COMPILER. Includes now can be disallowed by setting the include callback to NULL.
* Fix memory and file handle leaks.
* Calculate size of error message buffer correctly.
* Add test case.

libyara/compiler.c
@@ -56,14 +56,14 @@ YR_API int yr_compiler_create(
 
   new_compiler->errors = 0;
   new_compiler->callback = NULL;
+  new_compiler->include_callback = _yr_compiler_default_include_callback;
+  new_compiler->include_free = _yr_compiler_default_include_free;
   new_compiler->last_error = ERROR_SUCCESS;
   new_compiler->last_error_line = 0;
   new_compiler->current_line = 0;
   new_compiler->last_result = ERROR_SUCCESS;
-  new_compiler->file_stack_ptr = 0;
   new_compiler->file_name_stack_ptr = 0;
   new_compiler->fixup_stack_head = NULL;
-  new_compiler->allow_includes = 1;
   new_compiler->loop_depth = 0;
   new_compiler->loop_for_of_mem_offset = -1;
   new_compiler->compiled_rules_arena = NULL;
@@ -182,37 +182,125 @@ YR_API void yr_compiler_set_callback(
 }
 
 
-int _yr_compiler_push_file(
-    YR_COMPILER* compiler,
-    FILE* fh)
+const char* _yr_compiler_default_include_callback(
+    const char* include_name,
+    const char* calling_rule_filename,
+    const char* calling_rule_namespace,
+    void* user_data)
 {
-  if (compiler->file_stack_ptr < MAX_INCLUDE_DEPTH)
+  char* buffer;
+  char* s = NULL;
+  #ifdef _WIN32
+  char* b = NULL;
+  #endif
+  char* f;
+  FILE* fh;
+  char* file_buffer;
+  size_t file_length;
+
+  if (calling_rule_filename != NULL)
   {
-    compiler->file_stack[compiler->file_stack_ptr] = fh;
-    compiler->file_stack_ptr++;
-    return ERROR_SUCCESS;
+    buffer = (char*) calling_rule_filename;
   }
   else
   {
-    compiler->last_result = ERROR_INCLUDE_DEPTH_EXCEEDED;
-    return ERROR_INCLUDE_DEPTH_EXCEEDED;
+    buffer = "\0";
   }
-}
 
+  s = strrchr(buffer, '/');
 
-FILE* _yr_compiler_pop_file(
-    YR_COMPILER* compiler)
-{
-  FILE* result = NULL;
+  #ifdef _WIN32
+  b = strrchr(buffer, '\\'); // in Windows both path delimiters are accepted
+  #endif
 
-  if (compiler->file_stack_ptr > 0)
+  #ifdef _WIN32
+  if (s != NULL || b != NULL)
+  #else
+  if (s != NULL)
+  #endif
   {
-    compiler->file_stack_ptr--;
-    result = compiler->file_stack[compiler->file_stack_ptr];
+    #ifdef _WIN32
+    f = (b > s)? (b + 1): (s + 1);
+    #else
+    f = s + 1;
+    #endif
+
+    strlcpy(f, include_name, sizeof(buffer) - (f - buffer));
+
+    f = buffer;
+    // SECURITY: Potential for directory traversal here.
+    fh = fopen(buffer, "rb");
+
+    // if include file was not found relative to current source file,
+    // try to open it with path as specified by user (maybe user wrote
+    // a full path)
+    if (fh == NULL)
+    {
+      f = (char*) include_name;
+      // SECURITY: Potential for directory traversal here.
+      fh = fopen(include_name, "rb");
+    }
+  }
+  else
+  {
+    f = (char*) include_name;
+    // SECURITY: Potential for directory traversal here.
+    fh = fopen(include_name, "rb");
   }
 
-  return result;
+  if (fh == NULL)
+    return NULL;
+
+  fseek(fh, 0, SEEK_END);
+  file_length = ftell(fh);
+  fseek(fh, 0, SEEK_SET);
+
+  file_buffer = (char*) yr_malloc(file_length + 1);
+
+  if (file_buffer == NULL)
+  {
+    fclose(fh);
+    return NULL;
+  }
+
+  if (file_length != fread(file_buffer, 1, file_length, fh))
+  {
+    yr_free(file_buffer);
+    fclose(fh);
+    return NULL;
+  }
+  else
+  {
+    file_buffer[file_length]='\0';
+  }
+
+  fclose(fh);
+  return file_buffer;
 }
+
+
+void _yr_compiler_default_include_free(
+  const char* callback_result_ptr,
+  void* user_data)
+{
+  if(callback_result_ptr != NULL)
+  {
+    yr_free((void*)callback_result_ptr);
+  }
+}
+
+
+YR_API void yr_compiler_set_include_callback(
+    YR_COMPILER* compiler,
+    YR_COMPILER_INCLUDE_CALLBACK_FUNC include_callback,
+    YR_COMPILER_INCLUDE_FREE_FUNC include_free,
+    void* user_data)
+{
+  compiler->include_callback = include_callback;
+  compiler->include_free = include_free;
+  compiler->incl_clbk_user_data = user_data;
+}
+
 
 int _yr_compiler_push_file_name(
     YR_COMPILER* compiler,

libyara/include/yara/compiler.h
@@ -52,6 +52,17 @@ typedef void (*YR_COMPILER_CALLBACK_FUNC)(
     void* user_data);
 
 
+typedef const char* (*YR_COMPILER_INCLUDE_CALLBACK_FUNC)(
+    const char* include_name,
+    const char* calling_rule_filename,
+    const char* calling_rule_namespace,
+    void* user_data);
+
+typedef void (*YR_COMPILER_INCLUDE_FREE_FUNC)(
+    const char* callback_result_ptr,
+    void* user_data );
+
+
 typedef struct _YR_FIXUP
 {
   void* address;
@@ -98,13 +109,8 @@ typedef struct _YR_COMPILER
   int               loop_depth;
   int               loop_for_of_mem_offset;
 
-  int               allow_includes;
-
   char*             file_name_stack[MAX_INCLUDE_DEPTH];
   int               file_name_stack_ptr;
-
-  FILE*             file_stack[MAX_INCLUDE_DEPTH];
-  int               file_stack_ptr;
 
   char              last_error_extra_info[MAX_COMPILER_ERROR_EXTRA_INFO];
 
@@ -114,8 +120,12 @@ typedef struct _YR_COMPILER
 
   char              include_base_dir[MAX_PATH];
   void*             user_data;
+  void*             incl_clbk_user_data;
 
   YR_COMPILER_CALLBACK_FUNC  callback;
+  YR_COMPILER_INCLUDE_CALLBACK_FUNC include_callback;
+  YR_COMPILER_INCLUDE_FREE_FUNC include_free;
+
 
 } YR_COMPILER;
 
@@ -134,14 +144,6 @@ typedef struct _YR_COMPILER
         fmt, __VA_ARGS__);
 
 
-int _yr_compiler_push_file(
-    YR_COMPILER* compiler,
-    FILE* fh);
-
-
-FILE* _yr_compiler_pop_file(
-    YR_COMPILER* compiler);
-
 
 int _yr_compiler_push_file_name(
     YR_COMPILER* compiler,
@@ -151,6 +153,15 @@ int _yr_compiler_push_file_name(
 void _yr_compiler_pop_file_name(
     YR_COMPILER* compiler);
 
+const char* _yr_compiler_default_include_callback(
+    const char* include_name,
+    const char* calling_rule_filename,
+    const char* calling_rule_namespace,
+    void* user_data);
+
+void _yr_compiler_default_include_free(
+    const char* callback_result_ptr,
+    void* user_data);
 
 YR_API int yr_compiler_create(
     YR_COMPILER** compiler);
@@ -163,6 +174,13 @@ YR_API void yr_compiler_destroy(
 YR_API void yr_compiler_set_callback(
     YR_COMPILER* compiler,
     YR_COMPILER_CALLBACK_FUNC callback,
+    void* user_data);
+
+
+YR_API void yr_compiler_set_include_callback(
+    YR_COMPILER* compiler,
+    YR_COMPILER_INCLUDE_CALLBACK_FUNC include_callback,
+    YR_COMPILER_INCLUDE_FREE_FUNC include_free,
     void* user_data);
 
 

libyara/lexer.c
@@ -1354,80 +1354,25 @@ YY_RULE_SETUP
 #line 188 "lexer.l"
 {
 
-  char            buffer[1024];
-  char            *current_file_name;
-  char            *s = NULL;
-  #ifdef _WIN32
-  char            *b = NULL;
-  #endif
-  char            *f;
-  FILE*           fh;
-
   if (compiler->allow_includes)
   {
     *yyextra->lex_buf_ptr = '\0'; // null-terminate included file path
 
     // move path of current source file into buffer
-    current_file_name = yr_compiler_get_current_file_name(compiler);
+    char* current_file_name = yr_compiler_get_current_file_name(
+                                                          compiler);
 
-    if (current_file_name != NULL)
+    const char* res = compiler->include_callback(
+                                  yyextra->lex_buf,
+                                  current_file_name,
+                                  compiler->current_namespace->name,
+                                  compiler->incl_clbk_user_data);
+
+    if (res != NULL)
     {
-      strlcpy(buffer, current_file_name, sizeof(buffer));
-    }
-    else
-    {
-      buffer[0] = '\0';
-    }
 
-    // make included file path relative to current source file
-    s = strrchr(buffer, '/');
-
-    #ifdef _WIN32
-    b = strrchr(buffer, '\\'); // in Windows both path delimiters are accepted
-    #endif
-
-    #ifdef _WIN32
-    if (s != NULL || b != NULL)
-    #else
-    if (s != NULL)
-    #endif
-    {
-      #ifdef _WIN32
-      f = (b > s)? (b + 1): (s + 1);
-      #else
-      f = s + 1;
-      #endif
-
-      strlcpy(f, yyextra->lex_buf, sizeof(buffer) - (f - buffer));
-
-      f = buffer;
-
-      // SECURITY: Potential for directory traversal here.
-      fh = fopen(buffer, "r");
-
-      // if include file was not found relative to current source file,
-      // try to open it with path as specified by user (maybe user wrote
-      // a full path)
-
-      if (fh == NULL)
-      {
-        f = yyextra->lex_buf;
-
-        // SECURITY: Potential for directory traversal here.
-        fh = fopen(yyextra->lex_buf, "r");
-      }
-    }
-    else
-    {
-      f = yyextra->lex_buf;
-
-      // SECURITY: Potential for directory traversal here.
-      fh = fopen(yyextra->lex_buf, "r");
-    }
-
-    if (fh != NULL)
-    {
-      int error_code = _yr_compiler_push_file_name(compiler, f);
+      int error_code = _yr_compiler_push_file_name( compiler,
+                                                    yyextra->lex_buf);
 
       if (error_code != ERROR_SUCCESS)
       {
@@ -1439,19 +1384,42 @@ YY_RULE_SETUP
         {
           yyerror(yyscanner, compiler, "includes depth exceeded");
         }
-
+        if(compiler->include_free != NULL)
+        {
+          compiler->include_free(res, compiler->incl_clbk_user_data);
+        }
         yyterminate();
       }
 
-      _yr_compiler_push_file(compiler, fh);
-      yara_yypush_buffer_state(yara_yy_create_buffer(fh,YY_BUF_SIZE,yyscanner),yyscanner);
+      // Workaround for flex issue: https://github.com/westes/flex/issues/58
+      yara_yypush_buffer_state(YY_CURRENT_BUFFER,yyscanner);
+      yara_yy_scan_string(res,yyscanner);
+      yara_yyset_lineno(1,yyscanner);
+      if(compiler->include_free != NULL)
+      {
+        compiler->include_free(res, compiler->incl_clbk_user_data);
+      }
     }
     else
     {
-      snprintf(buffer, sizeof(buffer),
-               "can't open include file: %s", yyextra->lex_buf);
-      yyerror(yyscanner, compiler, buffer);
+      char* base_err_msg = NULL;
+      if(compiler->include_callback == _yr_compiler_default_include_callback)
+      {
+        base_err_msg = "can't open include file: %s";
+      }
+      else
+      {
+        base_err_msg = "callback failed to provide include resource: %s";
+      }
+      size_t err_buff_len = sizeof(base_err_msg) + sizeof(yyextra->lex_buf);
+      char* error_buffer = (char*) yr_malloc(err_buff_len);
+      snprintf( error_buffer,
+                err_buff_len,
+                base_err_msg,
+                yyextra->lex_buf);
+      yyerror(yyscanner, compiler, error_buffer);
     }
+
   }
   else // not allowing includes
   {
@@ -1467,16 +1435,10 @@ case YY_STATE_EOF(str):
 case YY_STATE_EOF(regexp):
 case YY_STATE_EOF(include):
 case YY_STATE_EOF(comment):
-#line 300 "lexer.l"
+#line 267 "lexer.l"
 {
 
   YR_COMPILER* compiler = yara_yyget_extra(yyscanner);
-  FILE* file = _yr_compiler_pop_file(compiler);
-
-  if (file != NULL)
-  {
-    fclose(file);
-  }
 
   _yr_compiler_pop_file_name(compiler);
   yara_yypop_buffer_state(yyscanner);
@@ -1485,11 +1447,12 @@ case YY_STATE_EOF(comment):
   {
     yyterminate();
   }
+
 }
 	YY_BREAK
 case 44:
 YY_RULE_SETUP
-#line 320 "lexer.l"
+#line 282 "lexer.l"
 {
 
   yylval->c_string = yr_strdup(yytext);
@@ -1505,7 +1468,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 45:
 YY_RULE_SETUP
-#line 334 "lexer.l"
+#line 296 "lexer.l"
 {
 
   yylval->c_string = yr_strdup(yytext);
@@ -1521,7 +1484,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 46:
 YY_RULE_SETUP
-#line 348 "lexer.l"
+#line 310 "lexer.l"
 {
 
   yylval->c_string = yr_strdup(yytext);
@@ -1538,7 +1501,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 47:
 YY_RULE_SETUP
-#line 363 "lexer.l"
+#line 325 "lexer.l"
 {
 
   yylval->c_string = yr_strdup(yytext);
@@ -1555,7 +1518,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 48:
 YY_RULE_SETUP
-#line 378 "lexer.l"
+#line 340 "lexer.l"
 {
 
   yylval->c_string = yr_strdup(yytext);
@@ -1572,7 +1535,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 49:
 YY_RULE_SETUP
-#line 393 "lexer.l"
+#line 355 "lexer.l"
 {
 
   char* text = yytext;
@@ -1613,7 +1576,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 50:
 YY_RULE_SETUP
-#line 432 "lexer.l"
+#line 394 "lexer.l"
 {
 
   if (strlen(yytext) > 128)
@@ -1634,7 +1597,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 51:
 YY_RULE_SETUP
-#line 451 "lexer.l"
+#line 413 "lexer.l"
 {
 
   #ifdef _MSC_VER
@@ -1656,7 +1619,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 52:
 YY_RULE_SETUP
-#line 470 "lexer.l"
+#line 432 "lexer.l"
 {
   yylval->double_ = atof(yytext);
   return _DOUBLE_;
@@ -1664,7 +1627,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 53:
 YY_RULE_SETUP
-#line 475 "lexer.l"
+#line 437 "lexer.l"
 {
   yylval->integer = xtoi(yytext + 2);
   return _NUMBER_;
@@ -1672,7 +1635,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 54:
 YY_RULE_SETUP
-#line 480 "lexer.l"
+#line 442 "lexer.l"
 {
   yylval->integer = otoi(yytext + 2);
   return _NUMBER_;
@@ -1680,7 +1643,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 55:
 YY_RULE_SETUP
-#line 486 "lexer.l"
+#line 448 "lexer.l"
 {     /* saw closing quote - all done */
 
   ALLOC_SIZED_STRING(s, yyextra->lex_buf_len);
@@ -1696,7 +1659,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 56:
 YY_RULE_SETUP
-#line 500 "lexer.l"
+#line 462 "lexer.l"
 {
 
   LEX_CHECK_SPACE_OK("\t", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1706,7 +1669,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 57:
 YY_RULE_SETUP
-#line 508 "lexer.l"
+#line 470 "lexer.l"
 {
 
   LEX_CHECK_SPACE_OK("\n", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1716,7 +1679,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 58:
 YY_RULE_SETUP
-#line 516 "lexer.l"
+#line 478 "lexer.l"
 {
 
   LEX_CHECK_SPACE_OK("\"", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1726,7 +1689,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 59:
 YY_RULE_SETUP
-#line 524 "lexer.l"
+#line 486 "lexer.l"
 {
 
   LEX_CHECK_SPACE_OK("\\", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1736,7 +1699,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 60:
 YY_RULE_SETUP
-#line 532 "lexer.l"
+#line 494 "lexer.l"
 {
 
    int result;
@@ -1749,13 +1712,13 @@ YY_RULE_SETUP
 	YY_BREAK
 case 61:
 YY_RULE_SETUP
-#line 543 "lexer.l"
+#line 505 "lexer.l"
 { YYTEXT_TO_BUFFER; }
 	YY_BREAK
 case 62:
 /* rule 62 can match eol */
 YY_RULE_SETUP
-#line 546 "lexer.l"
+#line 508 "lexer.l"
 {
 
   yyerror(yyscanner, compiler, "unterminated string");
@@ -1765,7 +1728,7 @@ YY_RULE_SETUP
 case 63:
 /* rule 63 can match eol */
 YY_RULE_SETUP
-#line 552 "lexer.l"
+#line 514 "lexer.l"
 {
 
   yyerror(yyscanner, compiler, "illegal escape sequence");
@@ -1774,7 +1737,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 64:
 YY_RULE_SETUP
-#line 559 "lexer.l"
+#line 521 "lexer.l"
 {
 
   if (yyextra->lex_buf_len > 0)
@@ -1803,7 +1766,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 65:
 YY_RULE_SETUP
-#line 586 "lexer.l"
+#line 548 "lexer.l"
 {
 
   LEX_CHECK_SPACE_OK("/", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1813,7 +1776,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 66:
 YY_RULE_SETUP
-#line 594 "lexer.l"
+#line 556 "lexer.l"
 {
 
   LEX_CHECK_SPACE_OK("\\.", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1831,13 +1794,13 @@ YY_RULE_SETUP
 	YY_BREAK
 case 67:
 YY_RULE_SETUP
-#line 610 "lexer.l"
+#line 572 "lexer.l"
 { YYTEXT_TO_BUFFER; }
 	YY_BREAK
 case 68:
 /* rule 68 can match eol */
 YY_RULE_SETUP
-#line 613 "lexer.l"
+#line 575 "lexer.l"
 {
 
   yyerror(yyscanner, compiler, "unterminated regular expression");
@@ -1846,7 +1809,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 69:
 YY_RULE_SETUP
-#line 620 "lexer.l"
+#line 582 "lexer.l"
 {
 
   yyextra->lex_buf_ptr = yyextra->lex_buf;
@@ -1856,7 +1819,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 70:
 YY_RULE_SETUP
-#line 628 "lexer.l"
+#line 590 "lexer.l"
 {
 
   yyextra->lex_buf_ptr = yyextra->lex_buf;
@@ -1867,7 +1830,7 @@ YY_RULE_SETUP
 case 71:
 /* rule 71 can match eol */
 YY_RULE_SETUP
-#line 636 "lexer.l"
+#line 598 "lexer.l"
 {
   // Match hex-digits with whitespace or comments. The latter are stripped
   // out by hex_lexer.l
@@ -1883,12 +1846,12 @@ YY_RULE_SETUP
 case 72:
 /* rule 72 can match eol */
 YY_RULE_SETUP
-#line 649 "lexer.l"
+#line 611 "lexer.l"
 /* skip whitespace */
 	YY_BREAK
 case 73:
 YY_RULE_SETUP
-#line 651 "lexer.l"
+#line 613 "lexer.l"
 {
 
   if (yytext[0] >= 32 && yytext[0] < 127)
@@ -1904,10 +1867,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 74:
 YY_RULE_SETUP
-#line 664 "lexer.l"
+#line 626 "lexer.l"
 ECHO;
 	YY_BREAK
-#line 1911 "lexer.c"
+#line 1874 "lexer.c"
 
 	case YY_END_OF_BUFFER:
 		{
@@ -3056,7 +3019,7 @@ void yara_yyfree (void * ptr , yyscan_t yyscanner)
 
 #define YYTABLES_NAME "yytables"
 
-#line 664 "lexer.l"
+#line 626 "lexer.l"
 
 
 

libyara/lexer.l
@@ -187,80 +187,22 @@ include[ \t]+\"         {
 
 <include>\"             {
 
-  char            buffer[1024];
-  char            *current_file_name;
-  char            *s = NULL;
-  #ifdef _WIN32
-  char            *b = NULL;
-  #endif
-  char            *f;
-  FILE*           fh;
-
-  if (compiler->allow_includes)
+  if (compiler->include_callback != NULL)
   {
     *yyextra->lex_buf_ptr = '\0'; // null-terminate included file path
 
     // move path of current source file into buffer
-    current_file_name = yr_compiler_get_current_file_name(compiler);
+    char* current_file_name = yr_compiler_get_current_file_name(compiler);
 
-    if (current_file_name != NULL)
+    const char* included_rules = compiler->include_callback(
+        yyextra->lex_buf,
+        current_file_name,
+        compiler->current_namespace->name,
+        compiler->incl_clbk_user_data);
+
+    if (included_rules != NULL)
     {
-      strlcpy(buffer, current_file_name, sizeof(buffer));
-    }
-    else
-    {
-      buffer[0] = '\0';
-    }
-
-    // make included file path relative to current source file
-    s = strrchr(buffer, '/');
-
-    #ifdef _WIN32
-    b = strrchr(buffer, '\\'); // in Windows both path delimiters are accepted
-    #endif
-
-    #ifdef _WIN32
-    if (s != NULL || b != NULL)
-    #else
-    if (s != NULL)
-    #endif
-    {
-      #ifdef _WIN32
-      f = (b > s)? (b + 1): (s + 1);
-      #else
-      f = s + 1;
-      #endif
-
-      strlcpy(f, yyextra->lex_buf, sizeof(buffer) - (f - buffer));
-
-      f = buffer;
-
-      // SECURITY: Potential for directory traversal here.
-      fh = fopen(buffer, "r");
-
-      // if include file was not found relative to current source file,
-      // try to open it with path as specified by user (maybe user wrote
-      // a full path)
-
-      if (fh == NULL)
-      {
-        f = yyextra->lex_buf;
-
-        // SECURITY: Potential for directory traversal here.
-        fh = fopen(yyextra->lex_buf, "r");
-      }
-    }
-    else
-    {
-      f = yyextra->lex_buf;
-
-      // SECURITY: Potential for directory traversal here.
-      fh = fopen(yyextra->lex_buf, "r");
-    }
-
-    if (fh != NULL)
-    {
-      int error_code = _yr_compiler_push_file_name(compiler, f);
+      int error_code = _yr_compiler_push_file_name(compiler, yyextra->lex_buf);
 
       if (error_code != ERROR_SUCCESS)
       {
@@ -273,24 +215,53 @@ include[ \t]+\"         {
           yyerror(yyscanner, compiler, "includes depth exceeded");
         }
 
+        if (compiler->include_free != NULL)
+        {
+          compiler->include_free(included_rules, compiler->incl_clbk_user_data);
+        }
+
         yyterminate();
       }
 
-      _yr_compiler_push_file(compiler, fh);
-      yypush_buffer_state(
-          yy_create_buffer(fh, YY_BUF_SIZE, yyscanner), yyscanner);
+      // Workaround for flex issue: https://github.com/westes/flex/issues/58
+      yypush_buffer_state(YY_CURRENT_BUFFER, yyscanner);
+      yy_scan_string(included_rules, yyscanner);
+      yyset_lineno(1, yyscanner);
+
+      if (compiler->include_free != NULL)
+      {
+        compiler->include_free(included_rules, compiler->incl_clbk_user_data);
+      }
     }
     else
     {
-      snprintf(buffer, sizeof(buffer),
-               "can't open include file: %s", yyextra->lex_buf);
-      yyerror(yyscanner, compiler, buffer);
+      char* base_err_msg = NULL;
+
+      if (compiler->include_callback == _yr_compiler_default_include_callback)
+      {
+        base_err_msg = "can't open include file: %s";
+      }
+      else
+      {
+        base_err_msg = "callback failed to provide include resource: %s";
+      }
+
+      size_t err_buff_len = strlen(base_err_msg) + strlen(yyextra->lex_buf);
+      char* error_buffer = (char*) yr_malloc(err_buff_len);
+
+      snprintf(
+          error_buffer,
+          err_buff_len,
+          base_err_msg,
+          yyextra->lex_buf);
+
+      yyerror(yyscanner, compiler, error_buffer);
     }
+
   }
   else // not allowing includes
   {
     yyerror(yyscanner, compiler, "includes are disabled");
-    yyterminate();
   }
 
   BEGIN(INITIAL);
@@ -300,12 +271,6 @@ include[ \t]+\"         {
 <<EOF>> {
 
   YR_COMPILER* compiler = yyget_extra(yyscanner);
-  FILE* file = _yr_compiler_pop_file(compiler);
-
-  if (file != NULL)
-  {
-    fclose(file);
-  }
 
   _yr_compiler_pop_file_name(compiler);
   yypop_buffer_state(yyscanner);
@@ -314,6 +279,7 @@ include[ \t]+\"         {
   {
     yyterminate();
   }
+
 }
 
 

tests/test-rules.c
@@ -1792,6 +1792,47 @@ void test_disabled_rules()
 }
 
 
+const char* _include_callback(
+    const char* include_name,
+    const char* calling_rule_filename,
+    const char* calling_rule_namespace,
+    void* user_data)
+{
+  if (strcmp(include_name, "ok") == 0)
+    return "rule test {condition: true}";
+  else
+    return NULL;
+}
+
+
+void test_include_callback()
+{
+  YR_COMPILER* compiler = NULL;
+
+  if (yr_compiler_create(&compiler) != ERROR_SUCCESS)
+  {
+    perror("yr_compiler_create");
+    exit(EXIT_FAILURE);
+  }
+
+  yr_compiler_set_include_callback(compiler, _include_callback, NULL, NULL);
+
+  // This include produces no error.
+  if (yr_compiler_add_string(compiler, "include \"ok\"", NULL) != 0)
+  {
+    yr_compiler_destroy(compiler);
+    exit(EXIT_FAILURE);
+  }
+
+  // This include one error.
+  if (yr_compiler_add_string(compiler, "include \"fail\"", NULL) != 1)
+  {
+    yr_compiler_destroy(compiler);
+    exit(EXIT_FAILURE);
+  }
+}
+
+
 int main(int argc, char** argv)
 {
   yr_initialize();
@@ -1815,6 +1856,7 @@ int main(int argc, char** argv)
   test_for();
   test_re();
   test_filesize();
+  test_include_callback();
   // test_compile_file();
   // test_compile_files();
   // test_include_files();

commit 88051cf7162277304d17b8a6778310d2856e8cc7

Fix bug introduced in #749.

libyara/compiler.c
@@ -188,7 +188,7 @@ const char* _yr_compiler_default_include_callback(
     const char* calling_rule_namespace,
     void* user_data)
 {
-  char* buffer;
+  char buffer[1024];
   char* s = NULL;
   #ifdef _WIN32
   char* b = NULL;
@@ -200,11 +200,11 @@ const char* _yr_compiler_default_include_callback(
 
   if (calling_rule_filename != NULL)
   {
-    buffer = (char*) calling_rule_filename;
+    strlcpy(buffer, calling_rule_filename, sizeof(buffer));
   }
   else
   {
-    buffer = "\0";
+    buffer[0] = '\0';
   }
 
   s = strrchr(buffer, '/');
@@ -277,7 +277,7 @@ const char* _yr_compiler_default_include_callback(
   }
   else
   {
-    file_buffer[file_length]='\0';
+    file_buffer[file_length] = '\0';
   }
 
   fclose(fh);