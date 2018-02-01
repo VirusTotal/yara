7a78c2ecbf74389cad124bba07ba92d935112b48
CaldurG <rastislav.masaryk@gmail.com>
2018-02-01 17:41:01 +0100

    Raise warning when .+ is used in a regular expression (#713)

--- libyara/parser.c
+++ libyara/parser.c
@@ -574,7 +574,7 @@ YR_STRING* yr_parser_reduce_string_declaration(
     {
       yywarning(
           yyscanner,
-          "%s contains .*, consider using .{N} with a reasonable value for N",
+          "%s contains .* or .+, consider using .{N} or .{1-N} with a reasonable value for N",
           identifier);
     }
 
