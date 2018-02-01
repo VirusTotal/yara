210350f7fea38de67638a67d2724842a1d81e42c
Victor M. Alvarez <vmalvarez@virustotal.com>
2018-02-01 18:07:32 +0100

    Minor re-styling.

--- libyara/re.c
+++ libyara/re.c
@@ -463,7 +463,8 @@ SIZED_STRING* yr_re_ast_extract_literal(
 int _yr_re_node_contains_dot_star(
     RE_NODE* re_node)
 {
-  if ((re_node->type == RE_NODE_STAR || re_node->type == RE_NODE_PLUS) && re_node->left->type == RE_NODE_ANY)
+  if ((re_node->type == RE_NODE_STAR || re_node->type == RE_NODE_PLUS) &&
+      re_node->left->type == RE_NODE_ANY)
     return TRUE;
 
   if (re_node->left != NULL && _yr_re_node_contains_dot_star(re_node->left))
