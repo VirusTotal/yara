638123fe09512f03fd93b3dba1fc5559c1ee8566
Victor M. Alvarez <vmalvarez@virustotal.com>
2018-02-13 17:41:22 +0100

    Increase ARENA_FILE_VERSION.
    Commit f35c29dc8caf6045bd74afeadf137836d524055e changed the layout of structures that make old files incompatible.

--- libyara/include/yara/arena.h
+++ libyara/include/yara/arena.h
@@ -47,7 +47,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // address within the relocated arena.
 #define ARENA_FLAGS_RELOCATABLE       2
 
-#define ARENA_FILE_VERSION       ((16 << 16) | MAX_THREADS)
+#define ARENA_FILE_VERSION       ((17 << 16) | MAX_THREADS)
 
 #define EOL ((size_t) -1)
 
