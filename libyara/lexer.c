VirusTotal/yara

commit 54059646ede198405c193f0f04d0ad1940fbb95f

Fix issue 884 (#887)
The yr_compiler_add_fd was reading only the first 1024 bytes of the file.

libyara/lexer.c
@@ -732,6 +732,7 @@ with noyywrap then we can remove this pragma.
 #if defined(_WIN32) || defined(__CYGWIN__)
 #include <windows.h>
 #else
+#include <sys/stat.h>
 #include <unistd.h>
 #endif
 
@@ -817,7 +818,7 @@ static bool is_absolute_path(
 
 
 
-#line 821 "lexer.c"
+#line 822 "lexer.c"
 
 #define INITIAL 0
 #define str 1
@@ -1052,10 +1053,10 @@ YY_DECL
 	register int yy_act;
     struct yyguts_t * yyg = (struct yyguts_t*)yyscanner;
 
-#line 162 "lexer.l"
+#line 163 "lexer.l"
 
 
-#line 1059 "lexer.c"
+#line 1060 "lexer.c"
 
     yylval = yylval_param;
 
@@ -1150,213 +1151,213 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 164 "lexer.l"
+#line 165 "lexer.l"
 { return _DOT_DOT_;     }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 165 "lexer.l"
+#line 166 "lexer.l"
 { return _LT_;          }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 166 "lexer.l"
+#line 167 "lexer.l"
 { return _GT_;          }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 167 "lexer.l"
+#line 168 "lexer.l"
 { return _LE_;          }
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 168 "lexer.l"
+#line 169 "lexer.l"
 { return _GE_;          }
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 169 "lexer.l"
+#line 170 "lexer.l"
 { return _EQ_;          }
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 170 "lexer.l"
+#line 171 "lexer.l"
 { return _NEQ_;         }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 171 "lexer.l"
+#line 172 "lexer.l"
 { return _SHIFT_LEFT_;  }
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 172 "lexer.l"
+#line 173 "lexer.l"
 { return _SHIFT_RIGHT_; }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 173 "lexer.l"
+#line 174 "lexer.l"
 { return _PRIVATE_;     }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 174 "lexer.l"
+#line 175 "lexer.l"
 { return _GLOBAL_;      }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 175 "lexer.l"
+#line 176 "lexer.l"
 { return _RULE_;        }
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 176 "lexer.l"
+#line 177 "lexer.l"
 { return _META_;        }
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 177 "lexer.l"
+#line 178 "lexer.l"
 { return _STRINGS_;     }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 178 "lexer.l"
+#line 179 "lexer.l"
 { return _ASCII_;       }
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 179 "lexer.l"
+#line 180 "lexer.l"
 { return _WIDE_;        }
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 180 "lexer.l"
+#line 181 "lexer.l"
 { return _XOR_;         }
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 181 "lexer.l"
+#line 182 "lexer.l"
 { return _FULLWORD_;    }
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 182 "lexer.l"
+#line 183 "lexer.l"
 { return _NOCASE_;      }
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 183 "lexer.l"
+#line 184 "lexer.l"
 { return _CONDITION_;   }
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 184 "lexer.l"
+#line 185 "lexer.l"
 { return _TRUE_;        }
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 185 "lexer.l"
+#line 186 "lexer.l"
 { return _FALSE_;       }
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
-#line 186 "lexer.l"
+#line 187 "lexer.l"
 { return _NOT_;         }
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
-#line 187 "lexer.l"
+#line 188 "lexer.l"
 { return _AND_;         }
 	YY_BREAK
 case 25:
 YY_RULE_SETUP
-#line 188 "lexer.l"
+#line 189 "lexer.l"
 { return _OR_;          }
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 189 "lexer.l"
+#line 190 "lexer.l"
 { return _AT_;          }
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 190 "lexer.l"
+#line 191 "lexer.l"
 { return _IN_;          }
 	YY_BREAK
 case 28:
 YY_RULE_SETUP
-#line 191 "lexer.l"
+#line 192 "lexer.l"
 { return _OF_;          }
 	YY_BREAK
 case 29:
 YY_RULE_SETUP
-#line 192 "lexer.l"
+#line 193 "lexer.l"
 { return _THEM_;        }
 	YY_BREAK
 case 30:
 YY_RULE_SETUP
-#line 193 "lexer.l"
+#line 194 "lexer.l"
 { return _FOR_;         }
 	YY_BREAK
 case 31:
 YY_RULE_SETUP
-#line 194 "lexer.l"
+#line 195 "lexer.l"
 { return _ALL_;         }
 	YY_BREAK
 case 32:
 YY_RULE_SETUP
-#line 195 "lexer.l"
+#line 196 "lexer.l"
 { return _ANY_;         }
 	YY_BREAK
 case 33:
 YY_RULE_SETUP
-#line 196 "lexer.l"
+#line 197 "lexer.l"
 { return _ENTRYPOINT_;  }
 	YY_BREAK
 case 34:
 YY_RULE_SETUP
-#line 197 "lexer.l"
+#line 198 "lexer.l"
 { return _FILESIZE_;    }
 	YY_BREAK
 case 35:
 YY_RULE_SETUP
-#line 198 "lexer.l"
+#line 199 "lexer.l"
 { return _MATCHES_;     }
 	YY_BREAK
 case 36:
 YY_RULE_SETUP
-#line 199 "lexer.l"
+#line 200 "lexer.l"
 { return _CONTAINS_;    }
 	YY_BREAK
 case 37:
 YY_RULE_SETUP
-#line 200 "lexer.l"
+#line 201 "lexer.l"
 { return _IMPORT_;      }
 	YY_BREAK
 case 38:
 YY_RULE_SETUP
-#line 203 "lexer.l"
+#line 204 "lexer.l"
 { BEGIN(comment);       }
 	YY_BREAK
 case 39:
 YY_RULE_SETUP
-#line 204 "lexer.l"
+#line 205 "lexer.l"
 { BEGIN(INITIAL);       }
 	YY_BREAK
 case 40:
 /* rule 40 can match eol */
 YY_RULE_SETUP
-#line 205 "lexer.l"
+#line 206 "lexer.l"
 { /* skip comments */   }
 	YY_BREAK
 case 41:
 YY_RULE_SETUP
-#line 208 "lexer.l"
+#line 209 "lexer.l"
 { /* skip single-line comments */ }
 	YY_BREAK
 case 42:
 YY_RULE_SETUP
-#line 211 "lexer.l"
+#line 212 "lexer.l"
 {
                           yyextra->lex_buf_ptr = yyextra->lex_buf;
                           yyextra->lex_buf_len = 0;
@@ -1366,12 +1367,12 @@ YY_RULE_SETUP
 case 43:
 /* rule 43 can match eol */
 YY_RULE_SETUP
-#line 218 "lexer.l"
+#line 219 "lexer.l"
 { yytext_to_buffer; }
 	YY_BREAK
 case 44:
 YY_RULE_SETUP
-#line 221 "lexer.l"
+#line 222 "lexer.l"
 {
 
   if (compiler->include_callback != NULL)
@@ -1503,7 +1504,7 @@ case YY_STATE_EOF(str):
 case YY_STATE_EOF(regexp):
 case YY_STATE_EOF(include):
 case YY_STATE_EOF(comment):
-#line 348 "lexer.l"
+#line 349 "lexer.l"
 {
 
   YR_COMPILER* compiler = yara_yyget_extra(yyscanner);
@@ -1519,7 +1520,7 @@ case YY_STATE_EOF(comment):
 	YY_BREAK
 case 45:
 YY_RULE_SETUP
-#line 362 "lexer.l"
+#line 363 "lexer.l"
 {
 
   yylval->c_string = yr_strdup(yytext);
@@ -1532,7 +1533,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 46:
 YY_RULE_SETUP
-#line 373 "lexer.l"
+#line 374 "lexer.l"
 {
 
   yylval->c_string = yr_strdup(yytext);
@@ -1545,7 +1546,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 47:
 YY_RULE_SETUP
-#line 384 "lexer.l"
+#line 385 "lexer.l"
 {
 
   yylval->c_string = yr_strdup(yytext);
@@ -1564,7 +1565,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 48:
 YY_RULE_SETUP
-#line 401 "lexer.l"
+#line 402 "lexer.l"
 {
 
   yylval->c_string = yr_strdup(yytext);
@@ -1583,7 +1584,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 49:
 YY_RULE_SETUP
-#line 418 "lexer.l"
+#line 419 "lexer.l"
 {
 
   yylval->c_string = yr_strdup(yytext);
@@ -1602,7 +1603,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 50:
 YY_RULE_SETUP
-#line 435 "lexer.l"
+#line 436 "lexer.l"
 {
 
   char* text = yytext;
@@ -1643,7 +1644,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 51:
 YY_RULE_SETUP
-#line 474 "lexer.l"
+#line 475 "lexer.l"
 {
 
   if (strlen(yytext) > 128)
@@ -1659,7 +1660,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 52:
 YY_RULE_SETUP
-#line 488 "lexer.l"
+#line 489 "lexer.l"
 {
 
   char *endptr;
@@ -1702,7 +1703,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 53:
 YY_RULE_SETUP
-#line 528 "lexer.l"
+#line 529 "lexer.l"
 {
   yylval->double_ = atof(yytext);
   return _DOUBLE_;
@@ -1710,7 +1711,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 54:
 YY_RULE_SETUP
-#line 533 "lexer.l"
+#line 534 "lexer.l"
 {
 
   char *endptr;
@@ -1729,7 +1730,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 55:
 YY_RULE_SETUP
-#line 549 "lexer.l"
+#line 550 "lexer.l"
 {
 
   char *endptr;
@@ -1748,7 +1749,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 56:
 YY_RULE_SETUP
-#line 566 "lexer.l"
+#line 567 "lexer.l"
 {     /* saw closing quote - all done */
 
   alloc_sized_string(s, yyextra->lex_buf_len);
@@ -1764,7 +1765,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 57:
 YY_RULE_SETUP
-#line 580 "lexer.l"
+#line 581 "lexer.l"
 {
 
   lex_check_space_ok("\t", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1774,7 +1775,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 58:
 YY_RULE_SETUP
-#line 588 "lexer.l"
+#line 589 "lexer.l"
 {
 
   lex_check_space_ok("\n", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1784,7 +1785,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 59:
 YY_RULE_SETUP
-#line 596 "lexer.l"
+#line 597 "lexer.l"
 {
 
   lex_check_space_ok("\"", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1794,7 +1795,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 60:
 YY_RULE_SETUP
-#line 604 "lexer.l"
+#line 605 "lexer.l"
 {
 
   lex_check_space_ok("\\", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1804,7 +1805,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 61:
 YY_RULE_SETUP
-#line 612 "lexer.l"
+#line 613 "lexer.l"
 {
 
    int result;
@@ -1817,13 +1818,13 @@ YY_RULE_SETUP
 	YY_BREAK
 case 62:
 YY_RULE_SETUP
-#line 623 "lexer.l"
+#line 624 "lexer.l"
 { yytext_to_buffer; }
 	YY_BREAK
 case 63:
 /* rule 63 can match eol */
 YY_RULE_SETUP
-#line 626 "lexer.l"
+#line 627 "lexer.l"
 {
   syntax_error("unterminated string");
 }
@@ -1831,14 +1832,14 @@ YY_RULE_SETUP
 case 64:
 /* rule 64 can match eol */
 YY_RULE_SETUP
-#line 631 "lexer.l"
+#line 632 "lexer.l"
 {
   syntax_error("illegal escape sequence");
 }
 	YY_BREAK
 case 65:
 YY_RULE_SETUP
-#line 636 "lexer.l"
+#line 637 "lexer.l"
 {
 
   if (yyextra->lex_buf_len > 0)
@@ -1866,7 +1867,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 66:
 YY_RULE_SETUP
-#line 662 "lexer.l"
+#line 663 "lexer.l"
 {
 
   lex_check_space_ok("/", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1876,7 +1877,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 67:
 YY_RULE_SETUP
-#line 670 "lexer.l"
+#line 671 "lexer.l"
 {
 
   lex_check_space_ok("\\.", yyextra->lex_buf_len, LEX_BUF_SIZE);
@@ -1891,20 +1892,20 @@ YY_RULE_SETUP
 	YY_BREAK
 case 68:
 YY_RULE_SETUP
-#line 683 "lexer.l"
+#line 684 "lexer.l"
 { yytext_to_buffer; }
 	YY_BREAK
 case 69:
 /* rule 69 can match eol */
 YY_RULE_SETUP
-#line 686 "lexer.l"
+#line 687 "lexer.l"
 {
   syntax_error("unterminated regular expression");
 }
 	YY_BREAK
 case 70:
 YY_RULE_SETUP
-#line 691 "lexer.l"
+#line 692 "lexer.l"
 {
 
   yylval->sized_string = NULL;
@@ -1915,7 +1916,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 71:
 YY_RULE_SETUP
-#line 700 "lexer.l"
+#line 701 "lexer.l"
 {
 
   yylval->sized_string = NULL;
@@ -1927,7 +1928,7 @@ YY_RULE_SETUP
 case 72:
 /* rule 72 can match eol */
 YY_RULE_SETUP
-#line 709 "lexer.l"
+#line 710 "lexer.l"
 {
   // Match hex-digits with whitespace or comments. The latter are stripped
   // out by hex_lexer.l
@@ -1943,12 +1944,12 @@ YY_RULE_SETUP
 case 73:
 /* rule 73 can match eol */
 YY_RULE_SETUP
-#line 722 "lexer.l"
+#line 723 "lexer.l"
 /* skip whitespace */
 	YY_BREAK
 case 74:
 YY_RULE_SETUP
-#line 724 "lexer.l"
+#line 725 "lexer.l"
 {
 
   if (yytext[0] >= 32 && yytext[0] < 127)
@@ -1963,10 +1964,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 75:
 YY_RULE_SETUP
-#line 736 "lexer.l"
+#line 737 "lexer.l"
 ECHO;
 	YY_BREAK
-#line 1970 "lexer.c"
+#line 1971 "lexer.c"
 
 	case YY_END_OF_BUFFER:
 		{
@@ -3096,7 +3097,7 @@ void yara_yyfree (void * ptr , yyscan_t yyscanner)
 
 #define YYTABLES_NAME "yytables"
 
-#line 736 "lexer.l"
+#line 737 "lexer.l"
 
 
 
@@ -3272,7 +3273,8 @@ int yr_lex_parse_rules_fd(
     YR_COMPILER* compiler)
 {
   yyscan_t yyscanner;
-  char buf[1024];
+  size_t file_size;
+  void* buffer;
 
   compiler->errors = 0;
 
@@ -3285,31 +3287,43 @@ int yr_lex_parse_rules_fd(
   yydebug = 1;
   #endif
 
-  yara_yyset_extra(compiler,yyscanner);
+  #if defined(_WIN32) || defined(__CYGWIN__)
+  file_size = (size_t) GetFileSize(rules_fd, NULL);
+  #else
+  struct stat fs;
+  fstat(rules_fd, &fs);
+  file_size = (size_t) fs.st_size;
+  #endif
 
-  while (1)
+  file_size = 0;
+
+  buffer = yr_malloc(file_size);
+
+  if (buffer == NULL)
   {
-#if defined(_WIN32) || defined(__CYGWIN__)
-    DWORD len;
-    if (!ReadFile(rules_fd, buf, sizeof(buf), &len, NULL))
-      break;
-#else
-    int len = (int) read(rules_fd, buf, sizeof(buf));
-    if (len < 0)
-    {
-      if (errno == EINTR)
-        continue;
-      else
-        break;
-    }
-#endif
-    if (len == 0)
-      break;
-    yara_yy_scan_bytes(buf,len,yyscanner);
+    compiler->errors = 1;
+    compiler->last_error = ERROR_INSUFFICIENT_MEMORY;
+    return compiler->errors;
   }
 
+  #if defined(_WIN32) || defined(__CYGWIN__)
+  if (!ReadFile(rules_fd, buffer, file_size, NULL, NULL))
+  #else
+  if (read(rules_fd, buffer, file_size) != file_size)
+  #endif
+  {
+    yr_free(buffer);
+    compiler->errors = 1;
+    compiler->last_error = ERROR_COULD_NOT_READ_FILE;
+    return compiler->errors;
+  }
+
+  yara_yyset_extra(compiler,yyscanner);
+  yara_yy_scan_bytes((const char*) buffer,file_size,yyscanner);
   yyparse(yyscanner, compiler);
   yara_yylex_destroy(yyscanner);
+
+  yr_free(buffer);
 
   return compiler->errors;
 }

libyara/lexer.l
@@ -55,6 +55,7 @@ with noyywrap then we can remove this pragma.
 #if defined(_WIN32) || defined(__CYGWIN__)
 #include <windows.h>
 #else
+#include <sys/stat.h>
 #include <unistd.h>
 #endif
 
@@ -907,7 +908,8 @@ int yr_lex_parse_rules_fd(
     YR_COMPILER* compiler)
 {
   yyscan_t yyscanner;
-  char buf[1024];
+  size_t file_size;
+  void* buffer;
 
   compiler->errors = 0;
 
@@ -920,31 +922,43 @@ int yr_lex_parse_rules_fd(
   yydebug = 1;
   #endif
 
-  yyset_extra(compiler, yyscanner);
+  #if defined(_WIN32) || defined(__CYGWIN__)
+  file_size = (size_t) GetFileSize(rules_fd, NULL);
+  #else
+  struct stat fs;
+  fstat(rules_fd, &fs);
+  file_size = (size_t) fs.st_size;
+  #endif
 
-  while (1)
+  file_size = 0;
+
+  buffer = yr_malloc(file_size);
+
+  if (buffer == NULL)
   {
-#if defined(_WIN32) || defined(__CYGWIN__)
-    DWORD len;
-    if (!ReadFile(rules_fd, buf, sizeof(buf), &len, NULL))
-      break;
-#else
-    int len = (int) read(rules_fd, buf, sizeof(buf));
-    if (len < 0)
-    {
-      if (errno == EINTR)
-        continue;
-      else
-        break;
-    }
-#endif
-    if (len == 0)
-      break;
-    yy_scan_bytes(buf, len, yyscanner);
+    compiler->errors = 1;
+    compiler->last_error = ERROR_INSUFFICIENT_MEMORY;
+    return compiler->errors;
   }
 
+  #if defined(_WIN32) || defined(__CYGWIN__)
+  if (!ReadFile(rules_fd, buffer, file_size, NULL, NULL))
+  #else
+  if (read(rules_fd, buffer, file_size) != file_size)
+  #endif
+  {
+    yr_free(buffer);
+    compiler->errors = 1;
+    compiler->last_error = ERROR_COULD_NOT_READ_FILE;
+    return compiler->errors;
+  }
+
+  yyset_extra(compiler, yyscanner);
+  yy_scan_bytes((const char*) buffer, file_size, yyscanner);
   yyparse(yyscanner, compiler);
   yylex_destroy(yyscanner);
+
+  yr_free(buffer);
 
   return compiler->errors;
 }

libyara/compiler.c
@@ -1159,6 +1159,13 @@ YR_API char* yr_compiler_get_error_message(
           buffer_size,
           "integer overflow in \"%s\"",
           compiler->last_error_extra_info);
+      break;
+    case ERROR_COULD_NOT_READ_FILE:
+      snprintf(
+          buffer,
+          buffer_size,
+          "could not read file");
+      break;
   }
 
   return buffer;

libyara/include/yara/error.h
@@ -97,6 +97,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define ERROR_INTEGER_OVERFLOW                  52
 #define ERROR_CALLBACK_REQUIRED                 53
 #define ERROR_INVALID_OPERAND                   54
+#define ERROR_COULD_NOT_READ_FILE               55
 
 
 #define FAIL_ON_ERROR(x) { \

tests/data/baz.yar
@@ -1,1 +1,24 @@
+/*
+    Add padding for making the file large enough to trigger issue #884
+
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+    pading pading pading pading pading pading pading pading pading pading
+*/
+
 rule baz { condition: true }

tests/test-api.c
@@ -548,7 +548,7 @@ void test_ast_callback()
 
 
 void stats_for_rules(
-    const char* rules_str, 
+    const char* rules_str,
     YR_RULES_STATS* stats)
 {
   YR_COMPILER* compiler = NULL;

commit 042f59b659cf97ec8f3955e67463bf2fa5b3b3f7

Fix a few issues introduced in 54059646ede198405c193f0f04d0ad1940fbb95f.

libyara/lexer.c
@@ -3281,21 +3281,18 @@ int yr_lex_parse_rules_fd(
   if (setjmp(compiler->error_recovery) != 0)
     return compiler->errors;
 
-  yara_yylex_init(&yyscanner);
-
-  #if YYDEBUG
-  yydebug = 1;
-  #endif
-
   #if defined(_WIN32) || defined(__CYGWIN__)
   file_size = (size_t) GetFileSize(rules_fd, NULL);
   #else
   struct stat fs;
-  fstat(rules_fd, &fs);
+  if (fstat(rules_fd, &fs) != 0)
+  {
+    compiler->errors = 1;
+    compiler->last_error = ERROR_COULD_NOT_READ_FILE;
+    return compiler->errors;
+  }
   file_size = (size_t) fs.st_size;
   #endif
-
-  file_size = 0;
 
   buffer = yr_malloc(file_size);
 
@@ -3317,6 +3314,12 @@ int yr_lex_parse_rules_fd(
     compiler->last_error = ERROR_COULD_NOT_READ_FILE;
     return compiler->errors;
   }
+
+  yara_yylex_init(&yyscanner);
+
+  #if YYDEBUG
+  yydebug = 1;
+  #endif
 
   yara_yyset_extra(compiler,yyscanner);
   yara_yy_scan_bytes((const char*) buffer,file_size,yyscanner);

libyara/lexer.l
@@ -916,21 +916,18 @@ int yr_lex_parse_rules_fd(
   if (setjmp(compiler->error_recovery) != 0)
     return compiler->errors;
 
-  yylex_init(&yyscanner);
-
-  #if YYDEBUG
-  yydebug = 1;
-  #endif
-
   #if defined(_WIN32) || defined(__CYGWIN__)
   file_size = (size_t) GetFileSize(rules_fd, NULL);
   #else
   struct stat fs;
-  fstat(rules_fd, &fs);
+  if (fstat(rules_fd, &fs) != 0)
+  {
+    compiler->errors = 1;
+    compiler->last_error = ERROR_COULD_NOT_READ_FILE;
+    return compiler->errors;
+  }
   file_size = (size_t) fs.st_size;
   #endif
-
-  file_size = 0;
 
   buffer = yr_malloc(file_size);
 
@@ -952,6 +949,12 @@ int yr_lex_parse_rules_fd(
     compiler->last_error = ERROR_COULD_NOT_READ_FILE;
     return compiler->errors;
   }
+
+  yylex_init(&yyscanner);
+
+  #if YYDEBUG
+  yydebug = 1;
+  #endif
 
   yyset_extra(compiler, yyscanner);
   yy_scan_bytes((const char*) buffer, file_size, yyscanner);