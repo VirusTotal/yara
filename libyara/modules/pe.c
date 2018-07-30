VirusTotal/yara

commit a43c41614c1383021ce1743ac9e7ce7ea0473118

Add thumbprint to pe.signatures[] (#897)
* Add thumbprint to pe.signatures[]

* Add tests for certificate parsing.

libyara/modules/pe.c
@@ -1192,10 +1192,25 @@ void pe_parse_certificates(
       const char* sig_alg;
       char buffer[256];
       int bytes;
+      const EVP_MD* sha1_digest = EVP_sha1();
+      unsigned char thumbprint[YR_SHA1_LEN];
+      char thumbprint_ascii[YR_SHA1_LEN * 2];
 
       ASN1_INTEGER* serial;
 
       X509* cert = sk_X509_value(certs, i);
+
+      X509_digest(cert, sha1_digest, thumbprint, NULL);
+      for (i = 0; i < YR_SHA1_LEN; i++)
+      {
+        sprintf(thumbprint_ascii + (i * 2), "%02x", thumbprint[i]);
+      }
+      set_sized_string(
+          (char*) thumbprint_ascii,
+          sizeof(thumbprint_ascii),
+          pe->object,
+          "signatures[%i].thumbprint",
+          counter);
 
       X509_NAME_oneline(
           X509_get_issuer_name(cert), buffer, sizeof(buffer));
@@ -2343,6 +2358,7 @@ begin_declarations;
 
   #if defined(HAVE_LIBCRYPTO)
   begin_struct_array("signatures");
+    declare_string("thumbprint");
     declare_string("issuer");
     declare_string("subject");
     declare_integer("version");

tests/test-pe.c
@@ -89,6 +89,16 @@ int main(int argc, char** argv)
       }",
       "tests/data/tiny");
 
+  assert_true_rule_file(
+      "import \"pe\" \
+      rule test { \
+        condition: \
+          pe.number_of_signatures == 1 and \
+          pe.signatures[0].thumbprint == \"c1bf1b8f751bf97626ed77f755f0a393106f2454\" and \
+          pe.signatures[0].subject == \"/C=US/ST=California/L=Menlo Park/O=Quicken, Inc./OU=Operations/CN=Quicken, Inc.\" \
+      }",
+      "tests/data/079a472d22290a94ebb212aa8015cdc8dd28a968c6b4d3b88acdd58ce2d3b885");
+
   #endif
 
   assert_true_rule_file(

commit 2394c285144ddc765562c230976871adea6f432c

Fix off-by-one buffer overflow introduced in a43c41614c1383021ce1743ac9e7ce7ea0473118.

libyara/modules/pe.c
@@ -1200,20 +1200,19 @@ void pe_parse_certificates(
       int bytes;
       const EVP_MD* sha1_digest = EVP_sha1();
       unsigned char thumbprint[YR_SHA1_LEN];
-      char thumbprint_ascii[YR_SHA1_LEN * 2];
+      char thumbprint_ascii[YR_SHA1_LEN * 2 + 1];
 
       ASN1_INTEGER* serial;
 
       X509* cert = sk_X509_value(certs, i);
 
       X509_digest(cert, sha1_digest, thumbprint, NULL);
+
       for (i = 0; i < YR_SHA1_LEN; i++)
-      {
         sprintf(thumbprint_ascii + (i * 2), "%02x", thumbprint[i]);
-      }
-      set_sized_string(
+
+      set_string(
           (char*) thumbprint_ascii,
-          sizeof(thumbprint_ascii),
           pe->object,
           "signatures[%i].thumbprint",
           counter);