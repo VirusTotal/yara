39602514e2c7aa990d4d361adbd9a89bdfd147dd
Victor M. Alvarez <plusvic@gmail.com>
2018-08-02 11:57:53 +0200

    Use YR_BUILDING_DLL and YR_IMPORTING_DLL for controlling the semantics of YR_API (#924)
    For compiling YARA in Windows as a DLL you must define YR_BUILDING_DLL. For using it that DLL in your project you must define YR_IMPORTING_DLL. This allows to use the same header files in both cases. If YR_BUILDING_DLL is defined, YR_API translates to __declspec(dllexport), if YR_IMPORTING_DLL is defined YR_API translates to __declspec(dllimport).

--- libyara/include/yara/utils.h
+++ libyara/include/yara/utils.h
@@ -54,21 +54,38 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define EXTERNC
 #endif
 
-#if defined(__GNUC__)
-#define YR_API EXTERNC __attribute__((visibility("default")))
-#elif defined(_MSC_VER)
-#define YR_API EXTERNC __declspec(dllexport)
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+  #ifdef YR_BUILDING_DLL
+    #ifdef __GNUC__
+      #define YR_API EXTERNC __attribute_((dllexport))
+      #define YR_DEPRECATED_API EXTERNC __attribute__((deprecated))
+    #else
+      #define YR_API EXTERNC __declspec(dllexport)
+      #define YR_DEPRECATED_API EXTERNC __declspec(deprecated)
+    #endif
+  #elif defined(YR_IMPORTING_DLL)
+    #ifdef __GNUC__
+      #define YR_API EXTERNC __attribute__((dllimport))
+      #define YR_DEPRECATED_API EXTERNC __attribute__((deprecated))
+    #else
+      #define YR_API EXTERNC __declspec(dllimport)
+      #define YR_DEPRECATED_API EXTERNC __declspec(deprecated)
+    #endif
+  #else
+    #define YR_API EXTERNC
+    #define YR_DEPRECATED_API EXTERNC
+  #endif
 #else
-#define YR_API EXTERNC
+  #if __GNUC__ >= 4
+    #define YR_API EXTERNC __attribute__((visibility ("default")))
+    #define YR_DEPRECATED_API EXTERNC __attribute__((deprecated))
+  #else
+    #define YR_API EXTERNC
+    #define YR_DEPRECATED_API EXTERNC
+  #endif
 #endif
 
-#if defined(__GNUC__)
-#define YR_DEPRECATED_API EXTERNC __attribute__((deprecated))
-#elif defined(_MSC_VER)
-#define YR_DEPRECATED_API EXTERNC __declspec(deprecated)
-#else
-#define YR_DEPRECATED_API EXTERNC
-#endif
 
 #if defined(__GNUC__)
 #define YR_ALIGN(n) __attribute__((aligned(n)))
