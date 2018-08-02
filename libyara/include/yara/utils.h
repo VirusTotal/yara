3180052ffcd85568505cf34ca336dfc9326e4478
Victor M. Alvarez <vmalvarez@virustotal.com>
2018-08-02 11:26:49 +0200

    Define YR_BUILDING_STATIC_LIB and YR_BUILDING_DLL while building YARA in Windows as DLL or static library respectively.
    This allows using the same header files both while building and using YARA from third-party projects.

--- libyara/include/yara/utils.h
+++ libyara/include/yara/utils.h
@@ -54,21 +54,39 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define EXTERNC
 #endif
 
-#if defined(__GNUC__)
-#define YR_API EXTERNC __attribute__((visibility("default")))
-#elif defined(_MSC_VER)
-#define YR_API EXTERNC __declspec(dllexport)
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+  #ifdef YR_BUILDING_DLL
+    #ifdef __GNUC__
+      #define YR_API EXTERNC __attribute_((dllexport))
+      #define YR_DEPRECATED_API EXTERNC __attribute__((deprecated))
+    #else
+      #define YR_API EXTERNC __declspec(dllexport)
+      #define YR_DEPRECATED_API EXTERNC __declspec(deprecated)
+    #endif
+  #elif defined(YR_BUILDING_STATIC_LIB)
+    #define YR_API EXTERNC
+    #define YR_DEPRECATED_API EXTERNC
+  #else
+    #ifdef __GNUC__
+      #define YR_API EXTERNC __attribute__((dllimport))
+      #define YR_DEPRECATED_API EXTERNC __attribute__((deprecated))
+    #else
+      #define YR_API EXTERNC __declspec(dllimport)
+      #define YR_DEPRECATED_API EXTERNC __declspec(deprecated)
+    #endif
+  #endif
+  #define DLL_LOCAL
 #else
-#define YR_API EXTERNC
+  #if __GNUC__ >= 4
+    #define YR_API EXTERNC __attribute__((visibility ("default")))
+    #define YR_DEPRECATED_API EXTERNC __attribute__((deprecated))
+  #else
+    #define YR_API EXTERNC
+    #define YR_DEPRECATED_API EXTERNC
+  #endif
 #endif
 
-#if defined(__GNUC__)
-#define YR_DEPRECATED_API EXTERNC __attribute__((deprecated))
-#elif defined(_MSC_VER)
-#define YR_DEPRECATED_API EXTERNC __declspec(deprecated)
-#else
-#define YR_DEPRECATED_API EXTERNC
-#endif
 
 #if defined(__GNUC__)
 #define YR_ALIGN(n) __attribute__((aligned(n)))
