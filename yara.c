ddb45e7edcc19eed36bc77f8731728662d274f5e
Rastislav Masaryk <rmasaryk@eset.sk>
2018-09-10 10:19:47 +0200

    added mem leaks detection

--- yara.c
+++ yara.c
@@ -41,6 +41,12 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define PRIx64 "I64x"
 #define PRId64 "I64d"
 
+#if defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
 #endif
 
 #include <time.h>
@@ -1337,6 +1343,10 @@ _exit:
     yr_rules_destroy(rules);
 
   yr_finalize();
+
+#if defined(_DEBUG)
+  _CrtDumpMemoryLeaks();
+#endif
 
   return result;
 }
