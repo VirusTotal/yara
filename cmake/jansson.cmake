550f7fdfc28667c8d70d91e72c499b89d567d432
Sylvain Peyrefitte <sylvain.peyrefitte@gmail.com>
2019-04-16 11:09:24 +0200

    Use install step for jansson

--- cmake/jansson.cmake
+++ cmake/jansson.cmake
@@ -7,22 +7,21 @@ ExternalProject_Add(
   jansson
   GIT_REPOSITORY "https://github.com/akheron/jansson.git"
   GIT_TAG "v2.12"
-  INSTALL_COMMAND ""	# do not install janson
-  CMAKE_ARGS -DJANSSON_BUILD_DOCS=OFF -DJANSSON_WITHOUT_TESTS=ON -DJANSSON_EXAMPLES=OFF -DCMAKE_DEBUG_POSTFIX=v -DCMAKE_RELEASE_POSTFIX=v
+  CMAKE_ARGS -DJANSSON_BUILD_DOCS=OFF -DJANSSON_WITHOUT_TESTS=ON -DJANSSON_EXAMPLES=OFF -DCMAKE_DEBUG_POSTFIX=v -DCMAKE_RELEASE_POSTFIX=v -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/jansson
 )
 
 add_library(libjansson STATIC IMPORTED GLOBAL)
 add_dependencies(libjansson jansson)
 
-set(LIBJANSSON_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/jansson-prefix/src/jansson-build/include)
+set(LIBJANSSON_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/jansson/include)
 file(MAKE_DIRECTORY ${LIBJANSSON_INCLUDE_DIR})
 set_target_properties(libjansson PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${LIBJANSSON_INCLUDE_DIR})
 
 
 if(MSVC)
-	set(LIBJANSSON_LIB ${CMAKE_CURRENT_BINARY_DIR}/jansson-prefix/src/jansson-build/lib/${CMAKE_CFG_INTDIR}/janssono.lib)
+	set(LIBJANSSON_LIB ${CMAKE_CURRENT_BINARY_DIR}/jansson/lib/janssonv.lib)
 else(UNIX)
-	set(LIBJANSSON_LIB ${CMAKE_CURRENT_BINARY_DIR}/jansson-prefix/src/jansson-build/lib/libjansson.a)
+	set(LIBJANSSON_LIB ${CMAKE_CURRENT_BINARY_DIR}/jansson/lib/libjansson.a)
 endif()
 
 set_target_properties(libjansson PROPERTIES IMPORTED_LOCATION ${LIBJANSSON_LIB})
