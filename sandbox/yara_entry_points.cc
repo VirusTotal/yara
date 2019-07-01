ee3e7e28b8dd59ebc85cd4dc65819cc00c0bf654
Christian Blichmann <cblichmann@google.com>
2019-07-01 12:30:44 +0200

    Add a thread-safe Sandboxed API for YARA
    The library built by the Bazel target //sandbox:yara_sapi can be used
    directly to perform sandboxed YARA scans.
    The API is intentionally kept very simple, only three methods are
    exposed by the YaraTransaction C++ class:
    * YaraTransaction::Create() to create with the specified timeout and
      number of worker threads
    * YaraTransaction::LoadRules() to load YARA rules from string, and
    * YaraTransaction::ScanFd() to scan the contents of a file descriptor.
    
    Internally, this creates a new sandboxed sub-process which maintains a
    dispatch queue and multiple worker threads. The transaction class then
    multiplexes scan tasks to the sandboxee where they will be executed in
    parallel. This allows many scan threads to share the same set of YARA
    rules, saving memory.
    
    See sandbox/sandboxed_yara.cc and the unit test in
    sandbox/yara_transaction_test.cc for examples on how to use this API.
    
    Signed-off-by: Christian Blichmann <cblichmann@google.com>

---
+++ sandbox/yara_entry_points.cc
@@ -0,0 +1,230 @@
+/*
+Copyright (c) 2019. The YARA Authors. All Rights Reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <cstdio>
+#include <memory>
+#include <queue>
+#include <string>
+#include <thread>
+
+#include "absl/base/attributes.h"
+#include "absl/container/node_hash_map.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
+#include "absl/synchronization/mutex.h"
+#include "libyara/include/yara.h"
+#include "sandbox/collect_matches.h"
+#include "sandbox/yara_matches.pb.h"
+
+namespace yara {
+namespace {
+
+struct ScanTask {
+  // Key into the g_results map, used by YaraGetScanResult()
+  uint64_t result_id;
+
+  // File descriptor containing the data to scan
+  int data_fd;
+
+  // File descriptor used to signal the host code on scan completion
+  int event_fd;
+
+  // Scan timeout. YARA only supports second granularity.
+  absl::Duration timeout;
+};
+
+struct ScanResult {
+  int code;
+  YaraMatches matches;
+};
+
+static const bool g_init_done ABSL_ATTRIBUTE_UNUSED = []() {
+  // Disable output buffering
+  setbuf(stdout, nullptr);
+  setbuf(stderr, nullptr);
+
+  // Increase stack size
+  struct rlimit stack_limit;
+  stack_limit.rlim_cur = 1 << 20 /* 1 MiB */;
+  stack_limit.rlim_max = stack_limit.rlim_cur;
+  ABSL_RAW_CHECK(setrlimit(RLIMIT_STACK, &stack_limit) == 0, strerror(errno));
+
+  // Initialize YARA. Note that the sandboxed code never calls yr_finalize().
+  // Instead, the OS will clean up on process exit.
+  const int err = yr_initialize();
+  ABSL_RAW_CHECK(
+      err == ERROR_SUCCESS,
+      absl::StrCat("yr_initialize() failed with code: ", err).c_str());
+  return true;
+}();
+
+// Global dispatch queue used to schedule new scan tasks
+ABSL_CONST_INIT static absl::Mutex g_queue_mutex(absl::kConstInit);
+static auto* g_queue GUARDED_BY(g_queue_mutex) = new std::queue<ScanTask>();
+
+static uint64_t g_result_id GUARDED_BY(g_queue_mutex) = 0;
+
+// This map tracks scan results. It relies on pointers staying stable, so this
+// uses a node_hash_map<> instead of a flat_hash_map<>.
+ABSL_CONST_INIT static absl::Mutex g_results_mutex(absl::kConstInit);
+static auto* g_results GUARDED_BY(g_results_mutex) =
+    new absl::node_hash_map<uint64_t, ScanResult>();
+
+ABSL_CONST_INIT static absl::Mutex g_rules_mutex(absl::kConstInit);
+static YR_RULES* g_rules GUARDED_BY(g_rules_mutex) = nullptr;
+
+void ScanWorker() {
+  while (true) {
+    // Wait for and retrieve a new ScanTask from the queue.
+    g_queue_mutex.LockWhen(absl::Condition(
+        +[](std::queue<ScanTask>* queue) { return !queue->empty(); }, g_queue));
+    const ScanTask task = std::move(g_queue->front());
+    g_queue->pop();
+    g_queue_mutex.Unlock();
+
+    ScanResult result;
+    {
+      absl::ReaderMutexLock lock(&g_rules_mutex);
+      result.code = yr_rules_scan_fd(
+          g_rules, task.data_fd,
+          // Disable SIGSEGV handler, allowing YARA to crash/coredump.
+          SCAN_FLAGS_NO_TRYCATCH, CollectMatches,
+          /*user_data=*/reinterpret_cast<void*>(&result.matches),
+          absl::ToInt64Seconds(task.timeout));
+    }
+    {
+      absl::MutexLock lock(&g_results_mutex);
+      (*g_results)[task.result_id] = std::move(result);
+    }
+
+    // Unblock any waiting clients on the host side. This should always succeed
+    // writing 8 bytes, as long as the event_fd stays open in this function,
+    // hence the CHECK.
+    uint64_t unblock_value = 1;
+    ABSL_RAW_CHECK(write(task.event_fd, &unblock_value,
+                         sizeof(unblock_value)) == sizeof(unblock_value),
+                   strerror(errno));
+
+    close(task.event_fd);
+    close(task.data_fd);
+  }
+}
+
+}  // namespace
+
+extern "C" void YaraInitWorkers(int num_workers) {
+  const int num_threads =
+      std::min(static_cast<unsigned int>(std::min(num_workers, YR_MAX_THREADS)),
+               std::thread::hardware_concurrency());
+  static auto* workers = new std::vector<std::thread>();
+  workers->reserve(num_threads);
+  for (int i = 0; i < num_threads; ++i) {
+    workers->emplace_back(ScanWorker);
+  }
+}
+
+// Initializes the global YARA rules set from a string. Returns the number of
+// rules loaded. Extended error information can be found in status if it is not
+// nullptr.
+extern "C" int YaraLoadRules(const char* rule_string, YaraStatus* error_status) {
+  _YR_COMPILER* compiler;
+  int error = yr_compiler_create(&compiler);
+  if (error != ERROR_SUCCESS) {
+    if (error_status) {
+      error_status->set_code(error);
+    }
+    return 0;
+  }
+  std::unique_ptr<_YR_COMPILER, void (*)(_YR_COMPILER*)> compiler_cleanup(
+      compiler, yr_compiler_destroy);
+
+  if (yr_compiler_add_string(compiler, rule_string, /*namespace_=*/nullptr) !=
+      0) {
+    if (error_status) {
+      error_status->set_code(compiler->last_error);
+
+      char message[512] = {'\0'};
+      yr_compiler_get_error_message(compiler, message, sizeof(message));
+      error_status->set_message(message);
+    }
+    return 0;
+  }
+
+  YR_RULES* rules = nullptr;
+  error = yr_compiler_get_rules(compiler, &rules);
+  if (error != ERROR_SUCCESS) {
+    if (error_status) {
+      error_status->set_code(error);
+    }
+    return 0;
+  }
+
+  int num_rules = 0;
+  YR_RULE* rule;
+  yr_rules_foreach(rules, rule) { ++num_rules; }
+
+  absl::MutexLock lock(&g_rules_mutex);
+  if (g_rules) {
+    yr_rules_destroy(g_rules);
+  }
+  g_rules = rules;
+
+  return num_rules;
+}
+
+// Schedules a new asynchronous YARA scan task on the data in the specified file
+// descriptor. Notifies host code via writing to the event_fd file descriptor.
+// Returns a unique identifier that can be used to retrieve the results.
+extern "C" uint64_t YaraAsyncScanFd(int data_fd, int event_fd,
+                                    int timeout_secs) {
+  absl::MutexLock queue_lock(&g_queue_mutex);
+  ++g_result_id;
+  g_queue->push({g_result_id, data_fd, event_fd, absl::Seconds(timeout_secs)});
+  return g_result_id;
+}
+
+extern "C" int YaraGetScanResult(uint64_t result_id, YaraMatches* matches) {
+  absl::MutexLock lock(&g_results_mutex);
+  auto result = g_results->find(result_id);
+  if (result == g_results->end()) {
+    return -1;
+  }
+
+  int code = result->second.code;
+  *matches = std::move(result->second.matches);
+  g_results->erase(result);
+  return code;
+}
+
+}  // namespace yara
---
+++ sandbox/yara_transaction.cc
@@ -0,0 +1,161 @@
+/*
+Copyright (c) 2019. The YARA Authors. All Rights Reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "sandbox/yara_transaction.h"
+
+#include <poll.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
+
+#include "absl/strings/str_cat.h"
+#include "libyara/include/yara/error.h"
+#include "sandboxed_api/util/canonical_errors.h"
+#include "sandboxed_api/util/status_macros.h"
+
+namespace yara {
+
+absl::Mutex YaraTransaction::mutex_(absl::kConstInit);
+
+::sapi::StatusOr<std::unique_ptr<YaraTransaction>> YaraTransaction::Create(
+    Options options) {
+  auto transaction =
+      absl::WrapUnique(new YaraTransaction(options.scan_timeout));
+  // "Run" the transaction in order to initialize the underlying sandbox.
+  SAPI_RETURN_IF_ERROR(transaction->Run());
+
+  sandbox::YaraApi api(transaction->GetSandbox());
+  SAPI_RETURN_IF_ERROR(
+      api.YaraInitWorkers(options.num_workers >= 1 ? options.num_workers : 1));
+
+  return transaction;
+}
+
+::sapi::StatusOr<int> YaraTransaction::LoadRules(
+    const std::string& rule_string) {
+  absl::MutexLock lock(&mutex_);
+  sandbox::YaraApi api(GetSandbox());
+
+  ::sapi::v::ConstCStr rule_string_sapi(rule_string.c_str());
+  YaraStatus error_status;
+  ::sapi::v::Proto<YaraStatus> error_status_sapi(error_status);
+  SAPI_ASSIGN_OR_RETURN(int num_rules,
+                        api.YaraLoadRules(rule_string_sapi.PtrBefore(),
+                                          error_status_sapi.PtrBoth()));
+  if (num_rules <= 0) {
+    auto error_status_copy = error_status_sapi.GetProtoCopy();
+    if (!error_status_copy) {
+      return ::sapi::UnknownError("Deserialization of response failed");
+    }
+    return ::sapi::InvalidArgumentError(error_status_copy->message());
+  }
+  return num_rules;
+}
+
+::sapi::StatusOr<YaraMatches> YaraTransaction::ScanFd(int fd) {
+  int local_event_fd = eventfd(0 /* initval */, 0 /* flags */);
+  if (local_event_fd == -1) {
+    return ::sapi::InternalError(
+        absl::StrCat("eventfd() error: ", strerror(errno)));
+  }
+  struct FDCloser {
+    ~FDCloser() { close(event_fd); }
+    int event_fd;
+  } event_fd_closer = {local_event_fd};
+
+  auto* sandbox = GetSandbox();
+  sandbox::YaraApi api(sandbox);
+  uint64_t result_id;
+  {
+    absl::MutexLock lock(&mutex_);
+
+    // Note: These SAPI Fd objects use the underlying sandbox comms to
+    //       synchronize. Hence they must live within this locked scope.
+    ::sapi::v::Fd event_fd{local_event_fd};
+    SAPI_RETURN_IF_ERROR(sandbox->TransferToSandboxee(&event_fd));
+    event_fd.OwnLocalFd(false);   // Needs to be valid during poll()
+    event_fd.OwnRemoteFd(false);  // Sandboxee will close
+
+    ::sapi::v::Fd data_fd{fd};
+    SAPI_RETURN_IF_ERROR(sandbox->TransferToSandboxee(&data_fd));
+    data_fd.OwnLocalFd(false);   // To be closed by caller
+    data_fd.OwnRemoteFd(false);  // Sandboxee will close
+
+    SAPI_ASSIGN_OR_RETURN(
+        result_id,
+        api.YaraAsyncScanFd(data_fd.GetRemoteFd(), event_fd.GetRemoteFd(),
+                            absl::ToInt64Seconds(scan_timeout_)));
+  }
+
+  pollfd poll_events{local_event_fd, POLLIN};
+  int poll_result;
+
+  // TEMP_FAILURE_RETRY is a GNU extension that retries if the call returns
+  // EINTR.
+  poll_result = TEMP_FAILURE_RETRY(
+      poll(&poll_events, 1 /* nfds */,
+           // Add extra time to allow code inside the sandbox to time out first.
+           absl::ToInt64Milliseconds(scan_timeout_ + absl::Seconds(10))));
+  if (poll_result == 0) {
+    return ::sapi::DeadlineExceededError("Scan timeout during poll()");
+  }
+
+  if (poll_result == -1) {
+    return ::sapi::InternalError(
+        absl::StrCat("poll() error: ", strerror(errno)));
+  }
+  if (poll_events.revents & POLLHUP || poll_events.revents & POLLERR ||
+      poll_events.revents & POLLNVAL) {
+    return ::sapi::InternalError(
+        absl::StrCat("poll() error, revents: ", poll_events.revents));
+  }
+
+  absl::MutexLock lock(&mutex_);
+  YaraMatches matches;
+  ::sapi::v::Proto<YaraMatches> matches_sapi(matches);
+  SAPI_ASSIGN_OR_RETURN(
+      int scan_result,
+      api.YaraGetScanResult(result_id, matches_sapi.PtrBoth()));
+  switch (scan_result) {
+    case ERROR_SUCCESS:
+    case ERROR_TOO_MANY_MATCHES: {
+      auto matches_copy = matches_sapi.GetProtoCopy();
+      if (!matches_copy) {
+        return ::sapi::UnknownError("Deserialization of response failed");
+      }
+      return *matches_copy;
+    }
+
+    case ERROR_SCAN_TIMEOUT:
+      return ::sapi::DeadlineExceededError("Scan timeout");
+  }
+  return ::sapi::InternalError(
+      absl::StrCat("Error during scan: ", scan_result));
+}
+
+}  // namespace yara
