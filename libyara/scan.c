VirusTotal/yara

commit 37aae685c20a621f61ff8f7187b6c948a87004eb

Private strings (#1096)
* Add private modifier for strings.

This commit adds a private modifier to the grammar, which can be used on any
string. When this modifier is used the string is treated as normal except it is
never reported back in match objects. It is still searched for normally and
still used normally in condition evaluation but it will never be seen in the
match callback.

This is a useful feature when you have strings that you want to use but they
match a lot of times and you don't want them polluting your log results. The
best example that demonstrates this is the technique described at [1]. In this
example the local file header magic is going to match a lot of times, and it is
only needed to look through the local file header for filenames at offsets
relative to the matches. We don't really care where the offsets of the local
file header are, just where the matching filenames are.

If you are logging where all the matches are for every string and there are
10,000 local file headers that's a lot of log data being generated which is not
helpful at all.

[1]: https://blog.korelogic.com/blog/2015/08/19/yara-ctf-2

* Optimize VM execution code for private strings.

Rather than checking the matches list and only if none are found then check for
private matches, it's better to check only the appropriate list after using
STRING_IS_PRIVATE.

* Add private strings in various tests.

This should cover all the cases of operators on strings that also need to check
for private strings.

* Add docs for private strings.

* Fixup documentation.

Private strings apply to all string types, not just text strings.

* Fix private modifier for chained strings. Add a test case for it too.

libyara/scan.c
@@ -439,7 +439,9 @@ static int _yr_scan_verify_chained_string_match(
               (void**) &match->data));
 
           FAIL_ON_ERROR(_yr_scan_add_match_to_list(
-              match, &string->matches[tidx], false));
+              match,
+              !STRING_IS_PRIVATE(string) ? &string->matches[tidx] : &string->private_matches[tidx],
+              false));
         }
 
         match = next_match;
@@ -606,7 +608,7 @@ static int _yr_scan_match_callback(
 
       FAIL_ON_ERROR(_yr_scan_add_match_to_list(
           new_match,
-          &string->matches[tidx],
+          !STRING_IS_PRIVATE(string) ? &string->matches[tidx] : &string->private_matches[tidx],
           STRING_IS_GREEDY_REGEXP(string)));
     }
   }

libyara/exec.c
@@ -200,6 +200,7 @@ int yr_execute_code(
   YR_VALUE r1;
   YR_VALUE r2;
   YR_VALUE r3;
+  YR_VALUE r4;
 
   uint64_t elapsed_time;
 
@@ -736,8 +737,13 @@ int yr_execute_code(
 
       case OP_FOUND:
         pop(r1);
-        r1.i = r1.s->matches[tidx].tail != NULL ? 1 : 0;
-        push(r1);
+
+        if (STRING_IS_PRIVATE(r1.s))
+          r2.i = r1.s->private_matches[tidx].tail != NULL ? 1 : 0;
+        else
+          r2.i = r1.s->matches[tidx].tail != NULL ? 1 : 0;
+
+        push(r2);
         break;
 
       case OP_FOUND_AT:
@@ -757,7 +763,11 @@ int yr_execute_code(
           return ERROR_INTERNAL_FATAL_ERROR;
         #endif
 
-        match = r2.s->matches[tidx].head;
+        if (STRING_IS_PRIVATE(r2.s))
+          match = r2.s->private_matches[tidx].head;
+        else
+          match = r2.s->matches[tidx].head;
+
         r3.i = false;
 
         while (match != NULL)
@@ -791,15 +801,19 @@ int yr_execute_code(
           return ERROR_INTERNAL_FATAL_ERROR;
         #endif
 
-        match = r3.s->matches[tidx].head;
-        r3.i = false;
+        if (STRING_IS_PRIVATE(r3.s))
+          match = r3.s->private_matches[tidx].head;
+        else
+          match = r3.s->matches[tidx].head;
 
-        while (match != NULL && !r3.i)
+        r4.i = false;
+
+        while (match != NULL && !r4.i)
         {
           if (match->base + match->offset >= r1.i &&
               match->base + match->offset <= r2.i)
           {
-            r3.i = true;
+            r4.i = true;
           }
 
           if (match->base + match->offset > r2.i)
@@ -808,7 +822,7 @@ int yr_execute_code(
           match = match->next;
         }
 
-        push(r3);
+        push(r4);
         break;
 
       case OP_COUNT:
@@ -820,8 +834,12 @@ int yr_execute_code(
           return ERROR_INTERNAL_FATAL_ERROR;
         #endif
 
-        r1.i = r1.s->matches[tidx].count;
-        push(r1);
+        if (STRING_IS_PRIVATE(r1.s))
+          r2.i = r1.s->private_matches[tidx].count;
+        else
+          r2.i = r1.s->matches[tidx].count;
+
+        push(r2);
         break;
 
       case OP_OFFSET:
@@ -836,7 +854,11 @@ int yr_execute_code(
           return ERROR_INTERNAL_FATAL_ERROR;
         #endif
 
-        match = r2.s->matches[tidx].head;
+        if (STRING_IS_PRIVATE(r2.s))
+          match = r2.s->private_matches[tidx].head;
+        else
+          match = r2.s->matches[tidx].head;
+
         i = 1;
         r3.i = UNDEFINED;
 
@@ -864,7 +886,11 @@ int yr_execute_code(
           return ERROR_INTERNAL_FATAL_ERROR;
         #endif
 
-        match = r2.s->matches[tidx].head;
+        if (STRING_IS_PRIVATE(r2.s))
+          match = r2.s->private_matches[tidx].head;
+        else
+          match = r2.s->matches[tidx].head;
+
         i = 1;
         r3.i = UNDEFINED;
 
@@ -887,7 +913,7 @@ int yr_execute_code(
 
         while (!is_undef(r1))
         {
-          if (r1.s->matches[tidx].tail != NULL)
+          if (r1.s->matches[tidx].tail != NULL || r1.s->private_matches[tidx].tail != NULL)
             found++;
           count++;
           pop(r1);

libyara/grammar.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.0.4.  */
+/* A Bison parser, made by GNU Bison 3.0.5.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -158,7 +158,7 @@ extern int yara_yydebug;
 
 union YYSTYPE
 {
-#line 245 "grammar.y" /* yacc.c:1909  */
+#line 248 "grammar.y" /* yacc.c:1916  */
 
   EXPRESSION      expression;
   SIZED_STRING*   sized_string;
@@ -169,7 +169,7 @@ union YYSTYPE
   YR_META*        meta;
   YR_RULE*        rule;
 
-#line 173 "grammar.h" /* yacc.c:1909  */
+#line 173 "grammar.h" /* yacc.c:1916  */
 };
 
 typedef union YYSTYPE YYSTYPE;

libyara/grammar.y
@@ -213,6 +213,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %type <integer> regexp_modifier
 %type <integer> regexp_modifiers
 
+%type <integer> hex_modifier
+%type <integer> hex_modifiers
+
 %type <integer> integer_set
 
 %type <integer> for_expression
@@ -578,15 +581,21 @@ string_declaration
 
         compiler->current_line = 0;
       }
-    | _STRING_IDENTIFIER_ '=' _HEX_STRING_
+    | _STRING_IDENTIFIER_ '='
+      {
+        compiler->current_line = yyget_lineno(yyscanner);
+      }
+      _HEX_STRING_ hex_modifiers
       {
         int result = yr_parser_reduce_string_declaration(
-            yyscanner, STRING_GFLAGS_HEXADECIMAL, $1, $3, &$$);
+            yyscanner, (int32_t) $5 | STRING_GFLAGS_HEXADECIMAL, $1, $4, &$$);
 
         yr_free($1);
-        yr_free($3);
+        yr_free($4);
 
         fail_if_error(result);
+
+        compiler->current_line = 0;
       }
     ;
 
@@ -603,6 +612,7 @@ string_modifier
     | _NOCASE_      { $$ = STRING_GFLAGS_NO_CASE; }
     | _FULLWORD_    { $$ = STRING_GFLAGS_FULL_WORD; }
     | _XOR_         { $$ = STRING_GFLAGS_XOR; }
+    | _PRIVATE_     { $$ = STRING_GFLAGS_PRIVATE; }
     ;
 
 regexp_modifiers
@@ -615,8 +625,17 @@ regexp_modifier
     | _ASCII_       { $$ = STRING_GFLAGS_ASCII; }
     | _NOCASE_      { $$ = STRING_GFLAGS_NO_CASE; }
     | _FULLWORD_    { $$ = STRING_GFLAGS_FULL_WORD; }
+    | _PRIVATE_     { $$ = STRING_GFLAGS_PRIVATE; }
     ;
 
+hex_modifiers
+    : /* empty */                         { $$ = 0; }
+    | hex_modifiers hex_modifier          { $$ = $1 | $2; }
+    ;
+
+hex_modifier
+    : _PRIVATE_     { $$ = STRING_GFLAGS_PRIVATE; }
+    ;
 
 identifier
     : _IDENTIFIER_

libyara/include/yara/types.h
@@ -68,6 +68,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define STRING_GFLAGS_DOT_ALL           0x20000
 #define STRING_GFLAGS_DISABLED          0x40000
 #define STRING_GFLAGS_XOR               0x80000
+#define STRING_GFLAGS_PRIVATE           0x100000
 
 #define STRING_IS_HEX(x) \
     (((x)->g_flags) & STRING_GFLAGS_HEXADECIMAL)
@@ -128,6 +129,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #define STRING_IS_XOR(x) \
     (((x)->g_flags) & STRING_GFLAGS_XOR)
+
+#define STRING_IS_PRIVATE(x) \
+    (((x)->g_flags) & STRING_GFLAGS_PRIVATE)
 
 #define STRING_FOUND(x) \
     ((x)->matches[yr_get_tidx()].tail != NULL)
@@ -272,6 +276,7 @@ struct YR_STRING
   int64_t fixed_offset;
 
   YR_MATCHES matches[YR_MAX_THREADS];
+  YR_MATCHES private_matches[YR_MAX_THREADS];
   YR_MATCHES unconfirmed_matches[YR_MAX_THREADS];
 };
 

libyara/parser.c
@@ -363,6 +363,9 @@ static int _yr_parser_write_string(
   memset((*string)->matches, 0,
          sizeof((*string)->matches));
 
+  memset((*string)->private_matches, 0,
+         sizeof((*string)->private_matches));
+
   memset((*string)->unconfirmed_matches, 0,
          sizeof((*string)->unconfirmed_matches));
 

commit be101c1b4ead0627af5524583b3bee5f26df5843

Avoid putting the same string multiple times in matching_strings_arena.
This fixes a small issue introduced in #1096. The same string could be put more than once in the matching_strings_arena, this doesn't causes any major error nor affect the correctness of the matching, but it could have a negative impact on performance.

libyara/scan.c
@@ -469,9 +469,10 @@ static int _yr_scan_verify_chained_string_match(
         match = next_match;
       }
     }
-    else // it's a part of a chain, but not the tail.
+    else // It's a part of a chain, but not the tail.
     {
       if (matching_string->matches[tidx].count == 0 &&
+          matching_string->private_matches[tidx].count == 0 &&
           matching_string->unconfirmed_matches[tidx].count == 0)
       {
         // If this is the first match for the string, put the string in the