dac15acb8d4cabf9aba53a949ea93693331a8b7b
Victor M. Alvarez <vmalvarez@virustotal.com>
2020-04-28 10:41:59 +0200

    Minor changes in documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'yara'
-copyright = u'2014-2019, VirusTotal'
+copyright = u'2014-2020, VirusTotal'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
