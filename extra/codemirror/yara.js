15a2d8ddb5530158571d2fc9e2cb7f000a2687f0
Ԝеѕ <5124946+wesinator@users.noreply.github.com>
2020-04-30 14:12:51 -0400

    codemirror - add yara 4.0 keywords

--- extra/codemirror/yara.js
+++ extra/codemirror/yara.js
@@ -8,7 +8,7 @@ CodeMirror.defineMode("yara", function(config) {
       for (var i = 0; i < words.length; ++i) obj[words[i]] = true;
       return obj;
     }
-    var keywords = words("all and any ascii at condition contains entrypoint filesize for " +
+    var keywords = words("all and any ascii at base64 base64wide condition contains entrypoint filesize for " +
                          "fullword global import in include int16 int32 int8 matches meta " +
                          "nocase not of or private rule strings them uint16 uint32 " +
                          "uint8 wide xor");
