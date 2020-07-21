VirusTotal/yara

commit a63d579b2c23656314225903436e85263b17b1ad

Set null terminator when creating SIZED_STRING in yr_re_ast_extract_literal.

libyara/re.c
@@ -395,12 +395,15 @@ SIZED_STRING* yr_re_ast_extract_literal(
   else
   {
     child = re_ast->root_node->children_tail;
+
     while (child != NULL)
     {
       string->c_string[--length] = child->value;
       child = child->prev_sibling;
     }
   }
+
+  string->c_string[length] = '\0';
 
   return string;
 }

commit e3ab59538464e2deae4ce096d3daabc350d3c072

Fix issue introduced in a63d579b2c23656314225903436e85263b17b1ad.
Also make sure that we set the null terminator for the SIZED_STRING allocated in _yr_base64_get_base64_substring.

libyara/base64.c
@@ -137,6 +137,7 @@ static SIZED_STRING* _yr_base64_get_base64_substring(
 {
   SIZED_STRING* new_str;
   SIZED_STRING* final_str;
+
   char* start;
   uint32_t length;
   int trailing;
@@ -155,7 +156,9 @@ static SIZED_STRING* _yr_base64_get_base64_substring(
   start = encoded_str->c_string + leading;
 
   memcpy(new_str->c_string, start, length);
+
   new_str->length = length;
+  new_str->c_string[new_str->length] = '\0';
 
   if (wide)
   {

libyara/re.c
@@ -403,7 +403,7 @@ SIZED_STRING* yr_re_ast_extract_literal(
     }
   }
 
-  string->c_string[length] = '\0';
+  string->c_string[string->length] = '\0';
 
   return string;
 }