VirusTotal/yara

commit 7bb8ecd742f802f76f1d6e3e5d5cd220ae1b39ca

Rename rules_list_head, strings_list_head and externals_list_head.
The new names are rules_table, strings_table and ext_vars_table respectively. Now that those fields are not linked list the previous names were somewhat confusing. Document the fields in YR_RULES a little bit better.

libyara/include/yara/types.h
@@ -525,14 +525,35 @@ struct YR_RULES
 {
   YR_ARENA* arena;
 
-  YR_RULE* rules_list_head;
-  YR_STRING* strings_list_head;
-  YR_EXTERNAL_VARIABLE* externals_list_head;
+  // Array of pointers with an entry for each rule. The rule_idx field in the
+  // YR_STRING structure is an index within this array.
+  YR_RULE* rules_table;
 
+  // Array of pointers with an entry for each of the defined strings. The idx
+  // field in the YR_STRING structure is an index within this array.
+  YR_STRING* strings_table;
+
+  // Array of pointers with an entry for each external variable.
+  YR_EXTERNAL_VARIABLE* ext_vars_table;
+
+  // Pointer to the Aho-Corasick transition table.
   YR_AC_TRANSITION* ac_transition_table;
+
+  // A pointer to the arena where YR_AC_MATCH structures are allocated.
   YR_AC_MATCH* ac_match_pool;
+
+  // Table that translates from Aho-Corasick states (which are identified by
+  // numbers 0, 1, 2.. and so on) to the index in ac_match_pool where the
+  // YR_AC_MATCH structures for the corresponding state start.
+  // If the entry corresponding to state N in ac_match_table is zero, it
+  // means that there's no match associated to the state. If it's non-zero,
+  // its value is the 1-based index within ac_match_pool where the first
+  // match resides.
   uint32_t* ac_match_table;
 
+  // Pointer to the first instruction that is executed whan evaluating the
+  // conditions for all rules. The code is executed by yr_execute_code and
+  // the instructions are defined by the OP_X macros in exec.h.
   const uint8_t* code_start;
 
   // Total number of rules.

libyara/exec.c
@@ -789,7 +789,7 @@ int yr_execute_code(YR_SCAN_CONTEXT* context)
       memcpy(&r1.i, ip, sizeof(uint64_t));
       ip += sizeof(uint64_t);
 
-      rule = &context->rules->rules_list_head[r1.i];
+      rule = &context->rules->rules_table[r1.i];
 
       if (RULE_IS_DISABLED(rule))
       {
@@ -813,7 +813,7 @@ int yr_execute_code(YR_SCAN_CONTEXT* context)
 
       assert(current_rule_idx < context->rules->num_rules);
 
-      current_rule = &context->rules->rules_list_head[current_rule_idx];
+      current_rule = &context->rules->rules_table[current_rule_idx];
 
       // If the rule is disabled let's skip its code.
       ip = jmp_if(RULE_IS_DISABLED(current_rule), ip);
@@ -831,7 +831,7 @@ int yr_execute_code(YR_SCAN_CONTEXT* context)
       memcpy(&r2.i, ip, sizeof(uint64_t));
       ip += sizeof(uint64_t);
 
-      rule = &context->rules->rules_list_head[r2.i];
+      rule = &context->rules->rules_table[r2.i];
 
 #if YR_PARANOID_EXEC
       ensure_within_rules_arena(rule);

libyara/include/yara/rules.h
@@ -67,7 +67,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     else /* user code block goes here */
 
 #define yr_rules_foreach(rules, rule) \
-  for (rule = rules->rules_list_head; !RULE_IS_NULL(rule); rule++)
+  for (rule = rules->rules_table; !RULE_IS_NULL(rule); rule++)
 
 YR_API int yr_rules_scan_mem_blocks(
     YR_RULES* rules,

libyara/rules.c
@@ -41,7 +41,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <yara/scanner.h>
 #include <yara/utils.h>
 
-
 YR_API int yr_rules_define_integer_variable(
     YR_RULES* rules,
     const char* identifier,
@@ -52,7 +51,7 @@ YR_API int yr_rules_define_integer_variable(
   if (identifier == NULL)
     return ERROR_INVALID_ARGUMENT;
 
-  external = rules->externals_list_head;
+  external = rules->ext_vars_table;
 
   while (!EXTERNAL_VARIABLE_IS_NULL(external))
   {
@@ -71,7 +70,6 @@ YR_API int yr_rules_define_integer_variable(
   return ERROR_INVALID_ARGUMENT;
 }
 
-
 YR_API int yr_rules_define_boolean_variable(
     YR_RULES* rules,
     const char* identifier,
@@ -82,7 +80,7 @@ YR_API int yr_rules_define_boolean_variable(
   if (identifier == NULL)
     return ERROR_INVALID_ARGUMENT;
 
-  external = rules->externals_list_head;
+  external = rules->ext_vars_table;
 
   while (!EXTERNAL_VARIABLE_IS_NULL(external))
   {
@@ -101,7 +99,6 @@ YR_API int yr_rules_define_boolean_variable(
   return ERROR_INVALID_ARGUMENT;
 }
 
-
 YR_API int yr_rules_define_float_variable(
     YR_RULES* rules,
     const char* identifier,
@@ -112,7 +109,7 @@ YR_API int yr_rules_define_float_variable(
   if (identifier == NULL)
     return ERROR_INVALID_ARGUMENT;
 
-  external = rules->externals_list_head;
+  external = rules->ext_vars_table;
 
   while (!EXTERNAL_VARIABLE_IS_NULL(external))
   {
@@ -131,7 +128,6 @@ YR_API int yr_rules_define_float_variable(
   return ERROR_INVALID_ARGUMENT;
 }
 
-
 YR_API int yr_rules_define_string_variable(
     YR_RULES* rules,
     const char* identifier,
@@ -142,7 +138,7 @@ YR_API int yr_rules_define_string_variable(
   if (identifier == NULL || value == NULL)
     return ERROR_INVALID_ARGUMENT;
 
-  external = rules->externals_list_head;
+  external = rules->ext_vars_table;
 
   while (!EXTERNAL_VARIABLE_IS_NULL(external))
   {
@@ -173,7 +169,6 @@ YR_API int yr_rules_define_string_variable(
   return ERROR_INVALID_ARGUMENT;
 }
 
-
 YR_API int yr_rules_scan_mem_blocks(
     YR_RULES* rules,
     YR_MEMORY_BLOCK_ITERATOR* iterator,
@@ -197,7 +192,6 @@ YR_API int yr_rules_scan_mem_blocks(
 
   return result;
 }
-
 
 YR_API int yr_rules_scan_mem(
     YR_RULES* rules,
@@ -233,7 +227,6 @@ YR_API int yr_rules_scan_mem(
   return result;
 }
 
-
 YR_API int yr_rules_scan_file(
     YR_RULES* rules,
     const char* filename,
@@ -257,7 +250,6 @@ YR_API int yr_rules_scan_file(
   return result;
 }
 
-
 YR_API int yr_rules_scan_fd(
     YR_RULES* rules,
     YR_FILE_DESCRIPTOR fd,
@@ -280,7 +272,6 @@ YR_API int yr_rules_scan_fd(
 
   return result;
 }
-
 
 YR_API int yr_rules_scan_proc(
     YR_RULES* rules,
@@ -313,7 +304,6 @@ YR_API int yr_rules_scan_proc(
   return result;
 }
 
-
 int yr_rules_from_arena(YR_ARENA* arena, YR_RULES** rules)
 {
   YR_RULES* new_rules = (YR_RULES*) yr_malloc(sizeof(YR_RULES));
@@ -334,11 +324,11 @@ int yr_rules_from_arena(YR_ARENA* arena, YR_RULES** rules)
   new_rules->num_strings = summary->num_strings;
   new_rules->num_namespaces = summary->num_namespaces;
 
-  new_rules->rules_list_head = yr_arena_get_ptr(arena, YR_RULES_TABLE, 0);
+  new_rules->rules_table = yr_arena_get_ptr(arena, YR_RULES_TABLE, 0);
 
-  new_rules->strings_list_head = yr_arena_get_ptr(arena, YR_STRINGS_TABLE, 0);
+  new_rules->strings_table = yr_arena_get_ptr(arena, YR_STRINGS_TABLE, 0);
 
-  new_rules->externals_list_head = yr_arena_get_ptr(
+  new_rules->ext_vars_table = yr_arena_get_ptr(
       arena, YR_EXTERNAL_VARIABLES_TABLE, 0);
 
   new_rules->ac_transition_table = yr_arena_get_ptr(
@@ -357,7 +347,6 @@ int yr_rules_from_arena(YR_ARENA* arena, YR_RULES** rules)
   return ERROR_SUCCESS;
 }
 
-
 YR_API int yr_rules_load_stream(YR_STREAM* stream, YR_RULES** rules)
 {
   YR_ARENA* arena;
@@ -375,7 +364,6 @@ YR_API int yr_rules_load_stream(YR_STREAM* stream, YR_RULES** rules)
 
   return ERROR_SUCCESS;
 }
-
 
 YR_API int yr_rules_load(const char* filename, YR_RULES** rules)
 {
@@ -396,12 +384,10 @@ YR_API int yr_rules_load(const char* filename, YR_RULES** rules)
   return result;
 }
 
-
 YR_API int yr_rules_save_stream(YR_RULES* rules, YR_STREAM* stream)
 {
   return yr_arena_save_stream(rules->arena, stream);
 }
-
 
 YR_API int yr_rules_save(YR_RULES* rules, const char* filename)
 {
@@ -421,7 +407,6 @@ YR_API int yr_rules_save(YR_RULES* rules, const char* filename)
   fclose(fh);
   return result;
 }
-
 
 static int _uint32_cmp(const void* a, const void* b)
 {
@@ -505,10 +490,9 @@ YR_API int yr_rules_get_stats(YR_RULES* rules, YR_RULES_STATS* stats)
   return ERROR_SUCCESS;
 }
 
-
 YR_API int yr_rules_destroy(YR_RULES* rules)
 {
-  YR_EXTERNAL_VARIABLE* external = rules->externals_list_head;
+  YR_EXTERNAL_VARIABLE* external = rules->ext_vars_table;
 
   while (!EXTERNAL_VARIABLE_IS_NULL(external))
   {
@@ -535,7 +519,6 @@ YR_API void yr_rule_disable(YR_RULE* rule)
     string->flags |= STRING_FLAGS_DISABLED;
   }
 }
-
 
 YR_API void yr_rule_enable(YR_RULE* rule)
 {

libyara/scanner.c
@@ -41,7 +41,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "exception.h"
 
-
 static int _yr_scanner_scan_mem_block(
     YR_SCANNER* scanner,
     const uint8_t* block_data,
@@ -160,7 +159,6 @@ static int _yr_scanner_scan_mem_block(
   return ERROR_SUCCESS;
 }
 
-
 static void _yr_scanner_clean_matches(YR_SCANNER* scanner)
 {
   memset(
@@ -180,7 +178,6 @@ static void _yr_scanner_clean_matches(YR_SCANNER* scanner)
       0,
       sizeof(YR_MATCHES) * scanner->rules->num_strings);
 }
-
 
 YR_API int yr_scanner_create(YR_RULES* rules, YR_SCANNER** scanner)
 {
@@ -232,7 +229,7 @@ YR_API int yr_scanner_create(YR_RULES* rules, YR_SCANNER** scanner)
   new_scanner->profiling_info = NULL;
 #endif
 
-  external = rules->externals_list_head;
+  external = rules->ext_vars_table;
 
   while (!EXTERNAL_VARIABLE_IS_NULL(external))
   {
@@ -260,7 +257,6 @@ YR_API int yr_scanner_create(YR_RULES* rules, YR_SCANNER** scanner)
 
   return ERROR_SUCCESS;
 }
-
 
 YR_API void yr_scanner_destroy(YR_SCANNER* scanner)
 {
@@ -296,7 +292,6 @@ YR_API void yr_scanner_destroy(YR_SCANNER* scanner)
   yr_free(scanner);
 }
 
-
 YR_API void yr_scanner_set_callback(
     YR_SCANNER* scanner,
     YR_CALLBACK_FUNC callback,
@@ -305,7 +300,6 @@ YR_API void yr_scanner_set_callback(
   scanner->callback = callback;
   scanner->user_data = user_data;
 }
-
 
 YR_API void yr_scanner_set_timeout(YR_SCANNER* scanner, int timeout)
 {
@@ -328,7 +322,6 @@ YR_API void yr_scanner_set_flags(YR_SCANNER* scanner, int flags)
   scanner->flags = flags;
 }
 
-
 YR_API int yr_scanner_define_integer_variable(
     YR_SCANNER* scanner,
     const char* identifier,
@@ -346,7 +339,6 @@ YR_API int yr_scanner_define_integer_variable(
   return yr_object_set_integer(value, obj, NULL);
 }
 
-
 YR_API int yr_scanner_define_boolean_variable(
     YR_SCANNER* scanner,
     const char* identifier,
@@ -354,7 +346,6 @@ YR_API int yr_scanner_define_boolean_variable(
 {
   return yr_scanner_define_integer_variable(scanner, identifier, value);
 }
-
 
 YR_API int yr_scanner_define_float_variable(
     YR_SCANNER* scanner,
@@ -373,7 +364,6 @@ YR_API int yr_scanner_define_float_variable(
   return yr_object_set_float(value, obj, NULL);
 }
 
-
 YR_API int yr_scanner_define_string_variable(
     YR_SCANNER* scanner,
     const char* identifier,
@@ -390,7 +380,6 @@ YR_API int yr_scanner_define_string_variable(
 
   return yr_object_set_string(value, strlen(value), obj, NULL);
 }
-
 
 YR_API int yr_scanner_scan_mem_blocks(
     YR_SCANNER* scanner,
@@ -475,7 +464,7 @@ YR_API int yr_scanner_scan_mem_blocks(
   if (result != ERROR_SUCCESS)
     goto _exit;
 
-  for (i = 0, rule = rules->rules_list_head; !RULE_IS_NULL(rule); i++, rule++)
+  for (i = 0, rule = rules->rules_table; !RULE_IS_NULL(rule); i++, rule++)
   {
     int message = 0;
 
@@ -522,24 +511,20 @@ _exit:
   return result;
 }
 
-
 static YR_MEMORY_BLOCK* _yr_get_first_block(YR_MEMORY_BLOCK_ITERATOR* iterator)
 {
   return (YR_MEMORY_BLOCK*) iterator->context;
 }
-
 
 static YR_MEMORY_BLOCK* _yr_get_next_block(YR_MEMORY_BLOCK_ITERATOR* iterator)
 {
   return NULL;
 }
 
-
 static const uint8_t* _yr_fetch_block_data(YR_MEMORY_BLOCK* block)
 {
   return (const uint8_t*) block->context;
 }
-
 
 static int __yr_scanner_scan_mem(
     YR_SCANNER* scanner,
@@ -571,7 +556,6 @@ static int __yr_scanner_scan_mem(
   return yr_scanner_scan_mem_blocks(scanner, &iterator);
 }
 
-
 //
 // Tests may override this default with their own iterator, e.g. for testing
 // multiple blocks.
@@ -581,7 +565,6 @@ YR_API int (*_yr_scanner_scan_mem)(
     const uint8_t* buffer,
     size_t buffer_size) = &__yr_scanner_scan_mem;
 
-
 YR_API int yr_scanner_scan_mem(
     YR_SCANNER* scanner,
     const uint8_t* buffer,
@@ -589,7 +572,6 @@ YR_API int yr_scanner_scan_mem(
 {
   return _yr_scanner_scan_mem(scanner, buffer, buffer_size);
 }
-
 
 YR_API int yr_scanner_scan_file(YR_SCANNER* scanner, const char* filename)
 {
@@ -606,7 +588,6 @@ YR_API int yr_scanner_scan_file(YR_SCANNER* scanner, const char* filename)
   return result;
 }
 
-
 YR_API int yr_scanner_scan_fd(YR_SCANNER* scanner, YR_FILE_DESCRIPTOR fd)
 {
   YR_MAPPED_FILE mfile;
@@ -621,7 +602,6 @@ YR_API int yr_scanner_scan_fd(YR_SCANNER* scanner, YR_FILE_DESCRIPTOR fd)
 
   return result;
 }
-
 
 YR_API int yr_scanner_scan_proc(YR_SCANNER* scanner, int pid)
 {
@@ -641,21 +621,18 @@ YR_API int yr_scanner_scan_proc(YR_SCANNER* scanner, int pid)
   return result;
 }
 
-
 YR_API YR_STRING* yr_scanner_last_error_string(YR_SCANNER* scanner)
 {
   return scanner->last_error_string;
 }
-
 
 YR_API YR_RULE* yr_scanner_last_error_rule(YR_SCANNER* scanner)
 {
   if (scanner->last_error_string == NULL)
     return NULL;
 
-  return &scanner->rules->rules_list_head[scanner->last_error_string->rule_idx];
+  return &scanner->rules->rules_table[scanner->last_error_string->rule_idx];
 }
-
 
 static int sort_by_cost_desc(
     const struct YR_RULE_PROFILING_INFO* r1,
@@ -692,7 +669,7 @@ YR_API YR_RULE_PROFILING_INFO* yr_scanner_get_profiling_info(
 
   for (uint32_t i = 0; i < scanner->rules->num_rules; i++)
   {
-    profiling_info[i].rule = &scanner->rules->rules_list_head[i];
+    profiling_info[i].rule = &scanner->rules->rules_table[i];
 #ifdef YR_PROFILING_ENABLED
     profiling_info[i].cost = scanner->profiling_info[i].exec_time +
                              (scanner->profiling_info[i].atom_matches *
@@ -714,7 +691,6 @@ YR_API YR_RULE_PROFILING_INFO* yr_scanner_get_profiling_info(
 
   return profiling_info;
 }
-
 
 YR_API void yr_scanner_reset_profiling_info(YR_SCANNER* scanner)
 {

commit 4c3bc7e8fe0ade34dfa39aa5cd6c87ec409af171

Avoid breaking existing code due to changes in field names introduced in 7bb8ecd742f802f76f1d6e3e5d5cd220ae1b39ca.
The old names can still be used, but they are marked as deprecated and will raise a warning.

libyara/include/yara/types.h
@@ -527,14 +527,41 @@ struct YR_RULES
 
   // Array of pointers with an entry for each rule. The rule_idx field in the
   // YR_STRING structure is an index within this array.
-  YR_RULE* rules_table;
+  union
+  {
+    YR_RULE* rules_table;
+    // The previous name for rules_table was rules_list_head, because this
+    // was previously a linked list. The old name is maintained but marked as
+    // deprecated, which will raise a warning if used.
+    // TODO(vmalvarez): Remove this field when a reasonable a few versions
+    // after 4.1 has been released.
+    YR_RULE* rules_list_head YR_DEPRECATED_API;
+  };
 
   // Array of pointers with an entry for each of the defined strings. The idx
   // field in the YR_STRING structure is an index within this array.
-  YR_STRING* strings_table;
+  union
+  {
+    YR_STRING* strings_table;
+    // The previous name for strings_table was strings_list_head, because this
+    // was previously a linked list. The old name is maintained but marked as
+    // deprecated, which will raise a warning if used.
+    // TODO(vmalvarez): Remove this field when a reasonable a few versions
+    // after 4.1 has been released.
+    YR_STRING* strings_list_head YR_DEPRECATED_API;
+  };
 
   // Array of pointers with an entry for each external variable.
-  YR_EXTERNAL_VARIABLE* ext_vars_table;
+  union
+  {
+    YR_EXTERNAL_VARIABLE* ext_vars_table;
+    // The previous name for ext_vars_table was externals_list_head, because
+    // this was previously a linked list. The old name is maintained but marked
+    // as deprecated, which will raise a warning if used.
+    // TODO(vmalvarez): Remove this field when a reasonable a few versions
+    // after 4.1 has been released.
+    YR_EXTERNAL_VARIABLE* externals_list_head YR_DEPRECATED_API;
+  };
 
   // Pointer to the Aho-Corasick transition table.
   YR_AC_TRANSITION* ac_transition_table;