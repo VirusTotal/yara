87381dfaf972e9d38e414f6db48a959f555f90c7
Victor M. Alvarez <vmalvarez@virustotal.com>
2021-01-12 13:58:41 +0100

    Fix build with MinGW and some styling issues.

--- cli/common.c
+++ cli/common.c
@@ -27,11 +27,18 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#ifdef _WIN32
-   #include <io.h>
-   #define access    _taccess_s
+#if defined(_WIN32)
+#include <io.h>
+
+// In Visual C++ use _taccess_s, in MinGW use access_s.
+#if defined(_MSC_VER)
+#define access _taccess_s
 #else
-   #include <unistd.h>
+#define access access_s
+#endif
+
+#else  // not _WIN32
+#include <unistd.h>
 #endif
 
 #include <stdbool.h>
@@ -40,45 +47,31 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "common.h"
 #include "unicode.h"
 
-#define exit_with_code(code) { result = code; goto _exit; }
+#define exit_with_code(code) \
+  {                          \
+    result = code;           \
+    goto _exit;              \
+  }
 
-
-#ifdef _UNICODE
+#if defined(_UNICODE)
 char* unicode_to_ansi(const char_t* str)
 {
-	if (str == NULL)
-	  return NULL;
+  if (str == NULL)
+    return NULL;
 
-	int str_len = WideCharToMultiByte(
-		CP_ACP,
-	    WC_NO_BEST_FIT_CHARS,
-	    str,
-        -1,
-	    NULL,
-	    0,
-	    NULL,
-	    NULL);
+  int str_len = WideCharToMultiByte(
+      CP_ACP, WC_NO_BEST_FIT_CHARS, str, -1, NULL, 0, NULL, NULL);
 
-	char* str_utf8 = (char*) malloc(str_len);
+  char* str_utf8 = (char*) malloc(str_len);
 
-	WideCharToMultiByte(
-		CP_ACP,
-		WC_NO_BEST_FIT_CHARS,
-		str,
-		-1,
-		str_utf8,
-		str_len,
-		NULL,
-		NULL);
+  WideCharToMultiByte(
+      CP_ACP, WC_NO_BEST_FIT_CHARS, str, -1, str_utf8, str_len, NULL, NULL);
 
-   return str_utf8;
+  return str_utf8;
 }
 #endif
 
-bool compile_files(
-    YR_COMPILER* compiler,
-    int argc,
-    const char_t** argv)
+bool compile_files(YR_COMPILER* compiler, int argc, const char_t** argv)
 {
   for (int i = 0; i < argc - 1; i++)
   {
@@ -118,20 +111,20 @@ bool compile_files(
 
     if (rule_file == NULL)
     {
-	  _ftprintf(stderr, _T("error: could not open file: %s\n"), file_name);
+      _ftprintf(stderr, _T("error: could not open file: %s\n"), file_name);
       return false;
     }
 
 #if defined(_UNICODE)
-	char* file_name_mb = unicode_to_ansi(file_name);
-	char* ns_mb = unicode_to_ansi(ns);
+    char* file_name_mb = unicode_to_ansi(file_name);
+    char* ns_mb = unicode_to_ansi(ns);
 
-	errors = yr_compiler_add_file(compiler, rule_file, ns_mb, file_name_mb);
+    errors = yr_compiler_add_file(compiler, rule_file, ns_mb, file_name_mb);
 
-	free(file_name_mb);
-	free(ns_mb);
+    free(file_name_mb);
+    free(ns_mb);
 #else
-	errors = yr_compiler_add_file(compiler, rule_file, ns, file_name);
+    errors = yr_compiler_add_file(compiler, rule_file, ns, file_name);
 #endif
 
     fclose(rule_file);
@@ -143,92 +136,73 @@ bool compile_files(
   return true;
 }
 
-
 int define_external_variables(
-	char** ext_vars,
-	YR_RULES* rules,
-	YR_COMPILER* compiler)
+    char** ext_vars,
+    YR_RULES* rules,
+    YR_COMPILER* compiler)
 {
-	int result = ERROR_SUCCESS;
+  int result = ERROR_SUCCESS;
 
-	for (int i = 0; ext_vars[i] != NULL; i++)
-	{
-		char* equal_sign = strchr(ext_vars[i], '=');
+  for (int i = 0; ext_vars[i] != NULL; i++)
+  {
+    char* equal_sign = strchr(ext_vars[i], '=');
 
-		if (!equal_sign)
-		{
-			fprintf(stderr, "error: wrong syntax for `-d` option.\n");
-			return ERROR_SUCCESS;
-		}
+    if (!equal_sign)
+    {
+      fprintf(stderr, "error: wrong syntax for `-d` option.\n");
+      return ERROR_SUCCESS;
+    }
 
-		// Replace the equal sign with null character to split the external
-		// variable definition (i.e: myvar=somevalue) in two strings: identifier
-		// and value.
+    // Replace the equal sign with null character to split the external
+    // variable definition (i.e: myvar=somevalue) in two strings: identifier
+    // and value.
 
-		*equal_sign = '\0';
+    *equal_sign = '\0';
 
-		char* value = equal_sign + 1;
-		char* identifier = ext_vars[i];
-		
-		if (is_float(value))
-		{
-			if (rules != NULL)
-				result = yr_rules_define_float_variable(
-					rules,
-					identifier,
-					atof(value));
+    char* value = equal_sign + 1;
+    char* identifier = ext_vars[i];
 
-			if (compiler != NULL)
-				result = yr_compiler_define_float_variable(
-					compiler,
-					identifier,
-					atof(value));
-		}
-		else if (is_integer(value))
-		{
-			if (rules != NULL)
-				result = yr_rules_define_integer_variable(
-					rules,
-					identifier,
-					atoi(value));
+    if (is_float(value))
+    {
+      if (rules != NULL)
+        result = yr_rules_define_float_variable(rules, identifier, atof(value));
 
-			if (compiler != NULL)
-				result = yr_compiler_define_integer_variable(
-					compiler,
-					identifier,
-					atoi(value));
-		}
-		else if (strcmp(value, "true") == 0 || strcmp(value, "false") == 0)
-		{
-			if (rules != NULL)
-				result = yr_rules_define_boolean_variable(
-					rules,
-					identifier,
-					strcmp(value, "true") == 0);
+      if (compiler != NULL)
+        result = yr_compiler_define_float_variable(
+            compiler, identifier, atof(value));
+    }
+    else if (is_integer(value))
+    {
+      if (rules != NULL)
+        result = yr_rules_define_integer_variable(
+            rules, identifier, atoi(value));
 
-			if (compiler != NULL)
-				result = yr_compiler_define_boolean_variable(
-					compiler,
-					identifier,
-					strcmp(value, "true") == 0);
-		}
-		else
-		{
-			if (rules != NULL)
-				result = yr_rules_define_string_variable(
-					rules,
-					identifier,
-					value);
+      if (compiler != NULL)
+        result = yr_compiler_define_integer_variable(
+            compiler, identifier, atoi(value));
+    }
+    else if (strcmp(value, "true") == 0 || strcmp(value, "false") == 0)
+    {
+      if (rules != NULL)
+        result = yr_rules_define_boolean_variable(
+            rules, identifier, strcmp(value, "true") == 0);
 
-			if (compiler != NULL)
-				result = yr_compiler_define_string_variable(
-					compiler,
-					identifier,
-					value);
-		}
-	}
+      if (compiler != NULL)
+        result = yr_compiler_define_boolean_variable(
+            compiler, identifier, strcmp(value, "true") == 0);
+    }
+    else
+    {
+      if (rules != NULL)
+        result = yr_rules_define_string_variable(rules, identifier, value);
 
-	return result;
+      if (compiler != NULL)
+        result = yr_compiler_define_string_variable(
+            compiler, identifier, value);
+    }
+  }
+
+  return result;
 }
 
 bool is_integer(const char* str)
@@ -239,7 +213,7 @@ bool is_integer(const char* str)
   if (*str == '\0')
     return false;
 
-  while(*str)
+  while (*str)
   {
     if (!isdigit(*str))
       return false;
@@ -249,22 +223,21 @@ bool is_integer(const char* str)
   return true;
 }
 
-
 bool is_float(const char* str)
 {
   bool has_dot = false;
 
-  if (*str == '-')      // skip the minus sign if present
+  if (*str == '-')  // skip the minus sign if present
     str++;
 
-  if (*str == '.')      // float can't start with a dot
+  if (*str == '.')  // float can't start with a dot
     return false;
 
-  while(*str)
+  while (*str)
   {
     if (*str == '.')
     {
-      if (has_dot)      // two dots, not a float
+      if (has_dot)  // two dots, not a float
         return false;
 
       has_dot = true;
@@ -277,5 +250,5 @@ bool is_float(const char* str)
     str++;
   }
 
-  return has_dot; // to be float must contain a dot
+  return has_dot;  // to be float must contain a dot
 }
