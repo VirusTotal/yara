4486851960636f97abbe7b6b27a749108e5aa203
Victor M. Alvarez <vmalvarez@virustotal.com>
2021-10-28 16:08:33 +0200

    Remove unused variable.

--- libyara/proc/linux.c
+++ libyara/proc/linux.c
@@ -306,7 +306,6 @@ _exit:;
 YR_API YR_MEMORY_BLOCK* yr_process_get_next_memory_block(
     YR_MEMORY_BLOCK_ITERATOR* iterator)
 {
-  YR_MEMORY_BLOCK* result = NULL;
   YR_PROC_ITERATOR_CTX* context = (YR_PROC_ITERATOR_CTX*) iterator->context;
   YR_PROC_INFO* proc_info = (YR_PROC_INFO*) context->proc_info;
 
