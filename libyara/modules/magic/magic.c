69bd11b0cd9f610f0d06826ef5016e3670c96ea4
LearnToGetBetter <loveopensource@huawei.com>
2021-12-07 21:40:10 +0800

    Solving memory leakage in function get_cache in magic.c

--- libyara/modules/magic/magic.c
+++ libyara/modules/magic/magic.c
@@ -67,12 +67,16 @@ static int get_cache(MAGIC_CACHE** cache)
     (*cache)->magic_cookie = magic_open(0);
 
     if ((*cache)->magic_cookie == NULL)
+    {
+      yr_free(*cache);
       return ERROR_INSUFFICIENT_MEMORY;
+    }
 
     if (magic_load((*cache)->magic_cookie, NULL) != 0)
     {
-        magic_close((*cache)->magic_cookie);
-        return ERROR_INTERNAL_FATAL_ERROR;
+      magic_close((*cache)->magic_cookie);
+      yr_free(*cache);
+      return ERROR_INTERNAL_FATAL_ERROR;
     }
 
     (*cache)->cached_type = NULL;
