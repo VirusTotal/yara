747272cea79d070956427b4862dca78297b8ef8c
Victor M. Alvarez <vmalvarez@virustotal.com>
2022-02-25 10:04:37 +0100

    Increase YR_ATOMS_PER_RULE_WARNING_THRESHOLD to 12000.

--- libyara/include/yara/limits.h
+++ libyara/include/yara/limits.h
@@ -87,7 +87,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // If a rule generates more than this number of atoms a warning is shown.
 #ifndef YR_ATOMS_PER_RULE_WARNING_THRESHOLD
-#define YR_ATOMS_PER_RULE_WARNING_THRESHOLD 10000
+#define YR_ATOMS_PER_RULE_WARNING_THRESHOLD 12000
 #endif
 
 // Maximum number of nested "for" loops in rule. Rules ith nested loops
