7ed3cc413635b51ea34006cd6094a8cd8455b526
Victor M. Alvarez <vmalvarez@virustotal.com>
2022-03-02 16:39:56 +0100

    Fix buffer overrun in dotnet module.

--- libyara/modules/dotnet/dotnet.c
+++ libyara/modules/dotnet/dotnet.c
@@ -438,16 +438,15 @@ void dotnet_parse_tilde_2(
     if (!((yr_le64toh(tilde_header->Valid) >> bit_check) & 0x01))
       continue;
 
+    if (!fits_in_pe(pe, row_offset + matched_bits, sizeof(uint32_t)))
+      return;
+
     num_rows = yr_le32toh(*(row_offset + matched_bits));
 
     // Make sure that num_rows has a reasonable value. For example
     // edc05e49dd3810be67942b983455fd43 sets a large value for number of
     // rows for the BIT_MODULE section.
     if (num_rows > 10000)
-      return;
-
-    // Make sure that table_offset is within the bounds of the PE.
-    if (!fits_in_pe(pe, table_offset, 1))
       return;
 
     // Those tables which exist, but that we don't care about must be
