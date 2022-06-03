285f72631a021ffd9c62b8146b85704add7fbd3d
BitsOfBinary <48534004+BitsOfBinary@users.noreply.github.com>
2022-06-03 18:44:55 +0100

    Refactor code into header + util files
    Moving a lot of the definitions over to a separate header file to keep the main code clean.
    
    Also following the PE module's structure of having a separate `lnk_utils` file to deal with some of the convenient functions it provides.

---
+++ libyara/include/yara/lnk.h
@@ -0,0 +1,100 @@
+#include <yara/integers.h>
+
+#ifndef _LNK_H
+#define _LNK_H
+
+#define TICKS_PER_SECOND 10000000
+#define EPOCH_DIFFERENCE 11644473600LL
+
+#pragma pack(push, 4)
+
+typedef struct _shell_link_header_t
+{
+  uint32_t header_size;
+  uint32_t clsid[4];
+  uint32_t link_flags;
+  uint32_t file_attributes_flags;
+  uint64_t creation_time;
+  uint64_t access_time;
+  uint64_t write_time;
+  uint32_t file_size;
+  uint32_t icon_index;
+  uint32_t show_command;
+  uint16_t hotkey_flags;
+  uint16_t reserved1;
+  uint32_t reserved2;
+  uint32_t reserved3;
+} shell_link_header_t;
+
+typedef struct _link_info_fixed_header_t
+{
+  uint32_t link_info_size;
+  uint32_t link_info_header_size;
+  uint32_t link_info_flags;
+  uint32_t volume_id_offset;
+  uint32_t local_base_path_offset;
+  uint32_t common_network_relative_link_offset;
+  uint32_t common_path_suffix_offset;
+} link_info_fixed_header_t;
+
+#pragma pack(pop)
+
+#define MIN_LNK_SIZE 76
+
+#define HEADER_SIZE 0x0000004C
+#define LINK_CLSID_0 0x00021401
+#define LINK_CLSID_1 0x00000000
+#define LINK_CLSID_2 0x000000C0
+#define LINK_CLSID_3 0x46000000
+
+#define LINK_INFO_FIXED_HEADER_LENGTH 28
+
+#define HasLinkTargetIDList            0x00000001
+#define HasLinkInfo                    0x00000002
+#define HasName                        0x00000004
+#define HasRelativePath                0x00000008
+#define HasWorkingDir                  0x00000010
+#define HasArguments                   0x00000020
+#define HasIconLocation                0x00000040
+#define IsUnicode                      0x00000080
+#define ForceNoLinkInfo                0x00000100
+#define HasExpString                   0x00000200
+#define RunInSeparateProcess           0x00000400
+#define Unused1                        0x00000800
+#define HasDarwinID                    0x00001000
+#define RunAsUser                      0x00002000
+#define HasExpIcon                     0x00004000
+#define NoPidlAlias                    0x00008000
+#define Unused2                        0x00010000
+#define RunWithShimLayer               0x00020000
+#define ForceNoLinkTrack               0x00040000
+#define EnableTargetMetadata           0x00080000
+#define DisableLinkPathTracking        0x00100000
+#define DisableKnownFolderTracking     0x00200000
+#define DisableKnownFolderAlias        0x00400000
+#define AllowLinkToLink                0x00800000
+#define UnaliasOnSave                  0x01000000
+#define PreferEnvironmentPath          0x02000000
+#define KeepLocalIDListForUNCTarget    0x04000000
+
+#define FILE_ATTRIBUTE_READONLY               0x00000001
+#define FILE_ATTRIBUTE_HIDDEN                 0x00000002
+#define FILE_ATTRIBUTE_SYSTEM                 0x00000004
+#define Reserved1                             0x00000008
+#define FILE_ATTRIBUTE_DIRECTORY              0x00000010
+#define FILE_ATTRIBUTE_ARCHIVE                0x00000020
+#define Reserved2                             0x00000040
+#define FILE_ATTRIBUTE_NORMAL                 0x00000080
+#define FILE_ATTRIBUTE_TEMPORARY              0x00000100
+#define FILE_ATTRIBUTE_SPARSE_FILE            0x00000200
+#define FILE_ATTRIBUTE_REPARSE_POINT          0x00000400
+#define FILE_ATTRIBUTE_COMPRESSED             0x00000800
+#define FILE_ATTRIBUTE_OFFLINE                0x00001000
+#define FILE_ATTRIBUTE_NOT_CONTENT_INDEXED    0x00002000
+#define FILE_ATTRIBUTE_ENCRYPTED              0x00004000
+
+#define HOTKEYF_SHIFT       0x01
+#define HOTKEYF_CONTROL     0x02
+#define HOTKEYF_ALT         0x04
+
+#endif
