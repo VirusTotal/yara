ddb5634aef2beb1ec74def9e282f5c5c0e1e18a3
Wesley Shields <wxs@atarininja.org>
2022-08-30 11:20:28 -0400

    Add math.length()
    Add a math.length() which will return the length of the sequence of bytes,
    including any NULL bytes.
    
    Fixes #1778.

--- libyara/modules/math/math.c
+++ libyara/modules/math/math.c
@@ -773,6 +773,12 @@ define_function(to_int_base)
   return_integer(result == 0 && errno ? YR_UNDEFINED : result);
 }
 
+define_function(string_length)
+{
+  SIZED_STRING* s = sized_string_argument(1);
+  return_integer(s->length);
+}
+
 begin_declarations
   declare_float("MEAN_BYTES");
   declare_function("in_range", "fff", "i", in_range);
@@ -800,6 +806,7 @@ begin_declarations
   declare_function("to_string", "ii", "s", to_string_base);
   declare_function("to_int", "s", "i", to_int);
   declare_function("to_int", "si", "i", to_int_base);
+  declare_function("length", "s", "i", string_length);
 end_declarations
 
 int module_initialize(YR_MODULE* module)
