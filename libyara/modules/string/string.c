18e04de4a8cdf7b30d26825ae4314036a105a57c
Wesley Shields <wxs@atarininja.org>
2022-12-14 21:41:07 -0500

    Fix copyright year in string module.

--- libyara/modules/string/string.c
+++ libyara/modules/string/string.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014-2021. The YARA Authors. All Rights Reserved.
+Copyright (c) 2014-2022. The YARA Authors. All Rights Reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
