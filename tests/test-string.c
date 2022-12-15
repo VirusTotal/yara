7283becc2c46a62f8c942c328cce98f791c1df21
Wesley Shields <wxs@atarininja.org>
2022-12-15 08:26:04 -0500

    Add tests/test-string.c and update .gitignore

---
+++ tests/test-string.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <yara.h>
+
+#include "util.h"
+
+int main(int argc, char** argv)
+{
+  int result = 0;
+
+  YR_DEBUG_INITIALIZE();
+  YR_DEBUG_FPRINTF(1, stderr, "+ %s() { // in %s\n", __FUNCTION__, argv[0]);
+
+  yr_initialize();
+
+  assert_true_rule(
+      "import \"string\" \
+      rule test { \
+        condition: \
+          string.to_int(\"1234\") == 1234 \
+      }",
+      NULL);
+
+  assert_true_rule(
+      "import \"string\" \
+      rule test { \
+        condition: \
+          string.to_int(\"-1\") == -1 \
+      }",
+      NULL);
+
+  // Leading spaces and + are allowed.
+  assert_true_rule(
+      "import \"string\" \
+      rule test { \
+        condition: \
+          string.to_int(\" +1\") == 1 \
+      }",
+      NULL);
+
+  // Strings can be prefixed with 0x and will be interpreted as hexadecimal.
+  assert_true_rule(
+      "import \"string\" \
+      rule test { \
+        condition: \
+          string.to_int(\"0x10\") == 16 \
+      }",
+      NULL);
+
+  // Strings prefixed with 0 will be interpreted as octal.
+  assert_true_rule(
+      "import \"string\" \
+      rule test { \
+        condition: \
+          string.to_int(\"010\") == 8 \
+      }",
+      NULL);
+
+  // Strings that are only partially converted are still fine.
+  assert_true_rule(
+      "import \"string\" \
+      rule test { \
+        condition: \
+          string.to_int(\"10A20\") == 10 \
+      }",
+      NULL);
+
+  assert_true_rule(
+      "import \"string\" \
+      rule test { \
+        condition: \
+          string.to_int(\"10\", 8) == 8 \
+      }",
+      NULL);
+
+  // Base 0 is a special case that tries to interpret the string by prefix, or
+  // default to decimal. We aren't doing anything special to get this, it is
+  // part of strtoll by default.
+  assert_true_rule(
+      "import \"string\" \
+      rule test { \
+        condition: \
+          string.to_int(\"010\", 0) == 8 and \
+          string.to_int(\"0x10\", 0) == 16 and \
+          string.to_int(\"10\", 0) == 10 \
+      }",
+      NULL);
+
+  assert_true_rule(
+      "import \"string\" \
+      rule test { \
+        condition: \
+          string.length(\"AXS\\x00ERS\") == 7 \
+      }",
+      NULL);
+
+  yr_finalize();
+
+  YR_DEBUG_FPRINTF(
+      1, stderr, "} = %d // %s() in %s\n", result, __FUNCTION__, argv[0]);
+
+  return result;
+}
