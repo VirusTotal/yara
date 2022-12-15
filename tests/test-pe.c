e67723b95aa08a2479a8b8b5e61aef360daff156
Wesley Shields <wxs@atarininja.org>
2022-12-14 21:31:35 -0500

    Add tests for https://github.com/VirusTotal/yara/pull/1561/

--- tests/test-pe.c
+++ tests/test-pe.c
@@ -873,6 +873,16 @@ int main(int argc, char** argv)
       }",
       "tests/data/pe_mingw");
 
+  // The first 0x410 bytes of
+  // c6f9709feccf42f2d9e22057182fe185f177fb9daaa2649b4669a24f2ee7e3ba are enough
+  // to trigger the bug in https://github.com/VirusTotal/yara/pull/1561
+  assert_true_rule_file(
+      "import \"pe\" \
+      rule rva_to_offset_weird_sections { \
+        condition: \
+          pe.rva_to_offset(4096) == 1024 \
+      }",
+      "tests/data/c6f9709feccf42f2d9e22057182fe185f177fb9daaa2649b4669a24f2ee7e3ba_0h_410h");
   yr_finalize();
 
   YR_DEBUG_FPRINTF(
