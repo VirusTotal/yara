e9906c14c2da1eb2a0eb8995a5e7e08c73c63e15
Victor M. Alvarez <vmalvarez@virustotal.com>
2026-06-02 10:55:07 +0200

    Try to find issue with magic module.

--- tests/test-magic.c
+++ tests/test-magic.c
@@ -23,6 +23,28 @@ int main(int argc, char** argv)
 
   assert_true_rule_blob(
       "import \"magic\" rule test { condition: \
+      ( magic.type() contains \"MS-DOS\" or \
+        magic.type() contains \"PE32\" }",
+      PE32_FILE);
+
+  assert_true_rule_blob(
+      "import \"magic\" rule test { condition: \
+      ( magic.type() contains \"MS-DOS executable\" or \
+        magic.type() contains \"PE32+ executable\" or \
+        magic.type() contains \"PE32 executable\") }",
+      PE32_FILE);
+
+  assert_true_rule_blob(
+      "import \"magic\" rule test { condition: \
+      ( magic.type() contains \"MS-DOS executable\" or \
+        magic.type() contains \"PE32+ executable\" or \
+        magic.type() contains \"PE32 executable\") and \
+      ( magic.mime_type() == \"application/x-dosexec\" or \
+        magic.mime_type() == \"application/vnd.microsoft.portable-executable\" ) }",
+      PE32_FILE);
+
+  assert_true_rule_blob(
+      "import \"magic\" rule test { condition: \
       ( magic.type() contains \"MS-DOS executable\" or \
         magic.type() contains \"PE32+ executable\" or \
         magic.type() contains \"PE32 executable\") and \
